60a145f59e4dd88bc3d169454f1bfc72f60309bc
Karan Adhikari <doc.adhikari@gmail.com>
2022-10-03 12:03:06 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -237,7 +237,11 @@ except KeyError as e:
     AUTO_DELETE_UPLOAD_MESSAGE_DURATION = -1
     LOGGER.warning("AUTO_DELETE_UPLOAD_MESSAGE_DURATION var missing!")
     pass
-
+try:
+    TIME_GAP = int(getConfig('TIME_GAP'))
+except KeyError as e:
+    TIME_GAP = -1
+    pass
 LOGGER.info("Generating SESSION_STRING")
 app = Client(name='pyrogram', api_id=int(TELEGRAM_API), api_hash=TELEGRAM_HASH, bot_token=BOT_TOKEN, parse_mode=enums.ParseMode.HTML, no_updates=True)
 
@@ -365,13 +369,6 @@ try:
     SEARCH_LIMIT = int(SEARCH_LIMIT)
 except:
     SEARCH_LIMIT = 0
-try:
-    TIME_GAP = getConfig('TIME_GAP')
-    if len(TIME_GAP) == 0:
-        raise KeyError
-    TIME_GAP = int(TIME_GAP)
-except:
-    TIME_GAP = 600
 try:
     PAID_SERVICE = getConfig('PAID_SERVICE')
     PAID_SERVICE = PAID_SERVICE.lower() == 'true'
