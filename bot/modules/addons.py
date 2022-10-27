0dfb5fca11b202b207c1e0ed5715abc5e4a0c0cc
Karan Adhikari <doc.adhikari@gmail.com>
2022-10-27 16:58:33 +0530

    V2.7 > some scrapper are added > Htp scraper by JohnWickKeanue > Thanks for 5MysterySD for help

--- bot/modules/addons.py
+++ bot/modules/addons.py
@@ -13,139 +13,85 @@ from bot.helper.ext_utils.db_handler import DbManger
 def prename_set(update, context):
     user_id_ = update.message.from_user.id 
     u_men = update.message.from_user.first_name
+
     if PAID_SERVICE is True:
-        if user_id_ in PAID_USERS and OWNER_ID:
-            if (BotCommands.PreNameCommand in update.message.text) and (len(update.message.text.split(' ')) == 1):
-                sendMessage(f'<b>Set Prename Like👇 \n/{BotCommands.PreNameCommand} channelName</b>', context.bot, update.message)
-            else:
-                lm = sendMessage(f"<b>Please Wait....Processing🤖</b>", context.bot, update.message)
-                pre_send = update.message.text.split(" ", maxsplit=1)
-                reply_to = update.message.reply_to_message
-                if len(pre_send) > 1:
-                    txt = pre_send[1]
-                elif reply_to is not None:
-                    txt = reply_to.text
-                else:
-                    txt = ""
-                prefix_ = txt
-                PRE_DICT[user_id_] = prefix_
-                if DB_URI:
-                    DbManger().user_pre(user_id_, prefix_)
-                    LOGGER.info(f"User : {user_id_} Prename is Saved in DB")
-                editMessage(f"<b>{u_men} Prename for the Leech file is Set now🚀</b>\n\n<b>Your Prename Text: </b>{txt}", lm)
-        else:
-            sendMessage(f"Buy Paid Service to use Feature.", context.bot, update.message)
+        if not (user_id_ in PAID_USERS) and user_id_ != OWNER_ID:
+            sendMessage(f"Buy Paid Service to Use this Prename Feature.", context.bot, update.message)
+            return
+    if (BotCommands.PreNameCommand in update.message.text) and (len(update.message.text.split(' ')) == 1):
+        sendMessage(f'<b>Set Prename Like👇 \n/{BotCommands.PreNameCommand} channelName</b>', context.bot, update.message)
     else:
-        if (BotCommands.PreNameCommand in update.message.text) and (len(update.message.text.split(' ')) == 1):
-            sendMessage(f'<b>Set Prename Like👇 \n/{BotCommands.PreNameCommand} channelName</b>', context.bot, update.message)
+        lm = sendMessage(f"<b>Please Wait....Processing🤖</b>", context.bot, update.message)
+        pre_send = update.message.text.split(" ", maxsplit=1)
+        reply_to = update.message.reply_to_message
+        if len(pre_send) > 1:
+            txt = pre_send[1]
+        elif reply_to is not None:
+            txt = reply_to.text
         else:
-            lm = sendMessage(f"<b>Please Wait....Processing🤖</b>", context.bot, update.message)
-            pre_send = update.message.text.split(" ", maxsplit=1)
-            reply_to = update.message.reply_to_message
-            if len(pre_send) > 1:
-                txt = pre_send[1]
-            elif reply_to is not None:
-                txt = reply_to.text
-            else:
-                txt = ""
-            prefix_ = txt
-            PRE_DICT[user_id_] = prefix_
-            if DB_URI:
-                DbManger().user_pre(user_id_, prefix_)
-                LOGGER.info(f"User : {user_id_} Prename is Saved in DB")
-            editMessage(f"<b>{u_men} Prename for the Leech file is Set now🚀</b>\n\n<b>Your Prename Text: </b>{txt}", lm)
+            txt = ""
+        prefix_ = txt
+        PRE_DICT[user_id_] = prefix_
+        if DB_URI:
+            DbManger().user_pre(user_id_, prefix_)
+            LOGGER.info(f"User : {user_id_} Prename is Saved in DB")
+        editMessage(f"<b>{u_men} Prename for the Leech file is Set now🚀</b>\n\n<b>Your Prename Text: </b>{txt}", lm)
 
 
 def caption_set(update, context):
     user_id_ = update.message.from_user.id 
     u_men = update.message.from_user.first_name
+
     if PAID_SERVICE is True:
-        if user_id_ in PAID_USERS and OWNER_ID:
-            if (BotCommands.CaptionCommand in update.message.text) and (len(update.message.text.split(' ')) == 1):
-                sendMessage(f'<b>Set Caption Like👇 \n/{BotCommands.CaptionCommand} text</b>', context.bot, update.message)
-            else:
-                lm = sendMessage(f"<b>Please Wait....Processing🤖</b>", context.bot, update.message)
-                pre_send = update.message.text.split(" ", maxsplit=1)
-                reply_to = update.message.reply_to_message
-                if len(pre_send) > 1:
-                    txt = pre_send[1]
-                elif reply_to is not None:
-                    txt = reply_to.text
-                else:
-                    txt = ""
-                caption_ = txt
-                CAP_DICT[user_id_] = caption_
-                if DB_URI:
-                    DbManger().user_cap(user_id_, caption_)
-                    LOGGER.info(f"User : {user_id_} Caption is Saved in DB")
-                editMessage(f"<b>{u_men} Caption for the Leech file is Set now🌋</b>\n\n<b>Your Caption Text: </b>{txt}", lm)
-        else:
-            sendMessage(f"Buy Paid Service to use Feature.", context.bot, update.message)
+        if not (user_id_ in PAID_USERS) and user_id_ != OWNER_ID:
+            sendMessage(f"Buy Paid Service to Use this Caption Feature.", context.bot, update.message)
+            return
+    if (BotCommands.CaptionCommand in update.message.text) and (len(update.message.text.split(' ')) == 1):
+        sendMessage(f'<b>Set Caption Like👇 \n/{BotCommands.CaptionCommand} text</b>', context.bot, update.message)
     else:
-        if (BotCommands.CaptionCommand in update.message.text) and (len(update.message.text.split(' ')) == 1):
-            sendMessage(f'<b>Set Caption Like👇 \n/{BotCommands.CaptionCommand} text</b>', context.bot, update.message)
+        lm = sendMessage(f"<b>Please Wait....Processing🤖</b>", context.bot, update.message)
+        pre_send = update.message.text.split(" ", maxsplit=1)
+        reply_to = update.message.reply_to_message
+        if len(pre_send) > 1:
+            txt = pre_send[1]
+        elif reply_to is not None:
+            txt = reply_to.text
         else:
-            lm = sendMessage(f"<b>Please Wait....Processing🤖</b>", context.bot, update.message)
-            pre_send = update.message.text.split(" ", maxsplit=1)
-            reply_to = update.message.reply_to_message
-            if len(pre_send) > 1:
-                txt = pre_send[1]
-            elif reply_to is not None:
-                txt = reply_to.text
-            else:
-                txt = ""
-            caption_ = txt
-            CAP_DICT[user_id_] = caption_
-            if DB_URI:
-                DbManger().user_cap(user_id_, caption_)
-                LOGGER.info(f"User : {user_id_} Caption is Saved in DB")
-            editMessage(f"<b>{u_men} Caption for the Leech file is Set now🌋</b>\n\n<b>Your Caption Text: </b>{txt}", lm)
+            txt = ""
+        caption_ = txt
+        CAP_DICT[user_id_] = caption_
+        if DB_URI:
+            DbManger().user_cap(user_id_, caption_)
+            LOGGER.info(f"User : {user_id_} Caption is Saved in DB")
+        editMessage(f"<b>{u_men} Caption for the Leech file is Set now🌋</b>\n\n<b>Your Caption Text: </b>{txt}", lm)
 
 
 def userlog_set(update, context):
     user_id_ = update.message.from_user.id 
     u_men = update.message.from_user.first_name
+
     if PAID_SERVICE is True:
-        if user_id_ in PAID_USERS and OWNER_ID:
-            if (BotCommands.UserLogCommand in update.message.text) and (len(update.message.text.split(' ')) == 1):
-                sendMessage(f'Send Your Backup Channel ID alone with command like \n\n{BotCommands.UserLogCommand} -100xxxxxxx', context.bot, update.message)
-            else:
-                lm = sendMessage("Please wait...🤖", context.bot, update.message)          
-                pre_send = update.message.text.split(" ", maxsplit=1)
-                reply_to = update.message.reply_to_message
-                if len(pre_send) > 1:
-                    txt = pre_send[1]
-                elif reply_to is not None:
-                    txt = reply_to.text
-                else:
-                    txt = ""
-                dumpid_ = txt
-                LEECH_DICT[user_id_] = dumpid_
-                if DB_URI:
-                    DbManger().user_dump(user_id_, dumpid_)
-                    LOGGER.info(f"User : {user_id_} LeechLog ID Saved in DB")
-                editMessage(f"<b>{u_men} your Channel ID Saved...🛸</b>", lm)
-        else:
-            sendMessage(f"Buy Paid Service to use Feature.", context.bot, update.message)
+        if not (user_id_ in PAID_USERS) and user_id_ != OWNER_ID:
+            sendMessage(f"Buy Paid Service to Use this Dump Feature.", context.bot, update.message)
+            return
+    if (BotCommands.UserLogCommand in update.message.text) and (len(update.message.text.split(' ')) == 1):
+        sendMessage(f'Send Your Backup Channel ID alone with command like \n\n{BotCommands.UserLogCommand} -100xxxxxxx', context.bot, update.message)
     else:
-        if (BotCommands.UserLogCommand in update.message.text) and (len(update.message.text.split(' ')) == 1):
-            sendMessage(f'Send Your Backup Channel ID alone with command like \n\n{BotCommands.UserLogCommand} -100xxxxxxx', context.bot, update.message)
+        lm = sendMessage("Please wait...🤖", context.bot, update.message)          
+        pre_send = update.message.text.split(" ", maxsplit=1)
+        reply_to = update.message.reply_to_message
+        if len(pre_send) > 1:
+            txt = pre_send[1]
+        elif reply_to is not None:
+            txt = reply_to.text
         else:
-            lm = sendMessage("Please wait...🤖", context.bot, update.message)          
-            pre_send = update.message.text.split(" ", maxsplit=1)
-            reply_to = update.message.reply_to_message
-            if len(pre_send) > 1:
-                txt = pre_send[1]
-            elif reply_to is not None:
-                txt = reply_to.text
-            else:
-                txt = ""
-            dumpid_ = txt
-            LEECH_DICT[user_id_] = dumpid_
-            if DB_URI:
-                DbManger().user_dump(user_id_, dumpid_)
-                LOGGER.info(f"User : {user_id_} LeechLog ID Saved in DB")
-            editMessage(f"<b>{u_men} your Channel ID Saved...🛸</b>", lm)
+            txt = ""
+        dumpid_ = txt
+        LEECH_DICT[user_id_] = dumpid_
+        if DB_URI:
+            DbManger().user_dump(user_id_, dumpid_)
+            LOGGER.info(f"User : {user_id_} LeechLog ID Saved in DB")
+        editMessage(f"<b>{u_men} your Channel ID Saved...🛸</b>", lm)
 
 
 prename_set_handler = CommandHandler(BotCommands.PreNameCommand, prename_set,
