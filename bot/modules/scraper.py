2e23539263dbe528c28c5c6f47e4f9ce1208a15f
Karan Adhikari <doc.adhikari@gmail.com>
2022-10-27 17:04:32 +0530

    sync with master branch

--- bot/modules/scraper.py
+++ bot/modules/scraper.py
@@ -1,72 +1,152 @@
+from re import match as rematch, findall, sub as resub
+from time import sleep
+from cloudscraper import create_scraper
+from urllib.parse import urlparse
+from requests import get as rget
+from bs4 import BeautifulSoup, NavigableString, Tag
+
 from telegram import Message
-import re
-import requests
-from bs4 import BeautifulSoup
 from telegram.ext import CommandHandler
 from bot import LOGGER, dispatcher, PAID_SERVICE, PAID_USERS, OWNER_ID
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot.helper.telegram_helper.message_utils import sendMessage
+from bot.helper.telegram_helper.message_utils import sendMessage, editMessage, deleteMessage
 
-
-def scrape(update, context):
+def scrapper(update, context):
     user_id_ = update.message.from_user.id
     if PAID_SERVICE is True:
-        if user_id_ in PAID_USERS and OWNER_ID:
-            message:Message = update.effective_message
-            link = None
-            if message.reply_to_message: link = message.reply_to_message.text
-            else:
-                link = message.text.split(' ', 1)
-                if len(link) != 2:
-                    help_msg = "<b>Send link after command:</b>"
-                    help_msg += f"\n<code>/{BotCommands.ScrapeCommand}" + " {link}" + "</code>"
-                    help_msg += "\n<b>By replying to message (including link):</b>"
-                    help_msg += f"\n<code>/{BotCommands.ScrapeCommand}" + " {message}" + "</code>"
-                    return sendMessage(help_msg, context.bot, update.message)
-                link = link[1]
-            try: link = re.match(r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*", link)[0]
-            except TypeError: return sendMessage('Not a valid link.', context.bot, update)
-            links = []
-            res = requests.get(link)
-            soup = BeautifulSoup(res.text, 'html.parser')
-            x = soup.select('a[href^="magnet:?xt=urn:btih:"]')
-            for a in x:
-                links.append(a['href'])
-            for o in links:
-#                 print(o)
-                sent = sendMessage(f"{o}", context.bot, update.message)
-        else:
-            sendMessage(f"Buy Paid Service to use Feature.", context.bot, update.message)
+        if not (user_id_ in PAID_USERS) and user_id_ != OWNER_ID:
+            sendMessage(f"Buy Paid Service to Use this Scrape Feature.", context.bot, update.message)
+            return
+    message:Message = update.effective_message
+    link = None
+    if message.reply_to_message: link = message.reply_to_message.text
     else:
-        message:Message = update.effective_message
-        link = None
-        if message.reply_to_message: link = message.reply_to_message.text
-        else:
-            link = message.text.split(' ', 1)
-            if len(link) != 2:
-                help_msg = "<b>Send link after command:</b>"
-                help_msg += f"\n<code>/{BotCommands.ScrapeCommand}" + " {link}" + "</code>"
-                help_msg += "\n<b>By replying to message (including link):</b>"
-                help_msg += f"\n<code>/{BotCommands.ScrapeCommand}" + " {message}" + "</code>"
-                return sendMessage(help_msg, context.bot, update.message)
+        link = message.text.split(' ', 1)
+        if len(link) == 2:
             link = link[1]
-        try: link = re.match(r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*", link)[0]
-        except TypeError: return sendMessage('Not a valid link.', context.bot, update)
-        links = []
-        res = requests.get(link)
+        else:
+            help_msg = "<b>Send link after command:</b>"
+            help_msg += f"\n<code>/{BotCommands.ScrapeCommand}" + " {link}" + "</code>"
+            help_msg += "\n<b>By Replying to Message (Including Link):</b>"
+            help_msg += f"\n<code>/{BotCommands.ScrapeCommand}" + " {message}" + "</code>"
+            return sendMessage(help_msg, context.bot, update.message)
+    try: link = rematch(r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*", link)[0]
+    except TypeError: return sendMessage('Not a Valid Link.', context.bot, update)
+    links = []
+    if "sharespark" in link:
+        gd_txt = ""
+        res = rget("?action=printpage;".join(link.split('?')))
         soup = BeautifulSoup(res.text, 'html.parser')
-        x = soup.select('a[href^="magnet:?xt=urn:btih:"]')
+        for br in soup.findAll('br'):
+            next_s = br.nextSibling
+            if not (next_s and isinstance(next_s,NavigableString)):
+                continue
+            next2_s = next_s.nextSibling
+            if next2_s and isinstance(next2_s,Tag) and next2_s.name == 'br':
+              text = str(next_s).strip()
+              if text:
+                  result = resub(r'(?m)^\(https://i.*', '', next_s)
+                  star = resub(r'(?m)^\*.*', ' ', result)
+                  extra = resub(r'(?m)^\(https://e.*', ' ', star)
+                  gd_txt += ', '.join(findall(r'(?m)^.*https://new1.gdtot.cfd/file/[0-9][^.]*', next_s)) + "\n\n"
+            if len(gd_txt) > 4000:
+                sendMessage(gd_txt, context.bot, update.message)
+                gd_txt = ""
+        if gd_txt != "":
+            sendMessage(gd_txt, context.bot, update.message)
+    elif "htpmovies" in link and "/exit.php" in link:
+        sent = sendMessage('Running scrape. Wait about some secs.', context.bot, update.message)
+        prsd = htpmovies(link)
+        editMessage(prsd, sent)
+    elif "htpmovies" in link:
+        sent = sendMessage('Running scrape. Wait about some secs.', context.bot, update.message)
+        prsd = ""
+        links = []
+        res = rget(link)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        x = soup.select('a[href^="/exit.php?url="]')
         for a in x:
             links.append(a['href'])
         for o in links:
-#                print(o)
-            sent = sendMessage(f"{o}", context.bot, update.message)
+            url = f"https://htpmovies.lol"+o
+            prsd += htpmovies(url) + '\n\n'
+            if len(prsd) > 4000:
+                deleteMessage(context.bot, sent)
+                sendMessage(prsd, context.bot, update.message)
+                prsd = ""
+        if prsd != "":
+            deleteMessage(context.bot, sent)
+            sendMessage(prsd, context.bot, update.message)
+    elif "cinevood" in link:
+        prsd = ""
+        links = []
+        res = rget(link)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        x = soup.select('a[href^="https://kolop.icu/file"]')
+        for a in x:
+            links.append(a['href'])
+        for o in links:
+            prsd += o + '\n\n'
+            if len(prsd) > 4000:
+                sendMessage(prsd, context.bot, update.message)
+                prsd = ""
+        if prsd != "":
+            sendMessage(prsd, context.bot, update.message)
+    elif "atishmkv" in link:
+        prsd = ""
+        links = []
+        res = rget(link)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        x = soup.select('a[href^="https://gdflix.top/file"]')
+        for a in x:
+            links.append(a['href'])
+        for o in links:
+            prsd += o + '\n\n'
+            if len(prsd) > 4000:
+                sendMessage(prsd, context.bot, update.message)
+                
+                prsd = ""
+        if prsd != "":
+            sendMessage(prsd, context.bot, update.message)
+    else:
+        res = rget(link)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        mystx = soup.select(r'a[href^="magnet:?xt=urn:btih:"]')
+        for hy in mystx:
+            links.append(hy['href'])
+        for txt in links:
+            sendMessage(txt, context.bot, update.message)
 
+def htpmovies(link):
+    download = rget(link.replace("exit", "go"), stream=True, allow_redirects=False) 
+    xurl =download.headers["location"]
+    client = create_scraper(allow_brotli=False)
+    p = urlparse(xurl)
+    final_url = f'{p.scheme}://{p.netloc}/links/go'
+    res = client.head(xurl)
+    header_loc = res.headers['location']
+    param = xurl.split("/")[-1]
+    req_url = f'{p.scheme}://{p.netloc}/{param}'
+    p = urlparse(header_loc)
+    ref_url = f'{p.scheme}://{p.netloc}/'
+    h = { 'referer': ref_url }
+    res = client.get(req_url, headers=h, allow_redirects=False)
+    bs4 = BeautifulSoup(res.content, 'html.parser')
+    inputs = bs4.find_all('input')
+    data = { input.get('name'): input.get('value') for input in inputs }
+    heads = {
+        'referer': ref_url,
+        'x-requested-with': 'XMLHttpRequest',
+    }
+    sleep(10)
+    res = client.post(final_url, headers=heads, data=data)
+    try:
+        return res.json()['url'].replace('\/','/')
+    except: return 'Something went wrong :('
+        
+srp_handler = CommandHandler(BotCommands.ScrapeCommand, scrapper,
 
-
-scrape_handler = CommandHandler(BotCommands.ScrapeCommand, scrape,
                                 filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 
-
-dispatcher.add_handler(scrape_handler)
+dispatcher.add_handler(srp_handler)
