fee275569c3d9ec02a8ba1d80920747485aadaff
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2022-11-12 23:10:00 +0530

    Check #1

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -8,7 +8,7 @@ from os import remove
 
 from bot import AUTO_DELETE_MESSAGE_DURATION, LOGGER, status_reply_dict, status_reply_dict_lock, \
                 Interval, DOWNLOAD_STATUS_UPDATE_INTERVAL, RSS_CHAT_ID, bot, rss_session, \
-                AUTO_DELETE_UPLOAD_MESSAGE_DURATION, PICS
+                AUTO_DELETE_UPLOAD_MESSAGE_DURATION, PICS, app
 from bot.helper.ext_utils.bot_utils import get_readable_message, setInterval
 
 
@@ -121,13 +121,13 @@ def deleteMessage(bot, message: Message):
         LOGGER.error(str(e))
 
 def sendLogFile(bot, message: Message):
-    bot.send_document(document='log.txt', thumb='Thumbnails/weeb.jpg',
+    app.send_document(document='log.txt', thumb='Thumbnails/weeb.jpg',
                           reply_to_message_id=message.message_id,
                           chat_id=message.chat_id, caption='log.txt')
 
 def sendFile(bot, message: Message, name: str, caption=""):
     try:
-        bot.send_document(document=name, reply_to_message_id=message.message_id,
+        app.send_document(document=name, reply_to_message_id=message.message_id,
                              caption=caption, parse_mode='HTML',chat_id=message.chat_id,
                              thumb='Thumbnails/weeb.jpg')
         remove(name)
