44503fb3efb79800ebdaec89dcfbc761d9d9f908
Karan Adhikari <doc.adhikari@gmail.com>
2022-11-18 17:43:06 +0530

    Update scraper.py

--- bot/modules/scraper.py
+++ bot/modules/scraper.py
@@ -95,6 +95,19 @@ def scrapper(update, context):
                 if len(prsd) > 4000:
                     sent = sendMessage("<i>Scrapping More...</i>", context.bot, update.message)
                     prsd = ""
+    elif "telguflix" in link:
+        client = requests.session()
+        r = client.get(link).text
+        y = r.split('id="download"')[-1]
+        f = y.split("</ul>") [0]
+        soup = BeautifulSoup (f, "html.parser")
+        for a in soup.find_all("a"):
+             c = a.get("href")
+             t = client.get(c).text
+             soupt = BeautifulSoup(t, "html.parser")
+             title = soupt.title
+             gd_txt = f"<code>{(title.text).replace('GDToT | ' , '')}</code>\n{c}\n\n"
+             sendMessage(gd_txt, context.bot, update.message)
     elif "cinevood" in link:
         prsd = ""
         links = []
