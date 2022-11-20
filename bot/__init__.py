f0b43f82528377da8a318353f06ab1cb718c1908
Karan Adhikari <doc.adhikari@gmail.com>
2022-11-20 20:55:52 +0530

    fix

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,6 +1,6 @@
 import re
 from PIL import Image
-from os import environ, remove
+from os import environ, remove, path as ospath, mkdir
 from logging import getLogger, FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info, warning as log_warning
 from socket import setdefaulttimeout
 from urllib.request import urlretrieve
