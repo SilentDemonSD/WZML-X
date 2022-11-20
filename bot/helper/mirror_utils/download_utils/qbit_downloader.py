f6b10955d1b188eadd97fc40d5add3a1d482fb43
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2022-11-20 11:00:37 +0530

    Update qbit_downloader.py

--- bot/helper/mirror_utils/download_utils/qbit_downloader.py
+++ bot/helper/mirror_utils/download_utils/qbit_downloader.py
@@ -10,7 +10,7 @@ from re import search as re_search
 from threading import Lock, Thread
 
 from bot import TELEGRAPH_STYLE, download_dict, download_dict_lock, BASE_URL, get_client, TORRENT_DIRECT_LIMIT, ZIP_UNZIP_LIMIT, STOP_DUPLICATE, TORRENT_TIMEOUT, LOGGER, STORAGE_THRESHOLD, LEECH_LIMIT, \
-                OWNER_ID, PAID_SERVICE, QbInterval
+                OWNER_ID, PAID_SERVICE, QbInterval, user_data
 from bot.helper.mirror_utils.status_utils.qbit_download_status import QbDownloadStatus
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot.helper.telegram_helper.message_utils import sendMessage, sendMarkup, deleteMessage, sendStatusMessage, update_all_messages, sendFile
