bb59b262f66d70e610a686438760db390fede0e9
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2022-11-21 18:37:33 +0530

    Update db_handler.py

--- bot/helper/ext_utils/db_handler.py
+++ bot/helper/ext_utils/db_handler.py
@@ -66,14 +66,14 @@ class DbManger:
             image_bin = False
         self.__db.users.update_one({'_id': user_id}, {'$set': {'thumb': image_bin}}, upsert=True)
 
-    def update_prefix(self, user_id, value=None):
+    def update_userval(self, user_id, data, value=None):
         if self.__err:
             return
         if value is not None:
             dbval = value
         else:
             dbval = False
-        self.__db.users.update_one({'_id': user_id}, {'$set': {'prefix': dbval}}, upsert=True)
+        self.__db.users.update_one({'_id': user_id}, {'$set': {data: dbval}}, upsert=True)
 
     def rss_update(self, title):
         if self.__err:
@@ -179,101 +179,6 @@ if DB_URI is not None:
 #             self.disconnect()
 #             return 'Successfully removed from Sudo'
 
-#     def user_media(self, user_id: int):
-#         if self.err:
-#             return
-#         elif not self.user_check(user_id):
-#             sql = 'INSERT INTO users (uid, media) VALUES ({}, TRUE)'.format(user_id)
-#         else:
-#             sql = 'UPDATE users SET media = TRUE, doc = FALSE WHERE uid = {}'.format(user_id)
-#         self.cur.execute(sql)
-#         self.conn.commit()
-#         self.disconnect()
-
-#     def user_doc(self, user_id: int):
-#         if self.err:
-#             return
-#         elif not self.user_check(user_id):
-#             sql = 'INSERT INTO users (uid, doc) VALUES ({}, TRUE)'.format(user_id)
-#         else:
-#             sql = 'UPDATE users SET media = FALSE, doc = TRUE WHERE uid = {}'.format(user_id)
-#         self.cur.execute(sql)
-#         self.conn.commit()
-#         self.disconnect()
-
-#     def user_pre(self, user_id: int, user_pre):
-#         if self.err:
-#             return
-#         elif not self.user_check(user_id):
-#             sql = 'INSERT INTO users (pre, uid) VALUES (%s, %s)'
-#         else:
-#             sql = 'UPDATE users SET pre = %s WHERE uid = %s'
-#         self.cur.execute(sql, (user_pre, user_id))
-#         self.conn.commit()
-#         self.disconnect()
-        
-
-#     def user_cfont(self, user_id: int, user_cfont):
-#         if self.err:
-#             return
-#         elif not self.user_check(user_id):
-#             sql = 'INSERT INTO users (cfont, uid) VALUES (%s, %s)'
-#         else:
-#             sql = 'UPDATE users SET cfont = %s WHERE uid = %s'
-#         self.cur.execute(sql, (user_cfont, user_id))
-#         self.conn.commit()
-#         self.disconnect()
-
-        
-        
-#     def user_suf(self, user_id: int, user_suf):
-#         if self.err:
-#             return
-#         elif not self.user_check(user_id):
-#             sql = 'INSERT INTO users (suf, uid) VALUES (%s, %s)'
-#         else:
-#             sql = 'UPDATE users SET suf = %s WHERE uid = %s'
-#         self.cur.execute(sql, (user_suf, user_id))
-#         self.conn.commit()
-#         self.disconnect()
-
-
-
-#     def user_cap(self, user_id: int, user_cap):
-#         if self.err:
-#             return
-#         elif not self.user_check(user_id):
-#             sql = 'INSERT INTO users (cap, uid) VALUES (%s, %s)'
-#         else:
-#             sql = 'UPDATE users SET cap = %s WHERE uid = %s'
-#         self.cur.execute(sql, (user_cap, user_id))
-#         self.conn.commit()
-#         self.disconnect()  
-
-
-#     def user_dump(self, user_id: int, user_dump):
-#         if self.err:
-#             return
-#         elif not self.user_check(user_id):
-#             sql = 'INSERT INTO users (dump, uid) VALUES (%s, %s)'
-#         else:
-#             sql = 'UPDATE users SET dump = %s WHERE uid = %s'
-#         self.cur.execute(sql, (user_dump, user_id))
-#         self.conn.commit()
-#         self.disconnect()
-
-
-#     def user_rem(self, user_id: int, user_rem):
-#         if self.err:
-#             return
-#         elif not self.user_check(user_id):
-#             sql = 'INSERT INTO users (rem, uid) VALUES (%s, %s)'
-#         else:
-#             sql = 'UPDATE users SET rem = %s WHERE uid = %s'
-#         self.cur.execute(sql, (user_rem, user_id))
-#         self.conn.commit()
-#         self.disconnect()
-
 #     def user_addpaid(self, user_id: int):
 #         if self.err:
 #             return "Error in DB connection, check log for details"
@@ -295,29 +200,6 @@ if DB_URI is not None:
 #             self.conn.commit()
 #             self.disconnect()
 #             return 'Successfully removed from Paid Membership'
-
-
-#     def user_save_thumb(self, user_id: int, path):
-#         if self.err:
-#             return
-#         image = open(path, 'rb+')
-#         image_bin = image.read()
-#         if not self.user_check(user_id):
-#             sql = 'INSERT INTO users (thumb, uid) VALUES (%s, %s)'
-#         else:
-#             sql = 'UPDATE users SET thumb = %s WHERE uid = %s'
-#         self.cur.execute(sql, (image_bin, user_id))
-#         self.conn.commit()
-#         self.disconnect()
-
-#     def user_rm_thumb(self, user_id: int, path):
-#         if self.err:
-#             return
-#         elif self.user_check(user_id):
-#             sql = 'UPDATE users SET thumb = NULL WHERE uid = {}'.format(user_id)
-#         self.cur.execute(sql)
-#         self.conn.commit()
-#         self.disconnect()
 
 #     def addleech_log(self, chat_id: int):
 #         if self.err:
@@ -345,74 +227,3 @@ if DB_URI is not None:
 #         self.cur.execute("SELECT * FROM users WHERE uid = {}".format(uid))
 #         res = self.cur.fetchone()
 #         return res
-
-#     def rss_add(self, name, link, last, title, filters):
-#         if self.err:
-#             return
-#         q = (name, link, last, title, filters)
-#         self.cur.execute("INSERT INTO rss (name, link, last, title, filters) VALUES (%s, %s, %s, %s, %s)", q)
-#         self.conn.commit()
-#         self.disconnect()
-
-#     def rss_update(self, name, last, title):
-#         if self.err:
-#             return
-#         q = (last, title, name)
-#         self.cur.execute("UPDATE rss SET last = %s, title = %s WHERE name = %s", q)
-#         self.conn.commit()
-#         self.disconnect()
-
-#     def rss_delete(self, name):
-#         if self.err:
-#             return
-#         self.cur.execute("DELETE FROM rss WHERE name = %s", (name,))
-#         self.conn.commit()
-#         self.disconnect()
-
-#     def add_incomplete_task(self, cid: int, link: str, tag: str):
-#         if self.err:
-#             return
-#         q = (cid, link, tag)
-#         self.cur.execute("INSERT INTO {} (cid, link, tag) VALUES (%s, %s, %s)".format(botname), q)
-#         self.conn.commit()
-#         self.disconnect()
-
-#     def rm_complete_task(self, link: str):
-#         if self.err:
-#             return
-#         self.cur.execute("DELETE FROM {} WHERE link = %s".format(botname), (link,))
-#         self.conn.commit()
-#         self.disconnect()
-
-#     def get_incomplete_tasks(self):
-#         if self.err:
-#             return False
-#         self.cur.execute("SELECT * from {}".format(botname))
-#         rows = self.cur.fetchall()  # return a list ==> (cid, link, tag)
-#         notifier_dict = {}
-#         if rows:
-#             for row in rows:
-#                 if row[0] in list(notifier_dict.keys()):
-#                     if row[2] in list(notifier_dict[row[0]].keys()):
-#                         notifier_dict[row[0]][row[2]].append(row[1])
-#                     else:
-#                         notifier_dict[row[0]][row[2]] = [row[1]]
-#                 else:
-#                     usr_dict = {}
-#                     usr_dict[row[2]] = [row[1]]
-#                     notifier_dict[row[0]] = usr_dict
-#         self.cur.execute("TRUNCATE TABLE {}".format(botname))
-#         self.conn.commit()
-#         self.disconnect()
-#         return notifier_dict # return a dict ==> {cid: {tag: [mid, mid, ...]}}
-
-
-#     def trunc_table(self, name):
-#         if self.err:
-#             return
-#         self.cur.execute("TRUNCATE TABLE {}".format(name))
-#         self.conn.commit()
-#         self.disconnect()
-
-# if DB_URI is not None:
-#     DbManger().db_init()
