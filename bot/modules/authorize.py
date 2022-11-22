f999a6ffb431c78e95af7a2913085a6a436fc140
Karan Adhikari <doc.adhikari@gmail.com>
2022-11-22 12:13:08 +0530

    sync upto complete usersetting https://github.com/anasty17/mirror-leech-telegram-bot/commit/7c9bcfaf97aa74d2b1ce2bf94f817471884c8eb3

--- bot/modules/authorize.py
+++ bot/modules/authorize.py
@@ -9,7 +9,7 @@ from bot.helper.ext_utils.bot_utils import update_user_ldata
 def authorize(update, context):
     reply_message = update.message.reply_to_message
     if len(context.args) == 1:
-        id_ = context.args[0]
+        id_ = int(context.args[0])
     elif reply_message:
         id_ = reply_message.from_user.id
     else:
@@ -26,7 +26,7 @@ def authorize(update, context):
 def unauthorize(update, context):
     reply_message = update.message.reply_to_message
     if len(context.args) == 1:
-        id_ = context.args[0]
+        id_ = int(context.args[0])
     elif reply_message:
         id_ = reply_message.from_user.id
     else:
@@ -78,7 +78,7 @@ def removeSudo(update, context):
 def addleechlog(update, context):
     reply_message = update.message.reply_to_message
     if len(context.args) == 1:
-        id_ = context.args[0]
+        id_ = int(context.args[0])
     elif reply_message:
         id_ = reply_message.from_user.id
     else:
@@ -95,7 +95,7 @@ def addleechlog(update, context):
 def rmleechlog(update, context):
     reply_message = update.message.reply_to_message
     if len(context.args) == 1:
-        id_ = context.args[0]
+        id_ = int(context.args[0])
     elif reply_message:
         id_ = reply_message.from_user.id
     else:
