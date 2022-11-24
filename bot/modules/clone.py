3d79760d8cdf8d090e5050ae5d217219f0f2b0f9
Karan Adhikari <doc.adhikari@gmail.com>
2022-11-24 20:57:48 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -316,7 +316,6 @@ def _clone(message, bot):
         if button in ["cancelled", ""]:
             sendMessage(f"{tag} {result}", bot, message)
         else:
-            sendMarkup(result + cc, bot, message, button)
             LOGGER.info(f'Cloning Done: {name}')
             if config_dict['FORCE_BOT_PM'] == '':
                 if PICS:
