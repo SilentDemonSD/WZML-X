08860ea7e361bbcc6f70785e44b9ee204598d9b1
Rajveer0916 <94645722+Rajveer0916@users.noreply.github.com>
2022-11-24 17:03:16 +0530

    Update mirror_leech.py

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -95,6 +95,8 @@ def _mirror_leech(bot, message, isZip=False, extract=False, isQbit=False, isLeec
     is_gdtot = False
     is_unified = False
     is_udrive = False
+    is_sharer = False
+    is_sharedrive = False
     index = 1
     ratio = None
     seed_time = None
@@ -223,6 +225,8 @@ def _mirror_leech(bot, message, isZip=False, extract=False, isQbit=False, isLeec
                 is_gdtot = is_gdtot_link(link)
                 is_unified = is_unified_link(link)
                 is_udrive = is_udrive_link(link)
+                is_sharer = is_sharer_link(link)
+                is_sharedrive = is_sharedrive_link(link)
                 link = direct_link_generator(link)
                 LOGGER.info(f"Generated link: {link}")
             except DirectDownloadLinkException as e:
@@ -280,7 +284,7 @@ def _mirror_leech(bot, message, isZip=False, extract=False, isQbit=False, isLeec
             gmsg += f"Use /{BotCommands.UnzipMirrorCommand} to extracts Google Drive archive file"
             sendMessage(gmsg, bot, message)
         else:
-            Thread(target=add_gd_download, args=(link, f'{DOWNLOAD_DIR}{listener.uid}', listener, is_gdtot, is_unified, is_udrive, name)).start()
+            Thread(target=add_gd_download, args=(link, f'{DOWNLOAD_DIR}{listener.uid}', listener, is_gdtot, is_unified, is_udrive, is_sharer, is_sharedrive, name)).start()
     elif is_mega_link(link):
         Thread(target=add_mega_download, args=(link, f'{DOWNLOAD_DIR}{listener.uid}/', listener, name)).start()
     elif isQbit and (is_magnet(link) or ospath.exists(link)):
