009a7e82fd9f8e0e67c6141cff65c06541a6627c
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2022-11-29 12:11:32 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -116,12 +116,13 @@ def sendPhoto(text, bot, message, photo, reply_markup=None):
 
 def editPhoto(text, message, photo, reply_markup=None):
     try:
-        return bot.edit_message_media(media=InputMediaPhoto(media=photo, caption=text, parse_mode='html'), chat_id=message.chat.id, message_id=message.message_id,
+        LOGGER.info(photo, text)
+        return bot.edit_message_media(media=InputMediaPhoto(media=str(photo), caption=text, parse_mode='html'), chat_id=message.chat.id, message_id=message.message_id,
                                       reply_markup=reply_markup)
     except RetryAfter as r:
         LOGGER.warning(str(r))
         sleep(r.retry_after * 1.5)
-        return editPhoto(text, bot, message, photo, reply_markup)
+        return editPhoto(text, message, photo, reply_markup)
     except Exception as e:
         LOGGER.error(str(e))
         return
