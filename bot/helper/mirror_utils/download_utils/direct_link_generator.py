72404c8de72e3e43e8db4f8426c25c5457622d60
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2022-12-03 07:15:11 +0530

    Add Ouo

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -86,6 +86,8 @@ def direct_link_generator(link: str):
         return try2link(link)
     elif 'ez4short.com' in link:
         return ez4(link)
+    elif 'ouo.io' in link or 'ouo.press' in link:
+        return ouo(link)
     elif is_gdtot_link(link):
         return gdtot(link)
     elif is_unified_link(link):
@@ -159,7 +161,47 @@ def ez4(url):
     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
     try:
         return r.json()['url']
-    except: return "Something went wrong :("        
+    except: return "Something went wrong :("
+
+ANCHOR_URL = 'https://www.google.com/recaptcha/api2/anchor?ar=1&k=6Lcr1ncUAAAAAH3cghg6cOTPGARa8adOf-y9zv2x&co=aHR0cHM6Ly9vdW8uaW86NDQz&hl=en&v=1B_yv3CBEV10KtI2HJ6eEXhJ&size=invisible&cb=4xnsug1vufyr'
+
+def RecaptchaV3(ANCHOR_URL):
+    url_base = 'https://www.google.com/recaptcha/'
+    post_data = "v={}&reason=q&c={}&k={}&co={}"
+    client = requests.Session()
+    client.headers.update({
+	'content-type': 'application/x-www-form-urlencoded'
+    })
+    matches = re.findall('([api2|enterprise]+)\/anchor\?(.*)', ANCHOR_URL)[0]
+    url_base += matches[0]+'/'
+    params = matches[1]
+    res = client.get(url_base+'anchor', params=params)
+    token = re.findall(r'"recaptcha-token" value="(.*?)"', res.text)[0]
+    params = dict(pair.split('=') for pair in params.split('&'))
+    post_data = post_data.format(params["v"], token, params["k"], params["co"])
+    res = client.post(url_base+'reload', params=f'k={params["k"]}', data=post_data)
+    answer = re.findall(r'"rresp","(.*?)"', res.text)[0]    
+    return answer
+
+def ouo(url):
+    client = requests.Session()
+    tempurl = url.replace("ouo.press", "ouo.io")
+    p = urlparse(tempurl)
+    id = tempurl.split('/')[-1]
+    res = client.get(tempurl)
+    next_url = f"{p.scheme}://{p.hostname}/go/{id}"
+    for _ in range(2):
+        if res.headers.get('Location'):
+            break
+	bs4 = BeautifulSoup(res.content, 'html.parser')
+	inputs = bs4.form.findAll("input", {"name": re.compile(r"token$")})
+	data = { input.get('name'): input.get('value') for input in inputs }        
+	ans = RecaptchaV3(ANCHOR_URL)
+        data['x-token'] = ans    
+	h = {'content-type': 'application/x-www-form-urlencoded'}        
+	res = client.post(next_url, data=data, headers=h, allow_redirects=False)
+	next_url = f"{p.scheme}://{p.hostname}/xreallcygo/{id}"
+    return res.headers.get('Location')        
         
 def zippy_share(url: str) -> str:
     base_url = re_search('http.+.zippyshare.com', url).group()
