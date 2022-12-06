513b6ffa5e15a013db3e1c0c06265ee3bc6778f7
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2022-12-06 19:22:00 +0530

    Update users_settings.py

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -60,20 +60,20 @@ def get_user_settings(from_user, key=None):
             thumbmsg = "Not Exists"
             buttons.sbutton("Set Thumbnail", f"userset {user_id} sthumb")
         buttxt = "Change/Delete UserLog" if userlog != "Not Exists" else "Set UserLog"
-        buttons.sbutton(buttxt, f"userset {user_id} suniversal userlog")
+        buttons.sbutton(buttxt, f"userset {user_id} suniversal userlog universal")
 
         imdbval, anival = "", ''
         if imdb != "Not Exists":
             imdbval = "Exists"
-            buttons.sbutton("Change/Delete IMDB", f"userset {user_id} suniversal imdb_temp")
+            buttons.sbutton("Change/Delete IMDB", f"userset {user_id} suniversal imdb_temp universal")
             buttons.sbutton("Show IMDB Template", f"userset {user_id} showimdb")
-        else: buttons.sbutton("Set IMDB", f"userset {user_id} suniversal imdb_temp")
+        else: buttons.sbutton("Set IMDB", f"userset {user_id} suniversal imdb_temp universal")
         if anilist != "Not Exists":
             anival = "Exists"
-            buttons.sbutton("Change/Delete AniList", f"userset {user_id} suniversal ani_temp")
+            buttons.sbutton("Change/Delete AniList", f"userset {user_id} suniversal ani_temp universal")
             buttons.sbutton("Show AniList Template", f"userset {user_id} showanilist")
         else:
-            buttons.sbutton("Set AniList", f"userset {user_id} suniversal ani_temp")
+            buttons.sbutton("Set AniList", f"userset {user_id} suniversal ani_temp universal")
         buttons.sbutton("Back", f"userset {user_id} mback")
         buttons.sbutton("Close", f"userset {user_id} close")
         button = buttons.build_menu(2)
@@ -93,11 +93,11 @@ def get_user_settings(from_user, key=None):
         remname = user_dict['remname'] if user_dict and user_dict.get('mremname') else "Not Exists"
 
         buttxt = "Change/Delete Prefix" if prefix != "Not Exists" else "Set Prefix"
-        buttons.sbutton(buttxt, f"userset {user_id} suniversal mprefix")
+        buttons.sbutton(buttxt, f"userset {user_id} suniversal mprefix mirror")
         buttxt = "Change/Delete Suffix" if suffix != "Not Exists" else "Set Suffix"
-        buttons.sbutton(buttxt, f"userset {user_id} suniversal msuffix")
+        buttons.sbutton(buttxt, f"userset {user_id} suniversal msuffix mirror")
         buttxt = "Change/Delete Remname" if remname != "Not Exists" else "Set Remname"
-        buttons.sbutton(buttxt, f"userset {user_id} suniversal mremname")
+        buttons.sbutton(buttxt, f"userset {user_id} suniversal mremname mirror")
 
         buttons.sbutton("Back", f"userset {user_id} mback")
         buttons.sbutton("Close", f"userset {user_id} close")
@@ -323,7 +323,7 @@ Check all available qualities options <a href="https://github.com/yt-dlp/yt-dlp#
             buttons.sbutton("Set Font Style", f"userset {user_id} font")
         if user_id in user_data and user_data[user_id].get(data[3]):
             menu = True
-            buttons.sbutton("Delete", f"userset {user_id} {data[3]}")
+            buttons.sbutton("Delete", f"userset {user_id} sremove {data[3]} {data[4]}")
         buttons.sbutton("Back", f"userset {user_id} back")
         buttons.sbutton("Close", f"userset {user_id} close")
         editMessage(f'<u>Send {data[3].capitalize()} text :</u>\n\nExamples:\n{example_dict[data[3]]}', message, buttons.build_menu(2) if menu else buttons.build_menu(1))
@@ -337,62 +337,13 @@ Check all available qualities options <a href="https://github.com/yt-dlp/yt-dlp#
                 handler_dict[user_id] = False
                 update_user_settings(message, query.from_user, data[4])
         dispatcher.remove_handler(UNI_HANDLER)
-    elif data[2] == "prefix":
+    elif data[2] == "sremove":
         handler_dict[user_id] = False
-        update_user_ldata(user_id, 'prefix', False)
+        update_user_ldata(user_id, data[3], False)
         if DATABASE_URL: 
             DbManger().update_userval(user_id, 'prefix')
-        query.answer(text="Your Prefix is Successfully Deleted!", show_alert=True)
-        update_user_settings(message, query.from_user)
-    elif data[2] == "suffix":
-        handler_dict[user_id] = False
-        update_user_ldata(user_id, 'suffix', False)
-        if DATABASE_URL: 
-            DbManger().update_userval(user_id, 'suffix')
-        query.answer(text="Your Suffix is Successfully Deleted!", show_alert=True)
-        update_user_settings(message, query.from_user)
-    elif data[2] == "caption":
-        handler_dict[user_id] = False
-        update_user_ldata(user_id, 'caption', False)
-        if DATABASE_URL: 
-            DbManger().update_userval(user_id, 'caption')
-        query.answer(text="Your Caption is Successfully Deleted!", show_alert=True)
-        update_user_settings(message, query.from_user)
-    elif data[2] == "remname":
-        handler_dict[user_id] = False
-        update_user_ldata(user_id, 'remname', False)
-        if DATABASE_URL: 
-            DbManger().update_userval(user_id, 'remname')
-        query.answer(text="Your Remname is Successfully Deleted!", show_alert=True)
-        update_user_settings(message, query.from_user)
-    elif data[2] == "userlog":
-        handler_dict[user_id] = False
-        update_user_ldata(user_id, 'userlog', False)
-        if DATABASE_URL: 
-            DbManger().update_userval(user_id, 'userlog')
-        query.answer(text="Your UserLog is Successfully Deleted!", show_alert=True)
-        update_user_settings(message, query.from_user)
-    elif data[2] == "cfont":
-        handler_dict[user_id] = False
-        update_user_ldata(user_id, 'cfont', False)
-        if DATABASE_URL: 
-            DbManger().update_userval(user_id, 'cfont')
-        query.answer(text="Your Caption Font is Successfully Deleted!", show_alert=True)
-        update_user_settings(message, query.from_user)
-    elif data[2] == "imdb_temp":
-        handler_dict[user_id] = False
-        update_user_ldata(user_id, 'imdb_temp', False)
-        if DATABASE_URL:
-            DbManger().update_userval(user_id, 'imdb_temp')
-        query.answer(text="Your IMDB Template is Successfully Deleted!", show_alert=True)
-        update_user_settings(message, query.from_user)
-    elif data[2] == "ani_temp":
-        handler_dict[user_id] = False
-        update_user_ldata(user_id, 'ani_temp', False)
-        if DATABASE_URL:
-            DbManger().update_userval(user_id, 'ani_temp')
-        query.answer(text="Your AniList Template is Successfully Deleted!", show_alert=True)
-        update_user_settings(message, query.from_user)
+        query.answer(text=f"Your {data[3].capitalize()} is Successfully Deleted!", show_alert=True)
+        update_user_settings(message, query.from_user, data[4])
     elif data[2] == "font":
         handler_dict[user_id] = False
         FONT_SPELL = {'b':'<b>Bold</b>', 'i':'<i>Italics</i>', 'code':'<code>Monospace</code>', 's':'<s>Strike</s>', 'u':'<u>Underline</u>', 'tg-spoiler':'<tg-spoiler>Spoiler</tg-spoiler>'}
