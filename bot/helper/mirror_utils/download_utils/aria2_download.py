c02471fd665488358b32124137f86f3656fa4d3d
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2022-12-10 21:07:30 +0530

    Update aria2_download.py

--- bot/helper/mirror_utils/download_utils/aria2_download.py
+++ bot/helper/mirror_utils/download_utils/aria2_download.py
@@ -222,7 +222,7 @@ def add_aria2c_download(link: str, path, listener, filename, auth, ratio, seed_t
         download = aria2.add_magnet(link, args)
     elif match(r'https?://.+\/\d+\:\/', link) and link[-1] == '/':
         link = indexScrape({"page_token": "", "page_index": 0}, link, "none", "none", folder_mode=True)
-        download = aria2.add_uris(link, args)
+        download = aria2.add_uris([link], args)
     else:
         download = aria2.add_uris([link], args)
     if download.error_message:
