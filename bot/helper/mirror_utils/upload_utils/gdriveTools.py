ec34614da4c20399426563d6c5afa649150fb217
Maverick <84721324+MajnuRangeela@users.noreply.github.com>
2022-12-10 19:36:40 +0530

    Fixed some more bools

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -388,7 +388,7 @@ class GoogleDriveHelper:
         IndexURL = ''
         IS_GDX = user_data[user_id].get('is_gdx').lower if user_id in user_data and user_data[user_id].get('is_gdx') else 'false'
         IS_PAID = is_paid(user_id)
-        if (config_dict['PAID_SERVICE'].lower() != 'true' and IS_GDX == 'true') or (config_dict['PAID_SERVICE'].lower() == 'true' and IS_PAID == True and IS_GDX.lower() == 'true'):
+        if (config_dict['PAID_SERVICE'] != True and IS_GDX == 'true') or (config_dict['PAID_SERVICE'] == True and IS_PAID == True and IS_GDX == 'true'):
             userDest = user_data[user_id].get('gdx') if user_id_ in user_data and user_data[user_id].get('gdx') else ''
             if len(userDest) != 0:
                 arrForUser = userDest.split()
