eb7bd82567ad868be7ee2e1f780372f69cf33f72
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2022-12-12 14:02:30 +0530

    Update mirror_leech.py

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -73,7 +73,7 @@ def _mirror_leech(bot, message, isZip=False, extract=False, isQbit=False, isLeec
     USER_TASKS_LIMIT = config_dict['USER_TASKS_LIMIT']
     TOTAL_TASKS_LIMIT = config_dict['TOTAL_TASKS_LIMIT']
     if config_dict['DAILY_TASK_LIMIT'] and user_id != OWNER_ID and not is_sudo(user_id) and not is_paid(user_id) and config_dict['DAILY_TASK_LIMIT'] >= getdailytasks(user_id):
-        msg = f"<b>Daily Total Task Limit : {config_dict['DAILY_TASK_LIMIT']} \nYou have exhausted all your Daily Task Limits\n#Daily_task_limit_exceed</b>
+        msg = f"<b>Daily Total Task Limit : {config_dict['DAILY_TASK_LIMIT']} \nYou have exhausted all your Daily Task Limits\n#Daily_task_limit_exceed</b>"
         if config_dict['PAID_SERVICE'] is True: msg += "\n#Buy Paid Service"
         return sendMessage(msg, bot ,message)
     else: ttask = getdailytasks(user_id, increase_task=True); LOGGER.info(f"User : {user_id} Daily Tasks : {ttask}")
