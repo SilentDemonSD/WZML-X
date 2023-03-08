b288ee63bd588f4ab7c4bfe009aa748c581191ba
Tharindu Prabath <116081100+tharindu899@users.noreply.github.com>
2023-03-08 13:51:33 +0530

    update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -16,7 +16,7 @@ def common_btn(isRecur, msg_id):
     buttons.sbutton("Folders", f"types folders {msg_id}")
     buttons.sbutton("Files", f"types files {msg_id}")
     buttons.sbutton("Both", f"types both {msg_id}")
-    buttons.sbutton(f"Recursive {'🔅' if isRecur else ''}", f"types recur {msg_id}")
+    buttons.sbutton(f"Recursive {'💠' if isRecur else ''}", f"types recur {msg_id}")
     buttons.sbutton("Cancel", f"types cancel {msg_id}")
     return buttons.build_menu(3)
 
