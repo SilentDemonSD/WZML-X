61ef5fc83671d0b3d43e409e11144999be5bebea
Tharindu Prabath <116081100+tharindu899@users.noreply.github.com>
2023-03-08 13:37:41 +0530

    update bot_settings.py

--- bot/modules/bot_settings.py
+++ bot/modules/bot_settings.py
@@ -944,7 +944,7 @@ def edit_variable(update, context, omsg, key):
             DRIVES_IDS[0] = value
         else:
             DRIVES_IDS.insert(0, value)
-        if CATEGORY_NAMES and CATEGORY_NAMES[0] == 'Root':
+        if CATEGORY_NAMES and CATEGORY_NAMES[0] == '😁Root':
             CATEGORY_IDS[0] = value
         else:
             CATEGORY_IDS.insert(0, value)
@@ -954,7 +954,7 @@ def edit_variable(update, context, omsg, key):
             INDEX_URLS[0] = value
         else:
             INDEX_URLS.insert(0, value)
-        if CATEGORY_NAMES and CATEGORY_NAMES[0] == 'Root':
+        if CATEGORY_NAMES and CATEGORY_NAMES[0] == '😅Root':
             CATEGORY_INDEX[0] = value
         else:
             CATEGORY_INDEX.insert(0, value)
