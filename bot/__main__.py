9277aae4b31182555bae85ceb751953191a6b439
Abdullah Al Noman <101085500+AtrociousMirrorBot@users.noreply.github.com>
2023-05-01 12:32:33 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -26,7 +26,7 @@ from .modules import authorize, list, cancel_mirror, mirror_status, mirror_leech
                      delete, count, users_settings, search, rss, wayback, speedtest, anilist, imdb, bt_select, mediainfo, hash, \
                      scraper, pictures, save_msg, sel_cat, broadcast, drive_clean, users
 
-version = "5.0.0"
+version = "5.0.1"
 
 def progress_bar(percentage):
     p_used = config_dict['FINISHED_PROGRESS_STR']
