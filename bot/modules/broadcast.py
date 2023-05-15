7097eab8aa722d019292ed7194eda01cc9a29c88
Abdullah Al Noman <101085500+AtrociousMirrorBot@users.noreply.github.com>
2023-05-15 22:56:16 +0600

    Update broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -5,7 +5,6 @@ from bot import bot, dispatcher, LOGGER, config_dict
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import sendMessage
 
-
 def broadcast(update, context):
     reply_to = update.message.reply_to_message
 
@@ -18,9 +17,8 @@ def broadcast(update, context):
         users_count = db.users.count_documents({})
     
         chat_ids = [str(user["_id"]) for user in users_collection.find({}, {"_id": 1})]
-
         success = 0
-
+        
         for chat_id in chat_ids:
             try:
                 context.bot.copy_message(chat_id=chat_id, from_chat_id=update.message.chat.id, message_id=reply_to.message_id)
@@ -28,13 +26,11 @@ def broadcast(update, context):
             except Exception as err:
                 LOGGER.error(err)
 
-
         msg = f"<b>Broadcasting Completed</b>\n"
         msg += f"<b>Total {users_count} users in Database</b>\n"
         msg += f"<b>Sucess: </b>{success} users\n"
         msg += f"<b>Failed: </b>{users_count - success} users"
         return sendMessage(msg, context.bot, update.message) 
 
-
-CommandHandler("broadcast", broadcast, filters=CustomFilters.owner_filter)
-dispatcher.add_handler(CommandHandler('broadcast', broadcast))
+broadcast_handler = CommandHandler("broadcast", broadcast, filters=CustomFilters.owner_filter)
+dispatcher.add_handler(broadcast_handler)
