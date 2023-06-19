483289abc4d11903be5935e2424905aa4eccc83e
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2023-06-19 14:02:44 +0530

    Add DDL

--- bot/modules/mediainfo.py
+++ bot/modules/mediainfo.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import aiohttp
+from re import match as re_match
 from shlex import split as ssplit
 from aiofiles import open as aiopen
 from aiofiles.os import remove as aioremove, path as aiopath, mkdir
@@ -42,13 +44,44 @@ async def telegram_mediainfo(message, media, mmsg):
     link_id = (await telegraph.create_page(title='MediaInfo', content=tele_content))["path"]
     await editMessage(temp_send, f"<b>MediaInfo:</b> https://graph.org/{link_id}")
 
+async def ddl_mediainfo(link):
+    temp_send = await sendMessage(message, '<i>Generating MediaInfo...</i>')
+    try:
+        path = "Mediainfo/"
+        if not await aiopath.isdir(path):
+            await mkdir(path)
+        filename = re_search(".+/(.+)", link).group(1)
+        if len(filename) > 60:
+            filename = filename[-60:]
+        des_path = ospath.join(path, filename)
+        headers = {"user-agent":"Mozilla/5.0 (Linux; Android 12; 2201116PI) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"}
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link, headers=headers) as response:
+                async with aiopen(des_path, "wb") as f:
+                    async for chunk in response.content.iter_any(10000000):
+                        await f.write(chunk)
+                        break
+        stdout, stderr, _ = await cmd_exec(ssplit(f'mediainfo "{des_path}"'))
+        tele_content = f"<h4>{ospath.basename(des_path)}</h4><br><br>"
+        if len(stdout) != 0:
+            tele_content += f"<br><br><pre>{stdout}</pre><br>"
+        if len(stderr) != 0:
+            tele_content += f"<br><br><pre>{stderr}</pre>"
+    except Exception as e:
+        LOGGER.error(e)
+        await editMessage(temp_send, f"MediaInfo Stopped due to {str(e)}")
+    finally:
+        await aioremove(des_path)
+    link_id = (await telegraph.create_page(title='MediaInfo', content=tele_content))["path"]
+    await editMessage(temp_send, f"<b>MediaInfo:</b> https://graph.org/{link_id}")
+
 
 async def mediainfo(_, message):
     help_msg = "\n<b>By replying to message (including media):</b>"
     help_msg += f"\n<code>/{BotCommands.MediaInfoCommand}" + " {message}" + "</code>"
     if len(message.command) > 2:
         link = message.command[1]
-        #Handle DDL or GDrive Links
+        return await ddl_mediainfo(link)
     elif (mediamessage := message.reply_to_message) and not mediamessage.text:
         file = next((i for i in [mediamessage.document, mediamessage.video, mediamessage.audio, mediamessage.photo, mediamessage.voice,
                          mediamessage.animation, mediamessage.video_note] if i is not None), None)
