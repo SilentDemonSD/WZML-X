57ba30120e647f84a1e746d51573e0d9a71cb3f2
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2023-06-22 23:32:37 +0530

    Shift It

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -1,4 +1,5 @@
 import hashlib
+from re import sub as re_sub
 from shlex import split as ssplit
 from os import path as ospath
 from aiofiles.os import remove as aioremove, path as aiopath, mkdir
@@ -8,7 +9,7 @@ from asyncio import create_subprocess_exec
 from asyncio.subprocess import PIPE
 
 from bot import LOGGER, MAX_SPLIT_SIZE, config_dict, user_data
-from bot.helper.ext_utils.bot_utils import cmd_exec, sync_to_async
+from bot.helper.ext_utils.bot_utils import cmd_exec, sync_to_async, get_readable_file_size
 from bot.helper.ext_utils.fs_utils import ARCH_EXT, get_mime_type
 from bot.helper.ext_utils.telegraph_helper import telegraph
 
@@ -188,7 +189,72 @@ async def split_file(path, size, file_, dirpath, split_size, listener, start_tim
             LOGGER.error(err)
     return True
 
+async def format_filename(file_, lprefix, lsuffix, lremname, lcaption, dirpath):
+    prefile_ = file_
+    # SD-Style V2 ~ WZML-X
+    if lremname:
+        if not lremname.startswith('|'):
+            lremname = f"|{lremname}"
+        lremname = lremname.replace('\s', ' ')
+        slit = lremname.split("|")
+        __newFileName = ospath.splitext(file_)[0]
+        for rep in range(1, len(slit)):
+            args = slit[rep].split(":")
+            if len(args) == 3:
+                __newFileName = re_sub(
+                    args[0], args[1], __newFileName, int(args[2]))
+            elif len(args) == 2:
+                __newFileName = re_sub(args[0], args[1], __newFileName)
+            elif len(args) == 1:
+                __newFileName = re_sub(args[0], '', __newFileName)
+        file_ = __newFileName + ospath.splitext(file_)[1]
+        LOGGER.info(f"New Filename : {file_}")
 
+    nfile_ = file_
+    if lprefix:
+        nfile_ = lprefix.replace('\s', ' ') + file_
+        lprefix = re_sub('<.*?>', '', lprefix).replace('\s', ' ')
+        if not file_.startswith(lprefix):
+            file_ = f"{lprefix}{file_}"
+
+    if lsuffix:
+        lsuffix = lsuffix.replace('\s', ' ')
+        sufLen = len(lsuffix)
+        fileDict = file_.split('.')
+        _extIn = 1 + len(fileDict[-1])
+        _extOutName = '.'.join(
+            fileDict[:-1]).replace('.', ' ').replace('-', ' ')
+        _newExtFileName = f"{_extOutName}{lsuffix}.{fileDict[-1]}"
+        if len(_extOutName) > (64 - (sufLen + _extIn)):
+            _newExtFileName = (
+                _extOutName[: 64 - (sufLen + _extIn)]
+                + f"{lsuffix}.{fileDict[-1]}"
+            )
+        file_ = _newExtFileName
+
+    cap_mono = f"<code>{nfile_}</code>"
+    if lcaption:
+        lcaption = lcaption.replace('\|', '%%').replace('\s', ' ')
+        slit = lcaption.split("|")
+        up_path = ospath.join(dirpath, prefile_)
+        cap_mono = slit[0].format(
+            filename=nfile_,
+            size=get_readable_file_size(await aiopath.getsize(up_path)),
+            duration = (await get_media_info(up_path))[0]
+        )
+        if len(slit) > 1:
+            for rep in range(1, len(slit)):
+                args = slit[rep].split(":")
+                if len(args) == 3:
+                    cap_mono = cap_mono.replace(args[0], args[1], int(args[2]))
+                elif len(args) == 2:
+                    cap_mono = cap_mono.replace(args[0], args[1])
+                elif len(args) == 1:
+                    cap_mono = cap_mono.replace(args[0], '')
+        cap_mono = cap_mono.replace('%%', '|')
+    return cap_mono, file_
+    
+    
 async def get_mediainfo_link(up_path):
     stdout, stderr, _ = await cmd_exec(ssplit(f'mediainfo "{up_path}"'))
     tele_content = f"<h4>{ospath.basename(up_path)}</h4><br><br>"
