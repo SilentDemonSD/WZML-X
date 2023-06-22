0f6931ce8af2004b8e86c58c1113fa6201991ce3
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2023-06-23 00:02:39 +0530

    Fix It

--- bot/modules/bot_settings.py
+++ bot/modules/bot_settings.py
@@ -135,8 +135,8 @@ async def load_config():
     if len(SEARCH_API_LINK) == 0:
         SEARCH_API_LINK = ''
 
-    CAP_FONT = environ.get('CAP_FONT', '')
-    if CAP_FONT not in ['', 'b', 'i', 'u', 's', 'spoiler', 'code']:
+    CAP_FONT = environ.get('CAP_FONT', '').lower()
+    if CAP_FONT.strip() not in ['', 'b', 'i', 'u', 's', 'spoiler', 'code']:
         CAP_FONT = 'code'
         
     LEECH_FILENAME_PREFIX = environ.get('LEECH_FILENAME_PREFIX', '')
