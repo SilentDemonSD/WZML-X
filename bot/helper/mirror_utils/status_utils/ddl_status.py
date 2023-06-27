d63e6e8345a7fcbc968ba4ef029d7e32c878d806
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2023-06-27 14:03:02 +0530

    update ddl_status.py

--- bot/helper/mirror_utils/status_utils/ddl_status.py
+++ bot/helper/mirror_utils/status_utils/ddl_status.py
@@ -2,10 +2,11 @@
 from bot.helper.ext_utils.bot_utils import MirrorStatus, get_readable_file_size, get_readable_time
 
 class DDLStatus:
-    def __init__(self, obj, size, message, gid):
+    def __init__(self, obj, size, message, gid, upload_details):
         self.__obj = obj
         self.__size = size
         self.__gid = gid
+        self.upload_details = upload_details
         self.message = message
 
     def processed_bytes(self):
