1d105b9f34256655839407dfbe926f5feb021390
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2023-06-27 14:53:04 +0530

    update rclone_status.py

--- bot/helper/mirror_utils/status_utils/rclone_status.py
+++ bot/helper/mirror_utils/status_utils/rclone_status.py
@@ -2,11 +2,12 @@ from bot.helper.ext_utils.bot_utils import EngineStatus, MirrorStatus
 
 
 class RcloneStatus:
-    def __init__(self, obj, message, gid, status):
+    def __init__(self, obj, message, gid, status, upload_details):
         self.__obj = obj
         self.__gid = gid
         self.__status = status
         self.message = message
+        self.upload_details = upload_details
 
     def gid(self):
         return self.__gid
