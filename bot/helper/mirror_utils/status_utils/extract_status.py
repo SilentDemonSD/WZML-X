a477aed504d43bb6e753e5d03bf284e60673a8f4
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2023-06-27 14:54:02 +0530

    update extract_status.py

--- bot/helper/mirror_utils/status_utils/extract_status.py
+++ bot/helper/mirror_utils/status_utils/extract_status.py
@@ -12,6 +12,7 @@ class ExtractStatus:
         self.__size = size
         self.__gid = gid
         self.__listener = listener
+        self.upload_details = listener.upload_details
         self.__uid = listener.uid
         self.__start_time = time()
         self.message = listener.message
