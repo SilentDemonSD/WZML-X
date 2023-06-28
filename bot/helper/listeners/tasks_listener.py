5093c15d1205e029ad21ac4868d5a1f0550c38d3
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2023-06-28 20:11:57 +0530

    Fix

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -390,7 +390,8 @@ class MirrorLeechListener:
                         toPM = True
                         await sendMessage(self.message, msg + BotTheme('L_BOT_MSG'), btn.build_menu(2), self.random_pic)
                 msg += BotTheme('L_LL_MSG')
-                self.leechlogmsg._client = bot
+                if self.leechlogmsg:
+                    self.leechlogmsg._client = bot
                 fmsg = '\n\n'
                 for index, (link, name) in enumerate(files.items(), start=1):
                     fmsg += f"{index}. <a href='{link}'>{name}</a>\n"
