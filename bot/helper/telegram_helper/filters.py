6c1b2a4f6c6d78584bb273549ae7bb517ca47364
drxxstrange@gmail.com <drxxstrange@gmail.com>
2023-07-01 20:45:49 +0530

    fix scope

--- bot/helper/telegram_helper/filters.py
+++ bot/helper/telegram_helper/filters.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from pyrogram.filters import create
+from pyrogram.enums import ChatType
 
-from bot import user_data, OWNER_ID, config_dict, LOGGER
+from bot import user_data, OWNER_ID
 from bot.helper.telegram_helper.message_utils import chat_info
 
 
@@ -25,30 +26,26 @@ class CustomFilters:
     
     async def authorized_usetting(self, _, message):
         uid = (message.from_user or message.sender_chat).id
-        #if uid == OWNER_ID or (uid in user_data and (user_data[uid].get('is_auth', False) or user_data[uid].get('is_sudo', False))):
-        #    LOGGER.info("Access Given uset")
-        #    return True
-        isExists = False
-        LOGGER.info("uset check")
-        for channel_id in config_dict['AUTHORIZED_CHATS']:
-            LOGGER.info("For Loop check")
-            try:
-                auth_chat = await chat_info(channel_id)
-                user = await auth_chat.get_member(uid)
-                LOGGER.info(user)
-                if user:
-                    isExists = True
-                    break
-            except Exception as e:
-                LOGGER.info(e)
-                continue
-        return isExists
+        chat_id = message.chat.id
+        if message.chat.type == ChatType.PRIVATE:
+            isExists = False
+            for channel_id in user_data:
+                if not (channel_id.get('is_auth') and str(channel_id).startswith('-100')):
+                    continue
+                try:
+                    if await (await chat_info(str(channel_id)).get_member(uid):
+                        isExists = True
+                        break
+                except:
+                    continue
+            return isExists
+        return bool(uid == OWNER_ID or (uid in user_data and (user_data[uid].get('is_auth', False) or user_data[uid].get('is_sudo', False))) or (chat_id in user_data and user_data[chat_id].get('is_auth', False)))
         
     authorized_uset = create(authorized_usetting)
 
     async def sudo_user(self, _, message):
         user = message.from_user or message.sender_chat
         uid = user.id
-        return bool(uid == OWNER_ID or uid in user_data and user_data[uid].get('is_sudo'))
+                return bool(uid == OWNER_ID or uid in user_data and user_data[uid].get('is_sudo'))
 
     sudo = create(sudo_user)
