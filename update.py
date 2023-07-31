642edec00fa0e7f32aec846dabb4b369a3def753
mr-m85 <106661735+mr-m85@users.noreply.github.com>
2023-07-31 10:29:28 +0300

    Update update.py

--- update.py
+++ update.py
@@ -36,10 +36,14 @@ bot_id = BOT_TOKEN.split(':', 1)[0]
 DATABASE_URL = environ.get('DATABASE_URL', '')
 if len(DATABASE_URL) == 0:
     DATABASE_URL = None
+    
+DATABASE_NAME = environ.get('DATABASE_NAME', '')
+if len(DATABASE_NAME) == 0:
+    DATABASE_NAME = 'wzmlx'
 
 if DATABASE_URL is not None:
     conn = MongoClient(DATABASE_URL)
-    db = conn.wzmlx
+    db = conn.get_database(DATABASE_NAME)
     old_config = db.settings.deployConfig.find_one({'_id': bot_id})
     config_dict = db.settings.config.find_one({'_id': bot_id})
     if old_config is not None:
