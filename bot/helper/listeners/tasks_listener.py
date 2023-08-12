1aa5c1e3b5ee20b7caf0041d418e7026fbc91c78
drxxstrange@gmail.com <drxxstrange@gmail.com>
2023-08-12 20:55:20 +0530

    fix

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -180,7 +180,6 @@ class MirrorLeechListener:
                     path = f"{self.dir}/{folder_name}"
                     await makedirs(path, exist_ok=True)
                 self.multiAria[0].pop(link)
-                LOGGER.info("MultiAria :" + self.multiAria)
                 await add_aria2c_download(link, path, self, '', headers, None, None)
                 return
         except:
