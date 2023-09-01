b4b9a7ca89191f90a9a128ff40082b4d0544574e
Karan Adhikari <doc.adhikari@gmail.com>
2023-09-01 22:53:48 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -16,6 +16,8 @@ from requests import Session, session as req_session
 import requests
 from threading import Thread
 from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
 from bot import LOGGER, config_dict
 from bot.helper.ext_utils.bot_utils import get_readable_time, is_share_link
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
