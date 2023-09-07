ba8591baee0cfe2b277649890c8a0736ea88dfee
drxxstrange@gmail.com <drxxstrange@gmail.com>
2023-09-07 20:17:32 +0530

    ..

--- bot/helper/mirror_utils/status_utils/ddl_status.py
+++ bot/helper/mirror_utils/status_utils/ddl_status.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from bot.helper.ext_utils.bot_utils import MirrorStatus, get_readable_file_size, get_readable_time
+from bot import LOGGER
 
 class DDLStatus:
     def __init__(self, obj, size, message, gid, upload_details):
