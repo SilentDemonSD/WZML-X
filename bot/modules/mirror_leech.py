da0cba862e81ce05be2588100680711032cd76dc
drxxstrange@gmail.com <drxxstrange@gmail.com>
2023-09-11 12:12:07 +0530

    fix

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -233,7 +233,7 @@ async def _mirror_leech(client, message, isQbit=False, isLeech=False, sameDir=No
         LOGGER.info(link)
         org_link = link
 
-    if (not is_mega_link(link) or (is_mega_link(link) and config_dict['DEBRID_LINK_API'])) and (not isQbit or (isQbit and config_dict['REAL_DEBRID_API'])) 
+    if (not is_mega_link(link) or (is_mega_link(link) and config_dict['DEBRID_LINK_API'])) and (not isQbit or (isQbit and config_dict['REAL_DEBRID_API'])) \
         and (not is_magnet(link) or (config_dict['REAL_DEBRID_API'] and is_magnet(link))) and not is_rclone_path(link) \
         and not is_gdrive_link(link) and not link.endswith('.torrent') and file_ is None:
         content_type = await get_content_type(link)
