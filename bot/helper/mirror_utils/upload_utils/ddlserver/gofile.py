6b4e35115920f55debe0a737fdbcee60d74c32d0
drxxstrange@gmail.com <drxxstrange@gmail.com>
2023-09-11 18:01:39 +0530

    refactor gofile

--- bot/helper/mirror_utils/upload_utils/ddlserver/gofile.py
+++ bot/helper/mirror_utils/upload_utils/ddlserver/gofile.py
@@ -41,24 +41,25 @@ class Gofile:
     async def upload_folder(self, path, folderId=None):
         if not await aiopath.isdir(path):
             raise Exception(f"Path: {path} is not a valid directory")
+            
         folder_data = await self.create_folder((await self.__getAccount())["rootFolder"], ospath.basename(path))
         await self.__setOptions(contentId=folder_data["id"], option="public", value="true")
+    
         folderId = folderId or folder_data["id"]
-        folderCode = folder_data["code"]
         folder_ids = {".": folderId}
         for root, _, files in await sync_to_async(walk, path):
             rel_path = ospath.relpath(root, path)
             parentFolderId = folder_ids.get(ospath.dirname(rel_path), folderId)
             folder_name = ospath.basename(rel_path)
             currFolderId = (await self.create_folder(parentFolderId, folder_name))["id"]
+            await self.__setOptions(contentId=currFolderId, option="public", value="true")
             folder_ids[rel_path] = currFolderId
 
             for file in files:
                 file_path = ospath.join(root, file)
                 up = await self.upload_file(file_path, currFolderId)
-                LOGGER.info(up)
-        LOGGER.info(folderCode)
-        return folderCode
+                
+        return folder_data["code"]
 
     async def upload_file(self, file: str, folderId: str = "", description: str = "", password: str = "", tags: str = "", expire: str = ""):
         if password and len(password) < 4:
@@ -88,14 +89,11 @@ class Gofile:
         
     async def upload(self, file_path):
         if await aiopath.isfile(file_path):
-            cmd = await self.upload_file(file=file_path)
-            if cmd and 'downloadPage' in cmd:
-                return cmd['downloadPage']
+            if (gCode := await self.upload_file(file=file_path)) and gCode.get("downloadPage", False):
+                return gCode['downloadPage']
         elif await aiopath.isdir(file_path):
-            cmd = await self.upload_folder(path=file_path)
-            if cmd:
-                LOGGER.info(f"https://gofile.io/d/{cmd}")
-                return(f"https://gofile.io/d/{cmd}")
+            if (gCode := await self.upload_folder(path=file_path)):
+                return f"https://gofile.io/d/{gCode}"
         raise Exception("Failed to upload file/folder to Gofile API, Retry or Try after sometimes...")
 
     async def create_folder(self, parentFolderId, folderName):
