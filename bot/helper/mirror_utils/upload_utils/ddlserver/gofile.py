24924606df8b0cfb85d10b130a0584456088d231
Karan Adhikari <doc.adhikari@gmail.com>
2023-09-11 20:43:42 +0530

    Revert

--- bot/helper/mirror_utils/upload_utils/ddlserver/gofile.py
+++ bot/helper/mirror_utils/upload_utils/ddlserver/gofile.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-import os
-import os.path as ospath
-import requests
+from os import path as ospath, walk
+from aiofiles.os import path as aiopath
+from asyncio import sleep
+from aiohttp import ClientSession
+
 from bot import LOGGER
-from bot.helper.ext_utils.bot_utils import is_gofile_token
+from bot.helper.ext_utils.bot_utils import is_gofile_token, sync_to_async
 
 class Gofile:
     def __init__(self, dluploader=None, token=None):
@@ -13,55 +15,57 @@ class Gofile:
         if self.token is not None:
             is_gofile_token(url=self.api_url, token=self.token)
 
-    def __resp_handler(self, response):
+    async def __resp_handler(self, response):
         api_resp = response.get("status", "")
         if api_resp == "ok":
             return response["data"]
         raise Exception(api_resp.split("-")[1] if "error-" in api_resp else "Response Status is not ok and Reason is Unknown")
 
-    def __getServer(self):
-        response = requests.get(f"{self.api_url}getServer").json()
-        return self.__resp_handler(response)
+    async def __getServer(self):
+        async with ClientSession() as session:
+            async with session.get(f"{self.api_url}getServer") as resp:
+                return await self.__resp_handler(await resp.json())
 
-    def __getAccount(self, check_account=False):
+    async def __getAccount(self, check_account=False):
         if self.token is None:
             raise Exception()
         
         api_url = f"{self.api_url}getAccountDetails?token={self.token}&allDetails=true"
-        response = requests.get(api_url).json()
-        if check_account:
-            return response["status"] == "ok" if True else self.__resp_handler(response)
-        else:
-            return self.__resp_handler(response)
+        async with ClientSession() as session:
+            resp = await (await session.get(url=api_url)).json()
+            if check_account:
+                return resp["status"] == "ok" if True else await self.__resp_handler(resp)
+            else:
+                return await self.__resp_handler(resp)
         
-    def upload_folder(self, path, folderId=None):
-        if not os.path.isdir(path):
+    async def upload_folder(self, path, folderId=None):
+        if not await aiopath.isdir(path):
             raise Exception(f"Path: {path} is not a valid directory")
             
-        folder_data = self.create_folder(self.__getAccount()["rootFolder"], ospath.basename(path))
-        self.__setOptions(contentId=folder_data["id"], option="public", value="true")
+        folder_data = await self.create_folder((await self.__getAccount())["rootFolder"], ospath.basename(path))
+        await self.__setOptions(contentId=folder_data["id"], option="public", value="true")
     
         folderId = folderId or folder_data["id"]
         folder_ids = {".": folderId}
-        for root, _, files in os.walk(path):
+        for root, _, files in await sync_to_async(walk, path):
             rel_path = ospath.relpath(root, path)
             parentFolderId = folder_ids.get(ospath.dirname(rel_path), folderId)
             folder_name = ospath.basename(rel_path)
-            currFolderId = self.create_folder(parentFolderId, folder_name)["id"]
-            self.__setOptions(contentId=currFolderId, option="public", value="true")
+            currFolderId = (await self.create_folder(parentFolderId, folder_name))["id"]
+            await self.__setOptions(contentId=currFolderId, option="public", value="true")
             folder_ids[rel_path] = currFolderId
 
             for file in files:
                 file_path = ospath.join(root, file)
-                up = self.upload_file(file_path, currFolderId)
+                up = await self.upload_file(file_path, currFolderId)
                 
         return folder_data["code"]
 
-    def upload_file(self, file: str, folderId: str = "", description: str = "", password: str = "", tags: str = "", expire: str = ""):
+    async def upload_file(self, file: str, folderId: str = "", description: str = "", password: str = "", tags: str = "", expire: str = ""):
         if password and len(password) < 4:
             raise ValueError("Password Length must be greater than 4")
 
-        server = self.__getServer()["server"]
+        server = (await self.__getServer())["server"]
         token = self.token if self.token else ""
         req_dict = {}
         if token:
@@ -80,73 +84,78 @@ class Gofile:
         if self.dluploader.is_cancelled:
             return
         self.dluploader.last_uploaded = 0
-        upload_file = self.dluploader.upload_httpx(f"https://{server}.gofile.io/uploadFile", file, "file", req_dict)
-        return self.__resp_handler(upload_file)
+        upload_file = await self.dluploader.upload_aiohttp(f"https://{server}.gofile.io/uploadFile", file, "file", req_dict)
+        return await self.__resp_handler(upload_file)
         
-    def upload(self, file_path):
-        if os.path.isfile(file_path):
-            if (gCode := self.upload_file(file=file_path)) and gCode.get("downloadPage", False):
+    async def upload(self, file_path):
+        if await aiopath.isfile(file_path):
+            if (gCode := await self.upload_file(file=file_path)) and gCode.get("downloadPage", False):
                 return gCode['downloadPage']
-        elif os.path.isdir(file_path):
-            if (gCode := self.upload_folder(path=file_path)):
+        elif await aiopath.isdir(file_path):
+            if (gCode := await self.upload_folder(path=file_path)):
                 return f"https://gofile.io/d/{gCode}"
         raise Exception("Failed to upload file/folder to Gofile API, Retry or Try after sometimes...")
 
-    def create_folder(self, parentFolderId, folderName):
+    async def create_folder(self, parentFolderId, folderName):
         if self.token is None:
             raise Exception()
         
-        response = requests.put(url=f"{self.api_url}createFolder",
-            data={
-                    "parentFolderId": parentFolderId,
-                    "folderName": folderName,
-                    "token": self.token
-                }
-            ).json()
-        return self.__resp_handler(response)
+        async with ClientSession() as session:
+            async with session.put(url=f"{self.api_url}createFolder",
+                data={
+                        "parentFolderId": parentFolderId,
+                        "folderName": folderName,
+                        "token": self.token
+                    }
+                ) as resp:
+                return await self.__resp_handler(await resp.json())
 
-    def __setOptions(self, contentId, option, value):
+    async def __setOptions(self, contentId, option, value):
         if self.token is None:
             raise Exception()
         
         if not option in ["public", "password", "description", "expire", "tags"]:
             raise Exception(f"Invalid GoFile Option Specified : {option}")
-        response = requests.put(url=f"{self.api_url}setOption",
-            data={
-                    "token": self.token,
-                    "contentId": contentId,
-                    "option": option,
-                    "value": value
-                }
-            ).json()
-        return self.__resp_handler(response)
+        async with ClientSession() as session:
+            async with session.put(url=f"{self.api_url}setOption",
+                data={
+                        "token": self.token,
+                        "contentId": contentId,
+                        "option": option,
+                        "value": value
+                    }
+                ) as resp:
+                return await self.__resp_handler(await resp.json())
 
-    def get_content(self, contentId):
+    async def get_content(self, contentId):
         if self.token is None:
             raise Exception()
         
-        response = requests.get(url=f"{self.api_url}getContent?contentId={contentId}&token={self.token}").json()
-        return self.__resp_handler(response)
+        async with ClientSession() as session:
+            async with session.get(url=f"{self.api_url}getContent?contentId={contentId}&token={self.token}") as resp:
+                return await self.__resp_handler(await resp.json())
 
-    def copy_content(self, contentsId, folderIdDest):
+    async def copy_content(self, contentsId, folderIdDest):
         if self.token is None:
             raise Exception()
-        response = requests.put(url=f"{self.api_url}copyContent",
-                data={
-                    "token": self.token,
-                    "contentsId": contentsId,
-                    "folderIdDest": folderIdDest
-                }
-            ).json()
-        return self.__resp_handler(response)
+        async with ClientSession() as session:
+            async with session.put(url=f"{self.api_url}copyContent",
+                    data={
+                        "token": self.token,
+                        "contentsId": contentsId,
+                        "folderIdDest": folderIdDest
+                    }
+                ) as resp:
+                return await self.__resp_handler(await resp.json())
 
-    def delete_content(self, contentId):
+    async def delete_content(self, contentId):
         if self.token is None:
             raise Exception()
-        response = requests.delete(url=f"{self.api_url}deleteContent",
-                data={
-                    "contentId": contentId,
-                    "token": self.token
-                }
-            ).json()
-        return self.__resp_handler(response)
+        async with ClientSession() as session:
+            async with session.delete(url=f"{self.api_url}deleteContent",
+                    data={
+                        "contentId": contentId,
+                        "token": self.token
+                    }
+                ) as resp:
+                return await self.__resp_handler(await resp.json())
