a8bb36e4bdf5dc1257adc0f9096ec936c2e156cf
karan <dox.adhikari@gmail.com>
2023-09-12 04:51:14 -0700

    fix

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -520,8 +520,8 @@ class MirrorLeechListener:
                 await sendMultiMessage(config_dict['MIRROR_LOG_ID'], message, buttons.build_menu(2), self.random_pic)
             if self.isSuperGroup and self.isPM:
                 message += BotTheme('M_BOT_MSG')
-                buttons.ibutton(BotTheme('CHECK_PM'), f"wzmlx {user_id} botpm")
                 await sendMessage(self.botpmmsg, msg, buttons.build_menu(2), self.random_pic)
+                buttons.ibutton(BotTheme('CHECK_PM'), f"wzmlx {user_id} botpm")
                 await deleteMessage(self.botpmmsg)
             await sendMessage(self.message, message , buttons.build_menu(2), photo=self.random_pic)
 
