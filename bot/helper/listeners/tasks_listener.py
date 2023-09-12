e6140d7abb3278659e0ab95052a879b5ea682757
karan <dox.adhikari@gmail.com>
2023-09-12 04:59:09 -0700

    fix

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -137,7 +137,7 @@ class MirrorLeechListener:
             dispTime = datetime.now(timezone(config_dict['TIMEZONE'])).strftime('%d/%m/%y, %I:%M:%S %p')
             self.linkslogmsg = await sendCustomMsg(config_dict['LINKS_LOG_ID'], BotTheme('LINKS_START', Mode=self.upload_details['mode'], Tag=self.tag) + BotTheme('LINKS_SOURCE', On=dispTime, Source=self.source_msg))
         user_dict = user_data.get(self.message.from_user.id, {})
-        if config_dict['BOT_PM'] or user_dict.get('bot_pm'):
+        if config_dict['BOT_PM'] or user_dict.get('bot_pm') and self.isSuperGroup:
             self.botpmmsg = await sendCustomMsg(self.message.from_user.id, BotTheme('PM_START', msg_link=self.source_url))
         if self.isSuperGroup and config_dict['INCOMPLETE_TASK_NOTIFIER'] and DATABASE_URL:
             await DbManger().add_incomplete_task(self.message.chat.id, self.message.link, self.tag, self.source_url)
