7bbfb4c91b9f22d926437953697281d64bc59fad
Karan Adhikari <doc.adhikari@gmail.com>
2023-09-14 08:12:35 +0530

    Update tasks_listener.py

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -437,11 +437,11 @@ class MirrorLeechListener:
                 await sendMessage(self.message, msg, photo=self.random_pic)
             else:
                 if self.source_url and config_dict['SOURCE_LINK']:
-                    btn.ubutton(BotTheme('SOURCE_URL'), self.source_url)
-                btns = btn.build_menu(2)
+                    bt.ubutton(BotTheme('SOURCE_URL'), self.source_url)
                 if self.isSuperGroup:
                     btn = extra_btns(buttons)[0]
                 message = msg
+                btn = bt
                 btns = btn.build_menu(2)
                 if self.isSuperGroup and not self.isPM:
                     message += BotTheme('L_LL_MSG')
