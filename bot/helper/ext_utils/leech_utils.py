bba49c1343ea7983c608397dd6e2da3db21e6e9a
drxxstrange@gmail.com <drxxstrange@gmail.com>
2023-09-17 22:28:59 +0530

    forgot

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -321,7 +321,7 @@ async def get_ss(up_path, ss_no):
     thumbs_path, tstamps = await take_ss(up_path, total=ss_no, gen_ss=True)
     th_html = f"📌 <h4>{ospath.basename(up_path)}</h4><br>📇 <b>Total Screenshots:</b> {ss_no}<br><br>"
     async def telefile(thumb):
-        tele_id = await sync_to_async(upload_file(ospath.join(thumbs_path, thumb)))
+        tele_id = await sync_to_async(upload_file, ospath.join(thumbs_path, thumb))
         return tele_id[0], tstamps[thumb]
     tasks = [telefile(thumb) for thumb in natsorted(await listdir(thumbs_path))]
     results = await gather(*tasks)
