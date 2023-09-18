4381f1c9f8df821a315d9effd5fb3f68a42fb553
drxxstrange@gmail.com <drxxstrange@gmail.com>
2023-09-18 12:13:11 +0530

    bug fix

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -47,23 +47,24 @@ async def get_media_info(path, metadata=False):
         result = await cmd_exec(["ffprobe", "-hide_banner", "-loglevel", "error", "-print_format",
                                  "json", "-show_format", "-show_streams", path])
         if res := result[1]:
-            LOGGER.warning(f'Get Media Info: {res}')
+            LOGGER.warning(f'Media Info FF: {res}')
     except Exception as e:
-        LOGGER.error(f'Get Media Info: {e}. Mostly File not found!')
+        LOGGER.error(f'Media Info: {e}. Mostly File not found!')
+        if metadata:
+            return 0, "", "", ""
         return 0, None, None
     ffresult = eval(result[0])
     fields = ffresult.get('format')
     if fields is None:
-        LOGGER.error(f"Get Media Info: {result}")
+        LOGGER.error(f"Media Info Sections: {result}")
+        if metadata:
+            return 0, "", "", ""
         return 0, None, None
     duration = round(float(fields.get('duration', 0)))
-    tags = fields.get('tags', {})
-    artist = tags.get('artist') or tags.get('ARTIST') or tags.get("Artist")
-    title = tags.get('title') or tags.get('TITLE') or tags.get("Title")
     if metadata:
         lang, qual, stitles = "", "", ""
         if (streams := ffresult.get('streams')) and streams[0].get('codec_type') == 'video':
-            qual = streams[0].get('height')
+            qual = int(streams[0].get('height'))
             qual = f"{480 if qual <= 480 else 540 if qual <= 540 else 720 if qual <= 720 else 1080 if qual <= 1080 else 2160 if qual <= 2160 else 4320 if qual <= 4320 else 8640}p"
             for stream in streams:
                 if stream.get('codec_type') == 'audio' and (lc := stream.get('tags', {}).get('language')):
@@ -71,6 +72,9 @@ async def get_media_info(path, metadata=False):
                 if stream.get('codec_type') == 'subtitle' and (st := stream.get('tags', {}).get('language')):
                     stitles += Language.get(st).display_name() + ", "
         return duration, qual, lang[:-2], stitles[:-2]
+    tags = fields.get('tags', {})
+    artist = tags.get('artist') or tags.get('ARTIST') or tags.get("Artist")
+    title = tags.get('title') or tags.get('TITLE') or tags.get("Title")
     return duration, artist, title
 
 
