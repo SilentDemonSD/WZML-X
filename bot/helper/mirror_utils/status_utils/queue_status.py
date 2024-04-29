b25e1ad2f21279f39213e5f6839a03422c98aeef
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:37:43 +0600

    improve

--- bot/helper/mirror_utils/status_utils/queue_status.py
+++ bot/helper/mirror_utils/status_utils/queue_status.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
+import asyncio
 from bot import LOGGER
 from bot.helper.ext_utils.bot_utils import EngineStatus, get_readable_file_size, MirrorStatus
 
 
 class QueueStatus:
-    def __init__(self, name, size, gid, listener, status):
+    def __init__(self, name: str, size: int, gid: int, listener, status: str):
+        """
+        Initialize QueueStatus object
+
+        :param name: Name of the queue
+        :param size: Size of the queue
+        :param gid: Group id of the queue
+        :param listener: Listener object
+        :param status: Status of the queue
+        """
         self.__name = name
         self.__size = size
         self.__gid = gid
@@ -13,42 +23,97 @@ class QueueStatus:
         self.__status = status
         self.message = listener.message
 
-    def gid(self):
+    @property
+    def gid(self) -> int:
+        """
+        Get group id of the queue
+
+        :return: Group id of the queue
+        """
         return self.__gid
 
-    def name(self):
+    @property
+    def name(self) -> str:
+        """
+        Get name of the queue
+
+        :return: Name of the queue
+        """
         return self.__name
 
-    def size(self):
+    @property
+    def size(self) -> str:
+        """
+        Get size of the queue in readable format
+
+        :return: Readable size of the queue
+        """
         return get_readable_file_size(self.__size)
 
-    def status(self):
-        if self.__status == 'dl':
-            return MirrorStatus.STATUS_QUEUEDL
-        return MirrorStatus.STATUS_QUEUEUP
+    def status(self) -> MirrorStatus:
+        """
+        Get status of the queue
 
-    def processed_bytes(self):
+        :return: Status of the queue
+        """
+        return MirrorStatus.STATUS_QUEUE if self.__status == 'dl' else MirrorStatus.STATUS_QUEUEUP
+
+    def processed_bytes(self) -> int:
+        """
+        Get processed bytes of the queue
+
+        :return: Processed bytes of the queue
+        """
         return 0
 
-    def progress(self):
+    def progress(self) -> str:
+        """
+        Get progress of the queue
+
+        :return: Progress of the queue
+        """
         return '0%'
 
-    def speed(self):
+    def speed(self) -> str:
+        """
+        Get speed of the queue
+
+        :return: Speed of the queue
+        """
         return '0B/s'
 
-    def eta(self):
+    def eta(self) -> str:
+        """
+        Get ETA of the queue
+
+        :return: ETA of the queue
+        """
         return '-'
 
-    def download(self):
+    def download(self) -> 'QueueStatus':
+        """
+        Get download object of the queue
+
+        :return: Download object of the queue
+        """
         return self
 
+    @asyncio.coroutine
     async def cancel_download(self):
+        """
+        Cancel download of the queue
+        """
         LOGGER.info(f'Cancelling Queue{self.__status}: {self.__name}')
         if self.__status == 'dl':
             await self.__listener.onDownloadError('task have been removed from queue/download')
         else:
             await self.__listener.onUploadError('task have been removed from queue/upload')
 
+    @property
+    def eng(self) -> EngineStatus:
+        """
+        Get engine status of the queue
 
-    def eng(self):
+        :return: Engine status of the queue
+        """
         return EngineStatus().STATUS_QUEUE
