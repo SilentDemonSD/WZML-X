30dc3763a245b6dd321e523a907dff9afaa18d95
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:38 +0600

    improve

--- bot/helper/mirror_utils/status_utils/clone_status.py
+++ bot/helper/mirror_utils/status_utils/clone_status.py
@@ -1,6 +1,5 @@
 from bot.helper.ext_utils.bot_utils import MirrorStatus, get_readable_file_size, get_readable_time, EngineStatus
 
-
 class CloneStatus:
     def __init__(self, obj, size, message, gid):
         self.__obj = obj
@@ -15,21 +14,21 @@ class CloneStatus:
         return self.__size
 
     def size(self):
-        return get_readable_file_size(self.__size)
+        return get_readable_file_size(self.__size) if self.__size is not None else 'Unknown'
 
     def status(self):
         return MirrorStatus.STATUS_CLONING
 
     def name(self):
-        return self.__obj.name
+        return self.__obj.name if self.__obj is not None else 'Unknown'
 
     def gid(self) -> str:
         return self.__gid
 
     def progress_raw(self):
         try:
-            return self.__obj.transferred_size / self.__size * 100
-        except:
+            return self.__obj.transferred_size / self.__size * 100 if self.__size is not None else 0
+        except ZeroDivisionError:
             return 0
 
     def progress(self):
@@ -37,17 +36,22 @@ class CloneStatus:
 
     def speed_raw(self):
         """
-        :return: Download speed in Bytes/Seconds
+        :return: Download speed in Bytes/Seconds or 0 if there's an error
         """
-        return self.__obj.cspeed()
+        try:
+            return self.__obj.cspeed() if self.__obj is not None else 0
+        except:
+            return 0
 
     def speed(self):
-        return f'{get_readable_file_size(self.speed_raw())}/s'
+        return f'{get_readable_file_size(self.speed_raw())}/s' if self.speed_raw() > 0 else 'Unknown'
 
     def eta(self):
         try:
-            seconds = (self.__size - self.__obj.transferred_size) / self.speed_raw()
-            return f'{get_readable_time(seconds)}'
+            if self.__size is not None and self.speed_raw() > 0:
+                seconds = (self.__size - self.__obj.transferred_size) / self.speed_raw()
+                return f'{get_readable_time(seconds)}'
+            return '-'
         except:
             return '-'
 
