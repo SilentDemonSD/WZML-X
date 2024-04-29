296698f215843335af1520ace541d65825333510
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:35:49 +0600

    improve

--- bot/helper/listeners/direct_listener.py
+++ bot/helper/listeners/direct_listener.py
@@ -2,10 +2,14 @@ from time import sleep
 
 from bot import LOGGER, aria2
 from bot.helper.ext_utils.bot_utils import async_to_sync, sync_to_async
-
+import aiohttp
+import asyncio
 
 class DirectListener:
-    def __init__(self, foldername, total_size, path, listener, a2c_opt):
+    """
+    A class to handle direct downloads.
+    """
+    def __init__(self, foldername: str, total_size: int, path: str, listener, a2c_opt):
         self.__path = path
         self.__listener = listener
         self.__is_cancelled = False
@@ -26,51 +30,59 @@ class DirectListener:
     def speed(self):
         return self.task.download_speed if self.task else 0
 
-    def download(self, contents):
+    async def download(self, contents):
+        """
+        Download the given contents.
+        """
         self.is_downloading = True
-        for content in contents:
-            if self.__is_cancelled:
-                break
-            if content['path']:
-                self.__a2c_opt['dir'] = f"{self.__path}/{content['path']}"
-            else:
-                self.__a2c_opt['dir'] = self.__path
-            filename = content['filename']
-            self.__a2c_opt['out'] = filename
-            try:
-                self.task = aria2.add_uris([content['url']], self.__a2c_opt, position=0)
-            except Exception as e:
-                self.__failed += 1
-                LOGGER.error(f'Unable to download {filename} due to: {e}')
-                continue
-            self.task = self.task.live
-            while True:
-                if self.__is_cancelled:
-                    if self.task:
-                        self.task.remove(True, True)
-                    break
-                self.task = self.task.live
-                if error_message:= self.task.error_message:
-                    self.__failed += 1
-                    LOGGER.error(f'Unable to download {self.task.name} due to: {error_message}')
-                    self.task.remove(True, True)
-                    break
-                elif self.task.is_complete:
-                    self.__proc_bytes += self.task.total_length
-                    self.task.remove(True)
-                    break
-                sleep(1)
-            self.task = None
-        if self.__is_cancelled:
+        try:
+            async with aiohttp.ClientSession() as session:
+                tasks = []
+                for content in contents:
+                    if self.__is_cancelled:
+                        break
+                    if content['path']:
+                        self.__a2c_opt['dir'] = f"{self.__path}/{content['path']}"
+                    else:
+                        self.__a2c_opt['dir'] = self.__path
+                    filename = content['filename']
+                    self.__a2c_opt['out'] = filename
+                    try:
+                        task = aria2.add_uris([content['url']], self.__a2c_opt, position=0, session=session)
+                    except Exception as e:
+                        self.__failed += 1
+                        LOGGER.error(f'Unable to download {filename} due to: {e}')
+                        continue
+                    tasks.append(task)
+                await asyncio.gather(*tasks)
+        except asyncio.CancelledError:
+            self.__is_cancelled = True
+            LOGGER.info(f"Cancelling Download: {self.name}")
+            await self.__listener.onDownloadError("Download Cancelled by User!")
+            if self.task:
+                await sync_to_async(self.task.remove, force=True, files=True)
+            self.is_downloading = False
             return
+        except Exception as e:
+            LOGGER.error(f'Unable to download due to: {e}')
+            await self.__listener.onDownloadError('Download Failed!')
+            self.is_downloading = False
+            return
+
+        self.__proc_bytes = sum([task.completed_length for task in tasks])
+        self.is_downloading = False
         if self.__failed == len(contents):
-            async_to_sync(self.__listener.onDownloadError, 'All files are failed to download!')
+            await async_to_sync(self.__listener.onDownloadError, 'All files are failed to download!')
             return
-        async_to_sync(self.__listener.onDownloadComplete)
+        await async_to_sync(self.__listener.onDownloadComplete)
 
     async def cancel_download(self):
+        """
+        Cancel the current download.
+        """
         self.__is_cancelled = True
         LOGGER.info(f"Cancelling Download: {self.name}")
         await self.__listener.onDownloadError("Download Cancelled by User!")
         if self.task:
             await sync_to_async(self.task.remove, force=True, files=True)
+        self.is_downloading = False
