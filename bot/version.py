88de7810738e141b86faaf93c4149a30e62a010e
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:42:36 +0600

    improve

--- bot/version.py
+++ bot/version.py
@@ -1,8 +1,18 @@
-from typing import NamedTuple
+from dataclasses import dataclass
 
-class Version(NamedTuple):
+@dataclass
+class Version:
     major: int
     minor: int = 0
     patch: int = 0
     # You can add more attributes if needed
+
+    def __post_init__(self):
+        # Validate the attributes
+        if not isinstance(self.major, int):
+            raise TypeError("major must be an integer")
+        if not isinstance(self.minor, int):
+            raise TypeError("minor must be an integer")
+        if not isinstance(self.patch, int):
+            raise TypeError("patch must be an integer")
 
