5b571ae67894139e27535a9f5505a9438d89c683
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:52:28 +0600

    improve

--- bot/helper/telegram_helper/button_build.py
+++ bot/helper/telegram_helper/button_build.py
@@ -2,3 +2,24 @@ from typing import List, Union, Dict
 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def create_keyboard(buttons: List[List[str]], row_width: int = 3) -> InlineKeyboardMarkup:
+    """
+    Create an InlineKeyboardMarkup object from a list of buttons.
+
+    :param buttons: A list of lists of button labels.
+    :param row_width: The maximum number of buttons in a single row.
+    :return: An InlineKeyboardMarkup object.
+    """
+    keyboard = []
+    row = []
+    for button_labels in buttons:
+        for button_label in button_labels:
+            row.append(InlineKeyboardButton(button_label, callback_data=button_label))
+            if len(row) == row_width:
+                keyboard.append(row)
+                row = []
+        if row:
+            keyboard.append(row)
+            row = []
+    return InlineKeyboardMarkup(keyboard)
+
