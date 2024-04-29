f6e515e6a59a78b87ce93de51109688e2461a88c
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:45:20 +0600

    improve

--- bot/helper/mirror_utils/status_utils/mega_download_status.py
+++ bot/helper/mirror_utils/status_utils/mega_download_status.py
@@ -1,12 +1,9 @@
 import os
 from dataclasses import dataclass
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 from pathlib import Path
 from datetime import timedelta
 import dateutil.parser
-
-# Custom classes
-# from bot.helper.ext_utils.bot_utils import EngineStatus, MirrorStatus, get_readable_file_size, get_readable_time
 
 class CustomNamedTuple(NamedTuple):
     """
@@ -16,48 +13,38 @@ class CustomNamedTuple(NamedTuple):
     url: str
 
     @property
-    def readable_size(self):
+    def readable_size(self) -> str:
         """
         Returns the size of the file in a human-readable format.
         """
         return get_readable_file_size(self.size)
 
-
+@dataclass
 class MegaDownloadStatus:
     """
     A class representing the status of a Mega download.
     """
-    def __init__(self, name: str, size: int, gid: str, obj, message, upload_details):
-        """
-        Initialize the MegaDownloadStatus object.
-
-        :param name: Name of the file being downloaded
-        :param size: Size of the file in bytes
-        :param gid: The unique identifier of the download
-        :param obj: The object containing download details
-        :param message: The message object associated with the download
-        :param upload_details: Details of the upload
-        """
-        self.name = name
-        self.size = size
-        self.gid = gid
-        self.obj = obj
-        self.message = message
-        self.upload_details = upload_details
+    name: str
+    size: int
+    gid: str
+    obj: Optional[Path]
+    message: Optional[Path]
+    upload_details: str
+    time: timedelta
 
     @property
-    def readable_size(self):
+    def readable_size(self) -> str:
         """
         Returns the size of the file in a human-readable format.
         """
         return get_readable_file_size(self.size)
 
     @property
-    def readable_time(self):
+    def readable_time(self) -> str:
         """
         Returns the time taken for the download in a human-readable format.
         """
-        return get_readable_time(self.obj.time)
+        return str(self.time)
 
     def __str__(self):
         """
