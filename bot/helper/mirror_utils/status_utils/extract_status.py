1ad26668057ba65b29cfbb6fd4e24d4b55abe353
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:42 +0600

    improve

--- bot/helper/mirror_utils/status_utils/extract_status.py
+++ bot/helper/mirror_utils/status_utils/extract_status.py
@@ -6,12 +6,24 @@ from bot.helper.ext_utils.fs_utils import get_path_size
 
 class ExtractStatus:
     def __init__(self, name: str, size: int, gid: int, listener):
-        self.__name = name
-        self.__size = size
-        self.__gid = gid
-        self.__listener = listener
+        self.name = name
+        self.size = size
+        self.gid = gid
+        self.listener = listener
         self.upload_details = listener.upload_details
-        self.__uid = listener.uid
-        self.__start_time = time()
+        self.uid = listener.uid
+        self.start_time = time()
         self.message = listener.message
+
+    @property
+    def readable_size(self):
+        return get_readable_file_size(self.size)
+
+    @property
+    def readable_time_elapsed(self):
+        return get_readable_time(time() - self.start_time)
+
+    @property
+    def path_size(self):
+        return get_path_size(self.name)
 
