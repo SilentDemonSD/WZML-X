68db0ddf6d9c5bb430f912fc5b6f4610d8a4049b
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:45:14 +0600

    improve

--- bot/helper/themes/__init__.py
+++ bot/helper/themes/__init__.py
@@ -6,42 +6,72 @@ from random import choice as rchoice
 from bot import config_dict, LOGGER
 from bot.helper.themes import wzml_minimal
 
-AVAILABLE_THEMES = {}
-theme_directory = join('bot', 'helper', 'themes')
-for theme in os.listdir(theme_directory):
-    if theme.startswith('wzml_') and theme.endswith('.py'):
-        theme_name = theme[5:-3]
-        AVAILABLE_THEMES[theme_name] = import_module(f'bot.helper.themes.{theme_name}')
+AVAILABLE_THEMES: dict = {}
+theme_directory: str = join('bot', 'helper', 'themes')
+
+if 'BOT_THEME' in config_dict:
+    if os.path.exists(theme_directory):
+        for theme in os.listdir(theme_directory):
+            if theme.startswith('wzml_') and theme.endswith('.py'):
+                theme_name: str = theme[5:-3]
+                try:
+                    theme_module: object = import_module(f'bot.helper.themes.{theme_name}')
+                except ModuleNotFoundError:
+                    continue
+                if hasattr(theme_module, 'WZMLStyle'):
+                    style_module: object = getattr(theme_module, 'WZMLStyle')
+                    if hasattr(style_module(), var_name):
+                        AVAILABLE_THEMES[theme_name] = style_module
 
 def bot_theme(var_name: str, **format_vars: dict) -> str:
-    text = None
-    theme_ = config_dict.get('BOT_THEME')
+    text: str = None
+    theme_: str = config_dict.get('BOT_THEME')
 
     if theme_ and theme_ in AVAILABLE_THEMES:
-        theme_module = AVAILABLE_THEMES[theme_]
-        if hasattr(theme_module, 'WZMLStyle'):
-            style_module = getattr(theme_module, 'WZMLStyle')
-            if hasattr(style_module(), var_name):
-                text = getattr(style_module(), var_name)
-                return text.format_map(format_vars)
+        style_module: object = AVAILABLE_THEMES[theme_]
+        if hasattr(style_module, var_name):
+            text = getattr(style_module, var_name)
+            if text and hasattr(text, 'format_map'):
+                if format_vars:
+                    return text.format_map(format_vars)
+                else:
+                    LOGGER.error(f"No format variables provided for {var_name} in {theme_}")
             else:
-                LOGGER.error(f"{var_name} not found in {theme_}. Please recheck with Official Repo")
+                LOGGER.error(f"{var_name} not a string or doesn't have a format_map method in {theme_}")
     elif theme_ == 'random':
-        available_themes = list(AVAILABLE_THEMES.values())
-        rtheme = rchoice(available_themes)
-        LOGGER.info(f"Random Theme Chosen: {rtheme}")
-        if hasattr(rtheme, 'WZMLStyle'):
-            style_module = getattr(rtheme, 'WZMLStyle')
-            if hasattr(style_module(), var_name):
-                text = getattr(style_module(), var_name)
-                return text.format_map(format_vars)
+        if AVAILABLE_THEMES:
+            rtheme: str = rchoice(list(AVAILABLE_THEMES.keys()))
+            LOGGER.info(f"Random Theme Chosen: {rtheme}")
+            style_module: object = AVAILABLE_THEMES[rtheme]
+            if hasattr(style_module, var_name):
+                text = getattr(style_module, var_name)
+                if text and hasattr(text, 'format_map'):
+                    if format_vars:
+                        return text.format_map(format_vars)
+                    else:
+                        LOGGER.error(f"No format variables provided for {var_name} in {rtheme}")
+                else:
+                    LOGGER.error(f"{var_name} not a string or doesn't have a format_map method in {rtheme}")
     else:
-        if hasattr(wzml_minimal, 'WZMLStyle'):
-            style_module = getattr(wzml_minimal, 'WZMLStyle')
-            if hasattr(style_module(), var_name):
-                text = getattr(style_module(), var_name)
-                return text.format_map(format_vars)
+        style_module: object = wzml_minimal
+        if hasattr(style_module, 'WZMLStyle'):
+            style_module = getattr(style_module, 'WZMLStyle')
+            if hasattr(style_module, var_name):
+                text = getattr(style_module, var_name)
+                if text and hasattr(text, 'format_map'):
+                    if format_vars:
+                        return text.format_map(format_vars)
+                    else:
+                        LOGGER.error(f"No format variables provided for {var_name} in wzml_minimal")
+                else:
+                    LOGGER.error(f"{var_name} not a string or doesn't have a format_map method in wzml_minimal")
 
-    LOGGER.error(f"{var_name} not found in any available themes. Please recheck with Official Repo")
+    if text and hasattr(text, 'format_map'):
+        if format_vars:
+            return text.format_map(format_vars)
+        else:
+            LOGGER.error(f"No format variables provided for {var_name} in any available themes")
+    else:
+        LOGGER.error(f"{var_name} not a string or doesn't have a format_map method in any available themes")
     return None
 
