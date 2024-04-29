8db6d92f379ac569f4c36fe15bb644b7ae898461
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:45:08 +0600

    improve

--- bot/modules/category_select.py
+++ bot/modules/category_select.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
+import argparse
 import asyncio
 import time
-from functools import partial
 from typing import Dict, Any, Optional
 
 import pyrogram
@@ -18,6 +18,19 @@ from bot.helper.telegram_helper.button_build import ButtonMaker
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import editMessage, sendMessage, open_category_btns
 
+def parse_arguments(args: list) -> Dict[str, Any]:
+    """
+    Parse command line arguments.
+
+    :param args: List of command line arguments
+    :return: Dictionary of parsed arguments
+    """
+    parser = argparse.ArgumentParser(description="Change the category of a download task.")
+    parser.add_argument('-id', type=str, help="GDrive ID of the file")
+    parser.add_argument('-index', type=str, help="Index link of the category")
+    parser.add_argument('link', type=str, help="GID or reply to a message")
+    return parser.parse_args(args).__dict__
+
 async def change_category(client: pyrogram.Client, message: Message) -> None:
     """
     Change the category of a download task.
@@ -26,46 +39,47 @@ async def change_category(client: pyrogram.Client, message: Message) -> None:
     :param message: Received message
     """
     user_id = message.from_user.id
-    text = message.text.split('\n')
-    input_list = text[0].split(' ')
+    args = parse_arguments(message.text.split()[1:])
 
-    arg_base = {'link': '', 
-                '-id': '',
-                '-index': ''}
+    if not categories_dict:
+        await sendMessage(message, "No categories defined!")
+        return
 
-    args = arg_parser(input_list[1:], arg_base)
+    if download_dict is None or client is None:
+        await sendMessage(message, "Internal error: missing download_dict or client object!")
+        return
 
-    drive_id = args['-id']
-    index_link = args['-index']
+    drive_id = args['id']
+    index_link = args['index']
+    gid = args['link']
 
     if drive_id and is_gdrive_link(drive_id):
         drive_id = GoogleDriveHelper.getIdFromUrl(drive_id)
 
     dl: Optional[Dict[str, Any]] = None
-    if gid := args['link']:
-        dl = await getDownloadByGid(gid)
-        if not dl:
-            await sendMessage(message, f"GID: <code>{gid}</code> Not Found.")
-            return
-    if reply_to := message.reply_to_message:
-        async with download_dict_lock:
-            dl = download_dict.get(reply_to.message_id, None)
-        if not dl:
-            await sendMessage(message, "This is not an active task!")
-            return
+    if gid.isdigit():
+        dl = download_dict.get(int(gid), None)
+    elif gid.startswith('gid:'):
+        dl = await getDownloadByGid(gid[4:])
+
     if not dl:
-        await sendMessage(message, CATEGORY_HELP_MESSAGE)
+        await sendMessage(message, "This is not an active task!" if gid.isdigit() else f"GID: <code>{gid[4:]}</code> Not Found.")
         return
+
     if not await CustomFilters.sudo(client, message) and dl.get('user_id') != user_id:
         await sendMessage(message, "You do not have permission to change the category of this task!")
         return
+
     if not index_link:
-        await sendMessage(message, "Please provide a valid index link!")
+        await sendMessage(message, CATEGORY_HELP_MESSAGE)
         return
+
     category = categories_dict.get(index_link)
     if not category:
         await sendMessage(message, "Invalid category!")
         return
+
     async with download_dict_lock:
         dl['category'] = category
+
     await sendMessage(message, f"Category changed to {category}!")
