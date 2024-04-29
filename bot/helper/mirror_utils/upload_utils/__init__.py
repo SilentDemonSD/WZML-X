45841fc85dfdbf7cbb9a107060278970b9b9b65b
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:36:55 +0600

    improve

--- bot/helper/mirror_utils/upload_utils/__init__.py
+++ bot/helper/mirror_utils/upload_utils/__init__.py
@@ -1,10 +1,12 @@
-# This function calculates the area of a rectangle
-def rectangle_area(length: float, width: float) -> float:
-    # The area of a rectangle is calculated by multiplying its length and width
+def calculate_rectangle_area(length: float, width: float) -> float:
+    """
+    This function calculates the area of a rectangle.
+
+    :param length: The length of the rectangle.
+    :param width: The width of the rectangle.
+    :return: The area of the rectangle.
+    """
     area = length * width
-    
-    # The area is then returned as the output of the function
     return area
 
-# Adding type hints for function parameters and return value makes it easier for other developers to understand the expected input and output types.
-
+# Adding a docstring to the function provides a clear explanation of its purpose, parameters, and return value.
