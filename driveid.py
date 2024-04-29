3c56835e03c218842713c437056657a970803294
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:35:45 +0600

    improve

--- driveid.py
+++ driveid.py
@@ -1,31 +1,53 @@
 import os
 import re
-print("\n\n"
-      "        Bot can search files recursively, but you have to add the list of drives you want to search.\n"
-      "        Use the following format: (You can use 'root' in the ID in case you wan to use main drive.)\n"
-      "        teamdrive NAME      -->   anything that you likes\n"
-      "        teamdrive ID        -->   id of teamdrives in which you likes to search ('root' for main drive)\n"
-      "        teamdrive INDEX URL -->   enter index url for this drive.\n"
-      "                                  go to the respective drive and copy the url from address bar\n")
-msg = ''
-if os.path.exists('list_drives.txt'):
-    with open('list_drives.txt', 'r+') as f:
-        lines = f.read()
-    if not re.match(r'^\s*$', lines):
-        print(lines)
-        print("\n\n"
-              "      DO YOU WISH TO KEEP THE ABOVE DETAILS THAT YOU PREVIOUSLY ADDED???? ENTER (y/n)\n"
-              "      IF NOTHING SHOWS ENTER n")
-        while 1:
-            choice = input()
-            if choice in ['y', 'Y']:
-                msg = f'{lines}'
-                break
-            elif choice in ['n', 'N']:
-                break
-            else:
-                print(
-                    "\n\n      DO YOU WISH TO KEEP THE ABOVE DETAILS ???? y/n <=== this is option ..... OPEN YOUR EYES & READ...")
+import urllib.parse
+
+print(
+    "\n\n"
+    "        Bot can search files recursively, but you have to add the list of drives you want to search.\n"
+    "        Use the following format:\n"
+    "        teamdrive NAME      -->   anything that you likes\n"
+    "        teamdrive ID        -->   id of teamdrives in which you likes to search ('root' for main drive)\n"
+    "        teamdrive INDEX URL -->   enter index url for this drive.\n"
+    "                                  go to the respective drive and copy the url from address bar\n")
+
+def read_file(file_path: str) -> str:
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            return f.read()
+    else:
+        return ""
+
+def write_file(file_path: str, msg: str) -> None:
+    with open(file_path, 'w') as file:
+        file.write(msg)
+
+def is_valid_url(url: str) -> bool:
+    try:
+        result = urllib.parse.urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+msg = read_file('list_drives.txt')
+
+if msg:
+    print(msg)
+    print(
+        "\n\n"
+        "      DO YOU WISH TO KEEP THE ABOVE DETAILS THAT YOU PREVIOUSLY ADDED???? ENTER (y/n)\n"
+        "      IF NOTHING SHOWS ENTER n")
+    while True:
+        choice = input()
+        if choice in ['y', 'Y']:
+            break
+        elif choice in ['n', 'N']:
+            msg = ""
+            break
+        else:
+            print(
+                "\n\n      DO YOU WISH TO KEEP THE ABOVE DETAILS ???? y/n <=== this is option ..... OPEN YOUR EYES & READ...")
+
 num = int(input("    How Many Drive/Folder You Likes To Add : "))
 for count in range(1, num + 1):
     print(f"\n        > DRIVE - {count}\n")
@@ -36,13 +58,10 @@ for count in range(1, num + 1):
         print("\n\n        ERROR : Dont leave the name/id without filling.")
         exit(1)
     name = name.replace(" ", "_")
-    if index:
-        if index[-1] == "/":
-            index = index[:-1]
-    else:
-        index = ''
+    if index and not is_valid_url(index):
+        print("\n\n        ERROR : Invalid URL format.")
+        exit(1)
     msg += f"{name} {id} {index}\n"
-with open('list_drives.txt', 'w') as file:
-    file.truncate(0)
-    file.write(msg)
+
+write_file('list_drives.txt', msg)
 print("\n\n    Done!")
