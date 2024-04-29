103a752eae801bc5efd914cb8c1a92f8f77a00c2
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:52:44 +0600

    improve

--- bot/modules/authorize.py
+++ bot/modules/authorize.py
@@ -10,68 +10,59 @@ from bot.helper.telegram_helper.message_utils import sendMessage, deleteMessage
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.ext_utils.db_handler import DbManger
-from bot.helper.ext_utils.bot_utils import update_user_ldata
+from bot.helper.ext_utils.bot_utils import is_sudo_user, is_blacklisted_user
 
-AuthorizeFilter = create(lambda _, __, message: message.chat.id == message.from_user.id and message.from_user.is_self)
-UnauthorizeFilter = create(lambda _, __, message: message.chat.id == message.from_user.id and message.from_user.is_self)
-AddSudoFilter = create(lambda _, __, message: message.chat.id == message.from_user.id and message.from_user.is_self)
-RemoveSudoFilter = create(lambda _, __, message: message.chat.id == message.from_user.id and message.from_user.is_self)
-AddBlacklistFilter = create(lambda _, __, message: message.chat.id == message.from_user.id and message.from_user.is_self)
-RmBlacklistFilter = create(lambda _, __, message: message.chat.id == message.from_user.id and message.from_user.is_self)
-BlacklistedFilter = create(lambda _, __, message: message.from_user.id not in user_data.get_sudoers() and user_data.is_user_blacklisted(message.from_user.id))
+AuthorizeFilter = create(lambda _, __, message: is_sudo_user(message))
+UnauthorizeFilter = create(lambda _, __, message: is_sudo_user(message))
+AddSudoFilter = create(lambda _, __, message: is_sudo_user(message))
+RemoveSudoFilter = create(lambda _, __, message: is_sudo_user(message))
+AddBlacklistFilter = create(lambda _, __, message: is_sudo_user(message))
+RmBlacklistFilter = create(lambda _, __, message: is_sudo_user(message))
+BlacklistedFilter = create(lambda _, __, message: is_blacklisted_user(message))
 
 async def authorize(client: pyrogram.Client, message: pyrogram.types.Message) -> None:
-    """Authorize a user as a sudoer."""
-    try:
-        user_id = message.reply_to_message.from_user.id
-    except AttributeError:
+    user_id = message.reply_to_message.from_user.id if message.reply_to_message else None
+    if not user_id:
         await sendMessage(message, "Please reply to a user's message to authorize them as a sudoer.")
         return
-    await client.invoke(pyrogram.raw.functions.channels.EditBanned(
-        channel=message.chat.id,
-        user_id=user_id,
-        banned_rights=pyrogram.raw.types.ChatBannedRights(
+    try:
+        await client.edit_banned(
+            message.chat.id,
+            user_id,
             until_date=0,
-            view_messages=True,
-            send_messages=True,
-            send_media=True,
-            send_stickers=True,
-            send_gifs=True,
-            send_games=True,
-            send_inline=True,
-            embed_links=True
+            can_send_messages=True,
+            can_send_media_messages=True,
+            can_send_other_messages=True,
+            can_add_web_page_previews=True
         )
-    ))
-    user_data.add_sudoer(user_id)
-    await sendMessage(message, f"Successfully authorized {message.reply_to_message.from_user.mention} as a sudoer.")
+        user_data.add_sudoer(user_id)
+        await sendMessage(message, f"Successfully authorized {message.reply_to_message.from_user.mention} as a sudoer.")
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        await authorize(client, message)
 
 async def unauthorize(client: pyrogram.Client, message: pyrogram.types.Message) -> None:
-    """Unauthorize a user as a sudoer."""
-    try:
-        user_id = message.reply_to_message.from_user.id
-    except AttributeError:
+    user_id = message.reply_to_message.from_user.id if message.reply_to_message else None
+    if not user_id:
         await sendMessage(message, "Please reply to a user's message to unauthorize them as a sudoer.")
         return
-    await client.invoke(pyrogram.raw.functions.channels.EditBanned(
-        channel=message.chat.id,
-        user_id=user_id,
-        banned_rights=pyrogram.raw.types.ChatBannedRights(
+    try:
+        await client.edit_banned(
+            message.chat.id,
+            user_id,
             until_date=0,
-            view_messages=False,
-            send_messages=False,
-            send_media=False,
-            send_stickers=False,
-            send_gifs=False,
-            send_games=False,
-            send_inline=False,
-            embed_links=False
+            can_send_messages=False,
+            can_send_media_messages=False,
+            can_send_other_messages=False,
+            can_add_web_page_previews=False
         )
-    ))
-    user_data.remove_sudoer(user_id)
-    await sendMessage(message, f"Successfully unauthorized {message.reply_to_message.from_user.mention} as a sudoer.")
+        user_data.remove_sudoer(user_id)
+        await sendMessage(message, f"Successfully unauthorized {message.reply_to_message.from_user.mention} as a sudoer.")
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        await unauthorize(client, message)
 
 async def add_sudo(client: pyrogram.Client, message: pyrogram.types.Message) -> None:
-    """Add a user to the sudoers list."""
     user_id = message.text.split()[1]
     if user_id.isdigit():
         user_data.add_sudoer(int(user_id))
@@ -80,7 +71,6 @@ async def add_sudo(client: pyrogram.Client, message: pyrogram.types.Message) -> 
         await sendMessage(message, "Invalid user ID. Please provide a valid user ID to add to the sudoers list.")
 
 async def remove_sudo(client: pyrogram.Client, message: pyrogram.types.Message) -> None:
-    """Remove a user from the sudoers list."""
     user_id = message.text.split()[1]
     if user_id.isdigit():
         user_data.remove_sudoer(int(user_id))
@@ -89,7 +79,6 @@ async def remove_sudo(client: pyrogram.Client, message: pyrogram.types.Message) 
         await sendMessage(message, "Invalid user ID. Please provide a valid user ID to remove from the sudoers list.")
 
 async def add_blacklist(client: pyrogram.Client, message: pyrogram.types.Message) -> None:
-    """Add a user to the blacklist."""
     user_id = message.text.split()[1]
     if user_id.isdigit():
         user_data.add_blacklisted_user(int(user_id))
@@ -98,7 +87,6 @@ async def add_blacklist(client: pyrogram.Client, message: pyrogram.types.Message
         await sendMessage(message, "Invalid user ID. Please provide a valid user ID to add to the blacklist.")
 
 async def rm_blacklist(client: pyrogram.Client, message: pyrogram.types.Message) -> None:
-    """Remove a user from the blacklist."""
     user_id = message.text.split()[1]
     if user_id.isdigit():
         user_data.remove_blacklisted_user(int(user_id))
@@ -107,7 +95,6 @@ async def rm_blacklist(client: pyrogram.Client, message: pyrogram.types.Message)
         await sendMessage(message, "Invalid user ID. Please provide a valid user ID to remove from the blacklist.")
 
 async def black_listed(client: pyrogram.Client, message: pyrogram.types.Message) -> None:
-    """Respond to a message from a blacklisted user."""
     if await user_data.is_user_blacklisted(message.from_user.id):
         await sendMessage(message, "You are blacklisted and cannot send messages in this chat.")
         await deleteMessage(message)
