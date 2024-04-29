27e16102f117c8ac1ecfceb23a420a305763d676
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:56 +0600

    improve

--- bot/modules/category_select.py
+++ bot/modules/category_select.py
@@ -1,91 +1,75 @@
 #!/usr/bin/env python3
-from pyrogram.filters import command, regex
-from pyrogram.handlers import CallbackQueryHandler, MessageHandler
+import re
 from time import time
 
+from pyrogram.filters import command, regex
+from pyrogram.handlers import CallbackQueryHandler, MessageHandler
+from pyrogram.types import CallbackQuery
+
 from bot import bot, bot_cache, categories_dict, download_dict, download_dict_lock
-from bot.helper.ext_utils.bot_utils import MirrorStatus, arg_parser, fetch_user_tds, fetch_user_dumps, getDownloadByGid, is_gdrive_link, new_task, sync_to_async, get_readable_time
+from bot.helper.ext_utils.bot_utils import MirrorStatus, arg_parser, fetch_user_tds, fetch_user_dumps, get_download_by_gid, is_gdrive_link, new_task, sync_to_async, get_readable_time
 from bot.helper.ext_utils.help_messages import CATEGORY_HELP_MESSAGE
+from bot.helper.ext_utils.telegram_utils import edit_message, send_message, open_category_btns
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.button_build import ButtonMaker
 from bot.helper.telegram_helper.filters import CustomFilters
-from bot.helper.telegram_helper.message_utils import editMessage, sendMessage, open_category_btns
+from bot.helper.telegram_helper.message_utils import send_message as sendMessage
 
 
 async def change_category(client, message):
-    if not message.from_user:
-        return
     user_id = message.from_user.id
-
-    text = message.text.split('\n')
-    input_list = text[0].split(' ')
-
-    arg_base = {'link': '', 
-                '-id': '',
-                '-index': ''}
-
-    args = arg_parser(input_list[1:], arg_base)
-
+    args = arg_parser(message.text.split()[1:], {'link': '', '-id': '', '-index': ''})
+    gid = args['link']
     drive_id = args['-id']
     index_link = args['-index']
 
-    if drive_id and is_gdrive_link(drive_id):
-        drive_id = GoogleDriveHelper.getIdFromUrl(drive_id)
+    dl = get_download_by_gid(gid) if gid else None
+    if dl is None:
+        reply_dl = download_dict.get(message.reply_to_message.id)
+        if reply_dl is None:
+            await send_message(message, CATEGORY_HELP_MESSAGE)
+            return
+        dl = reply_dl
 
-    dl = None
-    if gid := args['link']:
-        dl = await getDownloadByGid(gid)
-        if not dl:
-            await sendMessage(message, f"GID: <code>{gid}</code> Not Found.")
-            return
-    if reply_to := message.reply_to_message:
-        async with download_dict_lock:
-            dl = download_dict.get(reply_to.id, None)
-        if not dl:
-            await sendMessage(message, "This is not an active task!")
-            return
-    if not dl:
-        await sendMessage(message, CATEGORY_HELP_MESSAGE)
+    if dl and dl.status() not in [MirrorStatus.STATUS_DOWNLOADING, MirrorStatus.STATUS_PAUSED, MirrorStatus.STATUS_QUEUED]:
+        await send_message(message, f'Task should be on {MirrorStatus.STATUS_DOWNLOADING} or {MirrorStatus.STATUS_PAUSED} or {MirrorStatus.STATUS_QUEUED}')
         return
-    if not await CustomFilters.sudo(client, message) and dl.message.from_user.id != user_id:
-        await sendMessage(message, "This task is not for you!")
+
+    if dl and not await CustomFilters.sudo(client, message) and dl.message.from_user.id != user_id:
+        await send_message(message, "This task is not for you!")
         return
-    if dl.status() not in [MirrorStatus.STATUS_DOWNLOADING, MirrorStatus.STATUS_PAUSED, MirrorStatus.STATUS_QUEUEDL]:
-        await sendMessage(message, f'Task should be on {MirrorStatus.STATUS_DOWNLOADING} or {MirrorStatus.STATUS_PAUSED} or {MirrorStatus.STATUS_QUEUEDL}')
-        return
-    listener = dl.listener() if dl and hasattr(dl, 'listener') else None
-    if listener and not listener.isLeech:
+
+    if dl and not dl.listener.isLeech:
         if not index_link and not drive_id and categories_dict:
             drive_id, index_link, is_cancelled = await open_category_btns(message)
         if is_cancelled:
             return
         if not index_link and not drive_id:
-            return await sendMessage(message, "Time out")
+            return await send_message(message, "Time out")
         msg = '<b>Task has been Updated Successfully!</b>'
         if drive_id:
-            if not (folder_name := await sync_to_async(GoogleDriveHelper().getFolderData, drive_id)):
-                return await sendMessage(message, "Google Drive id validation failed!!")
-            if listener.drive_id and listener.drive_id == drive_id:
+            if not (folder_name := await sync_to_async(GoogleDriveHelper().get_folder_data, drive_id)):
+                return await send_message(message, "Google Drive id validation failed!!")
+            if dl.listener.drive_id and dl.listener.drive_id == drive_id:
                 msg += f'\n\n<b>Folder name</b> : {folder_name} Already selected'
             else:
                 msg += f'\n\n<b>Folder name</b> : {folder_name}'
-            listener.drive_id = drive_id
+            dl.listener.drive_id = drive_id
         if index_link:
-            listener.index_link = index_link
+            dl.listener.index_link = index_link
             msg += f'\n\n<b>Index Link</b> : <code>{index_link}</code>'
-        return await sendMessage(message, msg)
+        return await send_message(message, msg)
     else:
-        await sendMessage(message, "Can not change Category for this task!")
+        await send_message(message, "Can not change Category for this task!")
 
 
 @new_task
-async def confirm_category(client, query):
+async def confirm_category(client, query: CallbackQuery):
     user_id = query.from_user.id
-    data = query.data.split(maxsplit=3)
+    data = re.split(r'\s+', query.data)
     msg_id = int(data[2])
     if msg_id not in bot_cache:
-        return await editMessage(query.message, '<b>Old Task</b>')
+        return await edit_message(query.message, '<b>Old Task</b>')
     elif user_id != int(data[1]) and not await CustomFilters.sudo(client, query):
         return await query.answer(text="This task is not for you!", show_alert=True)
     elif data[3] == "sdone":
@@ -109,16 +93,16 @@ async def confirm_category(client, query):
             buttons.ibutton(f'{"✅️" if cat_name == _name else ""} {_name}', f"scat {user_id} {msg_id} {_name.replace(' ', '_')}")
     buttons.ibutton('Cancel', f'scat {user_id} {msg_id} scancel', 'footer')
     buttons.ibutton(f'Done ({get_readable_time(60 - (time() - bot_cache[msg_id][4]))})', f'scat {user_id} {msg_id} sdone', 'footer')
-    await editMessage(query.message, f"<b>Select the category where you want to upload</b>\n\n<i><b>Upload Category:</b></i> <code>{cat_name}</code>\n\n<b>Timeout:</b> 60 sec", buttons.build_menu(3))
+    await edit_message(query.message, f"<b>Select the category where you want to upload</b>\n\n<i><b>Upload Category:</b></i> <code>{cat_name}</code>\n\n<b>Timeout:</b> 60 sec", buttons.build_menu(3))
 
 
 @new_task
-async def confirm_dump(client, query):
+async def confirm_dump(client, query: CallbackQuery):
     user_id = query.from_user.id
-    data = query.data.split(maxsplit=3)
+    data = re.split(r'\s+', query.data)
     msg_id = int(data[2])
     if msg_id not in bot_cache:
-        return await editMessage(query.message, '<b>Old Task</b>')
+        return await edit_message(query.message, '<b>Old Task</b>')
     elif user_id != int(data[1]) and not await CustomFilters.sudo(client, query):
         return await query.answer(text="This task is not for you!", show_alert=True)
     elif data[3] == "ddone":
@@ -139,7 +123,7 @@ async def confirm_dump(client, query):
     buttons.ibutton('Upload in All', f'dcat {user_id} {msg_id} All', 'header')
     buttons.ibutton('Cancel', f'dcat {user_id} {msg_id} dcancel', 'footer')
     buttons.ibutton(f'Done ({get_readable_time(60 - (time() - bot_cache[msg_id][3]))})', f'dcat {user_id} {msg_id} ddone', 'footer')
-    await editMessage(query.message, f"<b>Select the category where you want to upload</b>\n\n<i><b>Upload Category:</b></i> <code>{cat_name}</code>\n\n<b>Timeout:</b> 60 sec", buttons.build_menu(3))
+    await edit_message(query.message, f"<b>Select the category where you want to upload</b>\n\n<i><b>Upload Category:</b></i> <code>{cat_name}</code>\n\n<b>Timeout:</b> 60 sec", buttons.build_menu(3))
 
 
 bot.add_handler(MessageHandler(change_category, filters=command(BotCommands.CategorySelect) & CustomFilters.authorized))
