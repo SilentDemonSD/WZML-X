91f062125c7c8b6e1a35d6c45b6d841ba1e72e23
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:35:55 +0600

    comment

--- bot/version.py
+++ bot/version.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python3
-from typing import Tuple
-
 def get_version() -> str:
     '''
-    Returns the version details. Do not Interfere with this !
+    This function returns the version details of the software as a string in the format 'vMAJOR.MINOR.PATCH-STATE'.
+    The version is represented as a tuple of four elements: 'MAJOR', 'MINOR', 'PATCH', and 'STATE'.
+    The 'MAJOR', 'MINOR', and 'PATCH' elements represent the major, minor, and patch versions respectively,
+    while the 'STATE' element represents the state of the release (e.g. 'alpha', 'beta', 'rc', or 'final').
+    The function does not modify any external state and can be safely called without side effects.
 
     :return: The version details in the format 'vMAJOR.MINOR.PATCH-STATE'
     :rtype: str
     '''
     version: Tuple[str, str, str, str] = ('1', '3', '3', 'x')
     return f"v{version[0]}.{version[1]}.{version[2]}-{version[3]}"
-
-if __name__ == '__main__':
-    print(get_version())
