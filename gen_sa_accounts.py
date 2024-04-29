071e43b320a5f64519a53e92b15eb90264592792
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:51:34 +0600

    comment

--- gen_sa_accounts.py
+++ gen_sa_accounts.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
+# Import necessary modules
 import os
 import sys
 import time
 import json
 import pickle
 import argparse
-import base64
-import psutil
-from json import loads 
+import base6
