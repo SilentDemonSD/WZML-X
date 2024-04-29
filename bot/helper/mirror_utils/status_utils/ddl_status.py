390cd5b50691f8bfbf62e9ea4651ff287543ceee
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:51:46 +0600

    comment

--- bot/helper/mirror_utils/status_utils/ddl_status.py
+++ bot/helper/mirror_utils/status_utils/ddl_status.py
@@ -3,7 +3,10 @@ def get_readable_file_size(size: int) -> str:
     Returns the size of a file in a human-readable format. This function takes a file size in bytes as input
     and returns a string representation of the size in a more readable format, such as "10 KB" or "5 MB".
 
-    :param size: The size of the file in bytes. This must be an integer value.
+    :param size: The size of the file in bytes. This must be an integer value. The input value should be
+                  accurate and represent the actual size of the file in bytes.
+    :type size: int
     :return: A string representation of the file size in a human-readable format, such as "10 KB" or "5 MB".
+    :rtype: str
     """
     pass
