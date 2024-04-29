8bb031cf018ef97bbc7726b51c4ff16871a52b90
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:35:44 +0600

    improve

--- bot/helper/themes/__init__.py
+++ bot/helper/themes/__init__.py
@@ -1,29 +1,47 @@
 #!/usr/bin/env python3
-from os import listdir
+import os
+from os.path import join
 from importlib import import_module
 from random import choice as rchoice
 from bot import config_dict, LOGGER
 from bot.helper.themes import wzml_minimal
 
-AVL_THEMES = {}
-for theme in listdir('bot/helper/themes'):
+AVAILABLE_THEMES = {}
+theme_directory = join('bot', 'helper', 'themes')
+for theme in os.listdir(theme_directory):
     if theme.startswith('wzml_') and theme.endswith('.py'):
-        AVL_THEMES[theme[5:-3]] = import_module(f'bot.helper.themes.{theme[:-3]}')
+        theme_name = theme[5:-3]
+        AVAILABLE_THEMES[theme_name] = import_module(f'bot.helper.themes.{theme_name}')
 
-def BotTheme(var_name, **format_vars):
+def bot_theme(var_name: str, **format_vars: dict) -> str:
     text = None
-    theme_ = config_dict['BOT_THEME']
+    theme_ = config_dict.get('BOT_THEME')
 
-    if theme_ in AVL_THEMES:
-        text = getattr(AVL_THEMES[theme_].WZMLStyle(), var_name, None)
-        if text is None:
-            LOGGER.error(f"{var_name} not Found in {theme_}. Please recheck with Official Repo")
+    if theme_ and theme_ in AVAILABLE_THEMES:
+        theme_module = AVAILABLE_THEMES[theme_]
+        if hasattr(theme_module, 'WZMLStyle'):
+            style_module = getattr(theme_module, 'WZMLStyle')
+            if hasattr(style_module(), var_name):
+                text = getattr(style_module(), var_name)
+                return text.format_map(format_vars)
+            else:
+                LOGGER.error(f"{var_name} not found in {theme_}. Please recheck with Official Repo")
     elif theme_ == 'random':
-        rantheme = rchoice(list(AVL_THEMES.values()))
-        LOGGER.info(f"Random Theme Chosen: {rantheme}")
-        text = getattr(rantheme.WZMLStyle(), var_name, None)
-        
-    if text is None:
-        text = getattr(wzml_minimal.WZMLStyle(), var_name)
+        available_themes = list(AVAILABLE_THEMES.values())
+        rtheme = rchoice(available_themes)
+        LOGGER.info(f"Random Theme Chosen: {rtheme}")
+        if hasattr(rtheme, 'WZMLStyle'):
+            style_module = getattr(rtheme, 'WZMLStyle')
+            if hasattr(style_module(), var_name):
+                text = getattr(style_module(), var_name)
+                return text.format_map(format_vars)
+    else:
+        if hasattr(wzml_minimal, 'WZMLStyle'):
+            style_module = getattr(wzml_minimal, 'WZMLStyle')
+            if hasattr(style_module(), var_name):
+                text = getattr(style_module(), var_name)
+                return text.format_map(format_vars)
 
-    return text.format_map(format_vars)
+    LOGGER.error(f"{var_name} not found in any available themes. Please recheck with Official Repo")
+    return None
+
