8f4d9174c1abb07001d04c353f834cb6fd989977
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:35 +0600

    improve

--- bot/helper/mirror_utils/download_utils/rclone_download.py
+++ bot/helper/mirror_utils/download_utils/rclone_download.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
-from asyncio import gather
-from json import loads
-from random import SystemRandom
-from string import ascii_letters, digits
+import asyncio
+import json
+import random
+from typing import List, Tuple, Dict, Any, Optional
 
-from bot import download_dict, download_dict_lock, queue_dict_lock, non_queued_dl, LOGGER
+import bot
 from bot.helper.ext_utils.bot_utils import cmd_exec
 from bot.helper.telegram_helper.message_utils import sendMessage, sendStatusMessage
 from bot.helper.ext_utils.task_manager import is_queued, stop_duplicate_check
@@ -12,28 +11,45 @@ from bot.helper.mirror_utils.status_utils.rclone_status import RcloneStatus
 from bot.helper.mirror_utils.status_utils.queue_status import QueueStatus
 from bot.helper.mirror_utils.rclone_utils.transfer import RcloneTransferHelper
 
+# Function to execute shell commands
+async def cmd_exec_safe(cmd: List[str]) -> Tuple[str, int]:
+    result, err_code = await cmd_exec(cmd)
+    if err_code != 0:
+        raise Exception(f'Error while executing command: {cmd}\nError: {result[:4000]}')
+    return result, err_code
 
-async def add_rclone_download(rc_path, config_path, path, name, listener):
-    remote, rc_path = rc_path.split(':', 1)
-    rc_path = rc_path.strip('/')
-
+# Function to get rclone stats and size
+async def get_rclone_stats_and_size(
+        remote: str, rc_path: str, config_path: str) -> Tuple[Dict[str, Any], int]:
     cmd1 = ['rclone', 'lsjson', '--fast-list', '--stat', '--no-mimetype',
             '--no-modtime', '--config', config_path, f'{remote}:{rc_path}']
     cmd2 = ['rclone', 'size', '--fast-list', '--json',
             '--config', config_path, f'{remote}:{rc_path}']
-    res1, res2 = await gather(cmd_exec(cmd1), cmd_exec(cmd2))
-    if res1[2] != res2[2] != 0:
-        if res1[2] != -9:
-            err = res1[1] or res2[1]
+    res1, res2 = await gather(cmd_exec_safe(cmd1), cmd_exec_safe(cmd2))
+    return loads(res1[0]), res2[1]
+
+# Function to add rclone download
+async def add_rclone_download(rc_path: str, config_path: str, path: str, name: Optional[str], listener):
+    remote, rc_path = rc_path.split(':', 1)
+    rc_path = rc_path.strip('/')
+
+    rstat, rstat_err_code = get_rclone_stats_and_size(remote, rc_path, config_path)
+    rsize, rsize_err_code = get_rclone_stats_and_size(remote, rc_path, config_path)
+
+    if rstat_err_code != rsize_err_code != 0:
+        if rstat_err_code != -9:
+            err = f'{res1[1]} {res2[1]}'
             msg = f'Error: While getting rclone stat/size. Path: {remote}:{rc_path}. Stderr: {err[:4000]}'
             await sendMessage(listener.message, msg)
         return
+
     try:
         rstat = loads(res1[0])
         rsize = loads(res2[0])
     except Exception as err:
         await sendMessage(listener.message, f'RcloneDownload JsonLoad: {err}')
         return
+
     if rstat['IsDir']:
         if not name:
             name = rc_path.rsplit('/', 1)[-1] if rc_path else remote
@@ -41,7 +57,7 @@ async def add_rclone_download(rc_path, config_path, path, name, listener):
     else:
         name = rc_path.rsplit('/', 1)[-1]
     size = rsize['bytes']
-    gid = ''.join(SystemRandom().choices(ascii_letters + digits, k=12))
+    gid = ''.join(random.SystemRandom().choices(ascii_letters + digits, k=12))
 
     msg, button = await stop_duplicate_check(name, listener)
     if msg:
