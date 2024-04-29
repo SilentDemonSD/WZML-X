8a7ce5b91613a2c8fe55a685c50388a1cd99de3e
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:36:57 +0600

    improve

--- bot/helper/mirror_utils/status_utils/rclone_status.py
+++ bot/helper/mirror_utils/status_utils/rclone_status.py
@@ -4,11 +4,32 @@ class RcloneStatus:
     """Class to represent the status of an rclone operation."""
 
     def __init__(self, obj, message, gid, status, upload_details):
-        self.__obj = obj
-        self.__gid = gid
-        self.__status = status
+        self.obj = obj
+        self.gid = gid
+        self.status = status
         self.message = message
         self.upload_details = upload_details
 
     @property
+    def engine_status(self):
+        """Returns the engine status as an EngineStatus object."""
+        if self.status == 'completed':
+            return EngineStatus.COMPLETED
+        elif self.status == 'failed':
+            return EngineStatus.FAILED
+        else:
+            return EngineStatus.IN_PROGRESS
 
+    @property
+    def mirror_status(self):
+        """Returns the mirror status as a MirrorStatus object."""
+        if self.status == 'completed':
+            return MirrorStatus.MIRROR_COMPLETED
+        elif self.status == 'failed':
+            return MirrorStatus.MIRROR_FAILED
+        else:
+            return MirrorStatus.MIRROR_IN_PROGRESS
+
+    def __str__(self):
+        """Returns a string representation of the RcloneStatus object."""
+        return f"RcloneStatus(obj={self.obj}, gid={self.gid}, status={self.status}, message={self.message}, upload_details={self.upload_details})"
