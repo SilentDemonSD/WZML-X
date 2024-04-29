5420d50f20d40c49d229052aeff820683b1c86f1
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:36:13 +0600

    improve

--- bot/helper/mirror_utils/status_utils/rclone_status.py
+++ bot/helper/mirror_utils/status_utils/rclone_status.py
@@ -1,7 +1,8 @@
 from bot.helper.ext_utils.bot_utils import EngineStatus, MirrorStatus
 
-
 class RcloneStatus:
+    """Class to represent the status of an rclone operation."""
+
     def __init__(self, obj, message, gid, status, upload_details):
         self.__obj = obj
         self.__gid = gid
@@ -9,38 +10,5 @@ class RcloneStatus:
         self.message = message
         self.upload_details = upload_details
 
-    def gid(self):
-        return self.__gid
+    @property
 
-    def progress(self):
-        return self.__obj.percentage
-
-    def speed(self):
-        return self.__obj.speed
-
-    def name(self):
-        return self.__obj.name
-
-    def size(self):
-        return self.__obj.size
-
-    def eta(self):
-        return self.__obj.eta
-
-    def status(self):
-        if self.__status == 'dl':
-            return MirrorStatus.STATUS_DOWNLOADING
-        elif self.__status == 'up':
-            return MirrorStatus.STATUS_UPLOADING
-        else:
-            return MirrorStatus.STATUS_CLONING
-
-    def processed_bytes(self):
-        return self.__obj.transferred_size
-
-    def download(self):
-        return self.__obj
-
-
-    def eng(self):
-        return EngineStatus().STATUS_RCLONE
