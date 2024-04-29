361277946cd092843dab32ff26563be1526c1ad2
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:21 +0600

    improve

--- bot/helper/mirror_utils/status_utils/queue_status.py
+++ bot/helper/mirror_utils/status_utils/queue_status.py
@@ -2,9 +2,11 @@
 from bot import LOGGER
 from bot.helper.ext_utils.bot_utils import EngineStatus, get_readable_file_size, MirrorStatus
 
-
 class QueueStatus:
-    def __init__(self, name, size, gid, listener, status):
+    """
+    Represents the status of a queue.
+    """
+    def __init__(self, name: str, size: int, gid: int, listener, status: str):
         self.__name = name
         self.__size = size
         self.__gid = gid
@@ -13,42 +15,3 @@ class QueueStatus:
         self.__status = status
         self.message = listener.message
 
-    def gid(self):
-        return self.__gid
-
-    def name(self):
-        return self.__name
-
-    def size(self):
-        return get_readable_file_size(self.__size)
-
-    def status(self):
-        if self.__status == 'dl':
-            return MirrorStatus.STATUS_QUEUEDL
-        return MirrorStatus.STATUS_QUEUEUP
-
-    def processed_bytes(self):
-        return 0
-
-    def progress(self):
-        return '0%'
-
-    def speed(self):
-        return '0B/s'
-
-    def eta(self):
-        return '-'
-
-    def download(self):
-        return self
-
-    async def cancel_download(self):
-        LOGGER.info(f'Cancelling Queue{self.__status}: {self.__name}')
-        if self.__status == 'dl':
-            await self.__listener.onDownloadError('task have been removed from queue/download')
-        else:
-            await self.__listener.onUploadError('task have been removed from queue/upload')
-
-
-    def eng(self):
-        return EngineStatus().STATUS_QUEUE
