928f48622f652a15883dc1f8b9ad380290aa7838
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:52:29 +0600

    improve

--- aria.sh
+++ aria.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-# Function to download the tracker list
-# This function accepts two arguments:
-# $1: The URL of the tracker list
+# Function to download a file
+# This function accepts three arguments:
+# $1: The URL of the file to download
 # $2: The output file path
-# It downloads the tracker list using curl and saves it to the specified output file.
-# If the download is not successful, the function returns 1.
-download_tracker_list() {
+# $3: The optional expected file size, used to check if the download was successful
+# It downloads the file using curl and saves it to the specified output file.
+# If the download is not successful or the file size does not match the expected size,
+# the function returns 1.
+download_file() {
   local url="$1"
   local output_file="$2"
+  local expected_size="$3"
 
   if ! curl -Ns "$url" -o "$output_file"; then
+    echo "Error: Failed to download $url"
+    return 1
+  fi
+
+  if [ -n "$expected_size" ] && [ "$(stat -c%s "$output_file")" -ne "$expected_size" ]; then
+    echo "Error: Downloaded file size does not match expected size"
     return 1
   fi
 
@@ -18,7 +27,9 @@ download_tracker_list() {
 }
 
 # Download the tracker list from the given URL and save it to /tmp/trackers.txt
-if ! download_tracker_list "https://ngosang.github.io/trackerslist/trackers_all_http.txt" "/tmp/trackers.txt"; then
+TRACKERS_URL="https://ngosang.github.io/trackerslist/trackers_all_http.txt"
+TRACKERS_FILE="/tmp/trackers.txt"
+if ! download_file "$TRACKERS_URL" "$TRACKERS_FILE" ""; then
   echo "Error: Failed to download tracker list"
   exit 1
 fi
