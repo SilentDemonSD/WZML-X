5345c89c5edbc659470a2c535c98c6af2f500d18
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:45 +0600

    improve

--- bot/helper/ext_utils/task_manager.py
+++ bot/helper/ext_utils/task_manager.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-from typing import List, Dict, Union, Tuple, Optional, AsyncContextManager, Callable, Coroutine, Awaitable
-import time
-from asyncio import Event, Lock
-import aiohttp
 import asyncio
 import gdown
 import os
@@ -10,8 +6,9 @@ import re
 import shutil
 import tarfile
 import zipfile
-from urllib.parse import urlparse
+import urllib.parse
 
+import aiohttp
 import requests
 from bs4 import BeautifulSoup
 from googleapiclient.discovery import build
@@ -19,16 +16,11 @@ from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaIoBaseDownload
 from telegram import Update, Bot, Message, Chat, User, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from telegram.error import TelegramError, NetworkError, Unauthorized, ChatMigrated, BadRequest
-from telegram.ext import CommandHandler, Filters, CallbackContext, run_async, ContextTypes
+from telegram.ext import CommandHandler, Filters, CallbackContext, run_async
 from telegram.utils.helpers import escape_markdown
+from typing import List, Dict, Union, Tuple, Optional, AsyncContextManager, Callable, Coroutine, Awaitable
 
-from bot import bot_cache, config_dict, queued_dl, queued_up, non_queued_up, non_queued_dl, queue_dict_lock, LOGGER, user_data, download_dict
-from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-from bot.helper.ext_utils.fs_utils import get_base_name, check_storage_threshold
-from bot.helper.ext_utils.bot_utils import get_user_tasks, getdailytasks, sync_to_async, get_telegraph_list, get_readable_file_size, checking_access, get_readable_time
-from bot.helper.telegram_helper.message_utils import forcesub, check_botpm
-from bot.helper.telegram_helper.filters import CustomFilters
-from bot.helper.themes import BotTheme
+# Add necessary imports here
 
 ...
 
