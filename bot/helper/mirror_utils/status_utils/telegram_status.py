8568bf29f4ee1fac1856c35149c85e4eb27f0c52
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:37:01 +0600

    improve

--- bot/helper/mirror_utils/status_utils/telegram_status.py
+++ bot/helper/mirror_utils/status_utils/telegram_status.py
@@ -8,7 +8,7 @@ class TelegramStatus:
     Class representing the status of a file transfer in Telegram.
     """
 
-    def __init__(self, obj, size, message, gid, status, upload_details):
+    def __init__(self, obj, size: int, message, gid: str, status: str, upload_details):
         self.__obj = obj
         self.__size = size
         self.__gid = gid
@@ -17,48 +17,46 @@ class TelegramStatus:
         self.message = message
 
     @property
-    def processed_bytes(self):
+    def processed_bytes(self) -> str:
         """
         Returns the number of processed bytes in a readable format.
         """
         return get_readable_file_size(self.__obj.processed_bytes)
 
     @property
-    def size(self):
+    def size(self) -> str:
         """
         Returns the total size of the file in a readable format.
         """
         return get_readable_file_size(self.__size)
 
     @property
-    def status(self):
+    def status(self) -> MirrorStatus:
         """
         Returns the status of the file transfer.
         """
         return MirrorStatus.STATUS_UPLOADING if self.__status == 'up' else MirrorStatus.STATUS_DOWNLOADING
 
     @property
-    def name(self):
+    def name(self) -> str:
         """
         Returns the name of the file.
         """
         return self.__obj.name
 
     @property
-    def progress(self):
+    def progress(self) -> str:
         """
         Returns the progress of the file transfer as a percentage.
         """
         try:
             progress_raw = self.__obj.processed_bytes / self.__size * 100
-            if math.isclose(progress_raw, 100.0):
-                return '100.00%'
             return f'{progress_raw:.2f}%'
         except:
             return '0.00%'
 
     @property
-    def speed(self):
+    def speed(self) -> str:
         """
         Returns the speed of the file transfer in a readable format.
         """
@@ -71,7 +69,7 @@ class TelegramStatus:
             return '-'
 
     @property
-    def eta(self):
+    def eta(self) -> str:
         """
         Returns the estimated time of arrival in a readable format.
         """
@@ -90,14 +88,14 @@ class TelegramStatus:
         """
         return self.__gid
 
-    def download(self):
+    def download(self) -> object:
         """
         Returns the file object for downloading.
         """
         return self.__obj
 
     @property
-    def eng(self):
+    def eng(self) -> EngineStatus:
         """
         Returns the engine status for Telegram.
         """
