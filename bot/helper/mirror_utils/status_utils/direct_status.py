37c7ddd7aefac552a361062d7ab560959796c21b
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:45:09 +0600

    improve

--- bot/helper/mirror_utils/status_utils/direct_status.py
+++ bot/helper/mirror_utils/status_utils/direct_status.py
@@ -10,76 +10,87 @@ from bot.helper.ext_utils.bot_utils import (
 class DirectStatus:
     def __init__(
         self,
-        obj,
-        gid,
-        listener,
-        upload_details,
+        file_info: object,
+        file_global_id: str,
+        listener: object,
+        upload_details: object,
     ):
         """
         Initialize the DirectStatus class.
 
-        :param obj: The object containing the file information
-        :param gid: The global ID of the file
+        :param file_info: The object containing the file information
+        :param file_global_id: The global ID of the file
         :param listener: The listener object
         :param upload_details: The upload details
         """
-        self.gid = gid
+        self.file_global_id = file_global_id
         self.listener = listener
-        self.obj = obj
+        self.file_info = file_info
         self.upload_details = upload_details
         self.message = listener.message
 
-    def gid(self) -> str:
+    @property
+    def file_global_id(self) -> str:
         """
         Get the global ID of the file.
 
         :return: The global ID of the file
         """
-        return self.__gid
+        return self.__file_global_id
 
-    def progress_raw(self) -> float:
+    @file_global_id.setter
+    def file_global_id(self, value: str):
+        self.__file_global_id = value
+
+    @property
+    def progress(self) -> float:
         """
         Get the progress of the file in percentage.
 
         :return: The progress of the file in percentage
         """
         try:
-            return self.obj.processed_bytes / self.obj.total_size * 100
+            return self.file_info.processed_bytes / self.file_info.total_size * 100
         except:
             return 0
 
-    def progress(self) -> str:
+    @property
+    def progress_str(self) -> str:
         """
         Get the progress of the file in percentage as a formatted string.
 
         :return: The progress of the file in percentage as a formatted string
         """
-        return f"{round(self.progress_raw(), 2)}%"
+        return f"{self.progress:.2f}%"
 
+    @property
     def speed(self) -> str:
         """
         Get the speed of the file transfer.
 
         :return: The speed of the file transfer
         """
-        return f"{get_readable_file_size(self.obj.speed)}/s"
+        return f"{get_readable_file_size(self.file_info.speed)}/s"
 
+    @property
     def name(self) -> str:
         """
         Get the name of the file.
 
         :return: The name of the file
         """
-        return self.obj.name
+        return self.file_info.name
 
+    @property
     def size(self) -> str:
         """
         Get the size of the file.
 
         :return: The size of the file
         """
-        return get_readable_file_size(self.obj.total_size)
+        return get_readable_file_size(self.file_info.total_size)
 
+    @property
     def eta(self) -> str:
         """
         Get the estimated time of arrival.
@@ -87,38 +98,42 @@ class DirectStatus:
         :return: The estimated time of arrival
         """
         try:
-            seconds = (self.obj.total_size - self.obj.processed_bytes) / self.obj.speed
-            return get_readable_time(seconds)
+            time_left = (self.file_info.total_size - self.file_info.processed_bytes) / self.file_info.speed
+            return get_readable_time(time_left)
         except:
             return "-"
 
+    @property
     def status(self) -> MirrorStatus:
         """
         Get the status of the file transfer.
 
         :return: The status of the file transfer
         """
-        if self.obj.task and self.obj.task.is_waiting:
+        if self.file_info.task and self.file_info.task.is_waiting:
             return MirrorStatus.STATUS_QUEUED
         return MirrorStatus.STATUS_DOWNLOADING
 
+    @property
     def processed_bytes(self) -> str:
         """
         Get the number of processed bytes.
 
         :return: The number of processed bytes
         """
-        return get_readable_file_size(self.obj.processed_bytes)
+        return get_readable_file_size(self.file_info.processed_bytes)
 
-    def download(self) -> object:
+    @property
+    def file_obj(self) -> object:
         """
         Get the file object.
 
         :return: The file object
         """
-        return self.obj
+        return self.file_info
 
-    def eng(self) -> EngineStatus:
+    @property
+    def engine_status(self) -> EngineStatus:
         """
         Get the engine status.
 
@@ -134,19 +149,19 @@ class DirectStatus:
         """
         return (
             f"DirectStatus(\n"
-            f"    gid={self.gid},\n"
+            f"    file_global_id={self.file_global_id},\n"
             f"    listener={self.listener},\n"
-            f"    obj={self.obj},\n"
+            f"    file_info={self.file_info},\n"
             f"    upload_details={self.upload_details},\n"
             f"    message={self.message},\n"
-            f"    progress={self.progress},\n"
+            f"    progress={self.progress_str},\n"
             f"    speed={self.speed},\n"
             f"    name={self.name},\n"
             f"    size={self.size},\n"
             f"    eta={self.eta},\n"
             f"    status={self.status},\n"
             f"    processed_bytes={self.processed_bytes},\n"
-            f"    download={self.download},\n"
-            f"    eng={self.eng}\n"
+            f"    file_obj={self.file_obj},\n"
+            f"    engine_status={self.engine_status}\n"
             f")"
         )
