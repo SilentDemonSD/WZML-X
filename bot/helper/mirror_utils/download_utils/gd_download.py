5e73c4452a72ba53e134cb1fa121c44a350db324
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:37:04 +0600

    improve

--- bot/helper/mirror_utils/download_utils/gd_download.py
+++ bot/helper/mirror_utils/download_utils/gd_download.py
@@ -3,6 +3,7 @@
 import json
 import secrets
 import cloudscraper
+from typing import Any, Dict, Union
 from bot import download_dict, download_dict_lock, LOGGER, non_queued_dl, queue_dict_lock
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot.helper.mirror_utils.status_utils.gdrive_status import GdriveStatus
@@ -12,68 +13,77 @@ from bot.helper.ext_utils.bot_utils import sync_to_async, get_readable_file_size
 from bot.helper.ext_utils.task_manager import is_queued, limit_checker, stop_duplicate_check
 
 # Function to add a download from Google Drive
-async def add_gd_download(link, path, listener, newname, org_link):
-    # Create a Google Drive Helper object
+async def add_gd_download(
+    link: str,
+    path: str,
+    listener: Any,
+    newname: Union[str, None],
+    org_link: str,
+) -> None:
+    """
+    Add a download from Google Drive.
+
+    :param link: Google Drive link
+    :param path: Path to save the file
+    :param listener: Listener object
+    :param newname: Optional new name for the file
+    :param org_link: Original Google Drive link
+    """
     drive = GoogleDriveHelper()
-    
-    # Get the name, mime type, size, and other information about the file
     name, mime_type, size, _, _ = await sync_to_async(drive.count, link)
-    
-    # If the link is a share link, send a request to a specific endpoint
+
     if is_share_link(org_link):
-        scraper = cget()
-        scraper.request('POST', "https://wzmlcontribute.vercel.app/contribute", headers={"Content-Type": "application/json"}, data=jdumps({"name": name, "link": org_link, "size": get_readable_file_size(size)}))
-    
-    # If the mime type is None, send a message and return
+        scraper = cloudscraper.create_scraper()
+        scraper.request(
+            "POST",
+            "https://wzmlcontribute.vercel.app/contribute",
+            headers={"Content-Type": "application/json"},
+            data=json.dumps({"name": name, "link": org_link, "size": get_readable_file_size(size)}),
+        )
+
     if mime_type is None:
         await sendMessage(listener.message, name)
         return
-    
-    # Set the name of the file, and get a unique identifier for it
+
     name = newname or name
-    gid = token_hex(5)
-    
-    # Check for duplicate files and send a message if necessary
+    gid = secrets.token_hex(5)
+
     msg, button = await stop_duplicate_check(name, listener)
     if msg:
         await sendMessage(listener.message, msg, button)
         return
-    
-    # Check if the user has exceeded their download limit and send a message if necessary
-    if limit_exceeded := await limit_checker(size, listener, isDriveLink=True):
+
+    if limit_exceeded := await limit_checker(size, listener, is_drive_link=True):
         await sendMessage(listener.message, limit_exceeded)
         return
-    
-    # Check if the user is already in the download queue and set the "added_to_queue" variable accordingly
+
     added_to_queue, event = await is_queued(listener.uid)
     if added_to_queue:
         LOGGER.info(f"Added to Queue/Download: {name}")
-        async with download_dict_lock:
+        with download_dict_lock:
             download_dict[listener.uid] = QueueStatus(
-                name, size, gid, listener, 'dl')
+                name, size, gid, listener, "dl"
+            )
         await listener.onDownloadStart()
         await sendStatusMessage(listener.message)
         await event.wait()
-        async with download_dict_lock:
+        with download_dict_lock:
             if listener.uid not in download_dict:
                 return
         from_queue = True
     else:
         from_queue = False
-    
-    # Create a Google Drive Helper object with the specified name, path, and listener
-    drive = GoogleDriveHelper(name, path, listener)
-    
-    # Add the download to the download dictionary and set the status to "dl"
-    async with download_dict_lock:
-        download_dict[listener.uid] = GdriveStatus(
-            drive, size, listener.message, gid, 'dl', listener.upload_details)
 
-    # Add the download to the set of non-queued downloads
-    async with queue_dict_lock:
+    drive = GoogleDriveHelper(name, path, listener)
+
+    with download_dict_lock:
+        download_dict[listener.uid] = GdriveStatus(
+            drive, size, listener.message, gid, "dl", listener.upload_details
+        )
+
+    with queue_dict_lock:
         non_queued_dl.add(listener.uid)
 
-    # If the download was added to the queue, log a message
     if from_queue:
         LOGGER.info(f'Start Queued Download from GDrive: {name}')
     else:
@@ -81,5 +91,4 @@ async def add_gd_download(link, path, listener, newname, org_link):
         await listener.onDownloadStart()
         await sendStatusMessage(listener.message)
 
-    # Download the file from Google Drive
     await sync_to_async(drive.download, link)
