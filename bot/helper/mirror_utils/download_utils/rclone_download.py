a10e03a13fa4d81f5f4a938a59b58d043a499d4e
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:35:48 +0600

    improve

--- bot/helper/mirror_utils/download_utils/rclone_download.py
+++ bot/helper/mirror_utils/download_utils/rclone_download.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-from asyncio import gather
-from json import loads
-from secrets import token_hex
+import asyncio
+import json
+import logging
+import secrets
+from typing import Any, Coroutine, List, Tuple
 
+import bot
 from bot import download_dict, download_dict_lock, queue_dict_lock, non_queued_dl, LOGGER
 from bot.helper.ext_utils.bot_utils import cmd_exec
 from bot.helper.telegram_helper.message_utils import sendMessage, sendStatusMessage
@@ -11,8 +14,21 @@ from bot.helper.mirror_utils.status_utils.rclone_status import RcloneStatus
 from bot.helper.mirror_utils.status_utils.queue_status import QueueStatus
 from bot.helper.mirror_utils.rclone_utils.transfer import RcloneTransferHelper
 
+logger = logging.getLogger(__name__)
 
-async def add_rclone_download(rc_path, config_path, path, name, listener):
+async def add_rclone_download(
+    rc_path: str, config_path: str, path: str, name: str, listener
+) -> Coroutine[Any, Any, None]:
+    """
+    Adds a new rclone download to the download queue.
+
+    :param rc_path: The rclone path in the format 'remote:path'
+    :param config_path: The path to the rclone config file
+    :param path: The path to save the downloaded file
+    :param name: The name of the downloaded file
+    :param listener: The listener object for the download
+    :return: None
+    """
     remote, rc_path = rc_path.split(':', 1)
     rc_path = rc_path.strip('/')
 
@@ -20,7 +36,7 @@ async def add_rclone_download(rc_path, config_path, path, name, listener):
             '--no-modtime', '--config', config_path, f'{remote}:{rc_path}']
     cmd2 = ['rclone', 'size', '--fast-list', '--json',
             '--config', config_path, f'{remote}:{rc_path}']
-    res1, res2 = await gather(cmd_exec(cmd1), cmd_exec(cmd2))
+    res1, res2 = await asyncio.gather(cmd_exec(cmd1), cmd_exec(cmd2))
     if res1[2] != res2[2] != 0:
         if res1[2] != -9:
             err = res1[1] or res2[1]
@@ -28,8 +44,8 @@ async def add_rclone_download(rc_path, config_path, path, name, listener):
             await sendMessage(listener.message, msg)
         return
     try:
-        rstat = loads(res1[0])
-        rsize = loads(res2[0])
+        rstat = json.loads(res1[0])
+        rsize = json.loads(res2[0])
     except Exception as err:
         await sendMessage(listener.message, f'RcloneDownload JsonLoad: {err}')
         return
@@ -40,7 +56,7 @@ async def add_rclone_download(rc_path, config_path, path, name, listener):
     else:
         name = rc_path.rsplit('/', 1)[-1]
     size = rsize['bytes']
-    gid = token_hex(5)
+    gid = secrets.token_hex(5)
     msg, button = await stop_duplicate_check(name, listener)
     if msg:
         await sendMessage(listener.message, msg, button)
@@ -48,7 +64,7 @@ async def add_rclone_download(rc_path, config_path, path, name, listener):
 
     added_to_queue, event = await is_queued(listener.uid)
     if added_to_queue:
-        LOGGER.info(f"Added to Queue/Download: {name}")
+        logger.info(f"Added to Queue/Download: {name}")
         async with download_dict_lock:
             download_dict[listener.uid] = QueueStatus(
                 name, size, gid, listener, 'dl')
@@ -70,10 +86,10 @@ async def add_rclone_download(rc_path, config_path, path, name, listener):
         non_queued_dl.add(listener.uid)
 
     if from_queue:
-        LOGGER.info(f'Start Queued Download with rclone: {rc_path}')
+        logger.info(f'Start Queued Download with rclone: {rc_path}')
     else:
         await listener.onDownloadStart()
         await sendStatusMessage(listener.message)
-        LOGGER.info(f"Download with rclone: {rc_path}")
+        logger.info(f"Download with rclone: {rc_path}")
 
     await RCTransfer.download(remote, rc_path, config_path, path)
