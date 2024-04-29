04714c6f08ef6cf17437c418992a3c7455ade38c
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:50 +0600

    improve

--- bot/modules/bt_select.py
+++ bot/modules/bt_select.py
@@ -1,124 +1,143 @@
-from telegram.ext import CommandHandler, CallbackQueryHandler
-from os import remove, path as ospath
+from typing import List, Union
 
-from bot import aria2, download_dict, dispatcher, download_dict_lock, OWNER_ID, user_data, LOGGER
+import os
+import logging
+import telegram
+from telegram.ext import CommandHandler, CallbackQueryHandler
+from telegram.error import TelegramError
+
+import aria2p
+from aria2p.client import Aria2Client
+from aria2p.protocol.event import Event
+
+from bot import dispatcher, download_dict, download_dict_lock, OWNER_ID, user_data, LOGGER
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import sendMessage, sendStatusMessage
-from bot.helper.ext_utils.bot_utils import getDownloadByGid, MirrorStatus, bt_selection_buttons
+from bot.helper.ext_utils.bot_utils import get_download_by_gid, MirrorStatus, bt_selection_buttons
 
-def select(update, context):
-    user_id = update.message.from_user.id
-    if len(context.args) == 1:
-        gid = context.args[0]
-        dl = getDownloadByGid(gid)
-        if not dl:
-            sendMessage(f"GID: <code>{gid}</code> Not Found.", context.bot, update.message)
-            return
+logger = logging.getLogger(__name__)
+
+async def select_download(update: telegram.Update, context: telegram.ext.CallbackContext) -> None:
+    user_id = update.effective_user.id
+    args = context.args
+
+    if len(args) == 1:
+        gid = args[0]
+        download_info = get_download_by_gid(gid)
     elif update.message.reply_to_message:
-        mirror_message = update.message.reply_to_message
+        reply_message = update.message.reply_to_message
         with download_dict_lock:
-            if mirror_message.message_id in download_dict:
-                dl = download_dict[mirror_message.message_id]
+            if reply_message.message_id in download_dict:
+                download_info = download_dict[reply_message.message_id]
             else:
-                dl = None
-        if not dl:
-            sendMessage("This is not an active task!", context.bot, update.message)
-            return
-    elif len(context.args) == 0:
-        msg = "Reply to an active /cmd which was used to start the qb-download or add gid along with cmd\n\n"
-        msg += "This command mainly for selection incase you decided to select files from already added torrent. "
-        msg += "But you can always use /cmd with arg `s` to select files before download start."
+                sendMessage("This is not an active task!", context.bot, update.message)
+                return
+    elif len(args) == 0:
+        msg = (
+            "Reply to an active /cmd which was used to start the qb-download or add gid along with cmd\n\n"
+            "This command mainly for selection incase you decided to select files from already added torrent. "
+            "But you can always use /cmd with arg `s` to select files before download start."
+        )
         sendMessage(msg, context.bot, update.message)
         return
+    else:
+        sendMessage("Invalid number of arguments!", context.bot, update.message)
+        return
 
-    if OWNER_ID != user_id and dl.message.from_user.id != user_id and \
-       (user_id not in user_data or not user_data[user_id].get('is_sudo')):
+    if (
+        OWNER_ID != user_id
+        and download_info.message.from_user.id != user_id
+        and (user_id not in user_data or not user_data[user_id].get("is_sudo"))
+    ):
         sendMessage("This task is not for you!", context.bot, update.message)
         return
-    if dl.status() not in [MirrorStatus.STATUS_DOWNLOADING, MirrorStatus.STATUS_PAUSED, MirrorStatus.STATUS_QUEUEDL]:
-        sendMessage('Task should be in download or pause (incase message deleted by wrong) or queued (status incase you used torrent file)!', context.bot, update.message)
+
+    if download_info.status not in [
+        MirrorStatus.STATUS_DOWNLOADING,
+        MirrorStatus.STATUS_PAUSED,
+        MirrorStatus.STATUS_QUEUED,
+    ]:
+        sendMessage(
+            'Task should be in download or pause (incase message deleted by wrong) or queued (status incase you used torrent file)!',
+            context.bot,
+            update.message,
+        )
         return
-    if dl.name().startswith('[METADATA]'):
-        sendMessage('Try after downloading metadata finished!', context.bot, update.message)
+
+    if download_info.name.startswith("[METADATA]"):
+        sendMessage("Try after downloading metadata finished!", context.bot, update.message)
         return
 
     try:
-        listener = dl.listener()
-        if listener.isQbit:
-            id_ = dl.hash()
-            client = dl.client()
-            client.torrents_pause(torrent_hashes=id_)
+        if download_info.is_qbit:
+            aria2 = Aria2Client()
+            aria2.client.torrents_pause(download_info.hash)
         else:
-            id_ = dl.gid()
-            try:
-                aria2.client.force_pause(id_)
-            except Exception as e:
-                LOGGER.error(f"{e} Error in pause, this mostly happens after abuse aria2")
-        listener.select = True
-    except:
-        sendMessage("This is not a bittorrent task!", context.bot, update.message)
+            aria2.client.force_pause(download_info.gid)
+    except aria2p.Aria2WebException as e:
+        logger.error(f"Error in pause: {e}")
+        sendMessage("Error in pause!", context.bot, update.message)
         return
 
-    SBUTTONS = bt_selection_buttons(id_)
+    buttons = bt_selection_buttons(download_info.gid)
     msg = "Your download paused. Choose files then press Done Selecting button to resume downloading."
-    sendMessage(msg, context.bot, update.message, SBUTTONS)
+    sendMessage(msg, context.bot, update.message, buttons)
 
-def get_confirm(update, context):
+async def get_confirm_selection(update: telegram.Update, context: telegram.ext.CallbackContext) -> None:
     query = update.callback_query
     user_id = query.from_user.id
-    data = query.data
-    data = data.split()
-    dl = getDownloadByGid(data[2])
-    if not dl:
+    data = query.data.split()
+    download_info = get_download_by_gid(data[2])
+
+    if not download_info:
         query.answer(text="This task has been cancelled!", show_alert=True)
         query.message.delete()
         return
-    if hasattr(dl, 'listener'):
-        listener = dl.listener()
-    else:
-        query.answer(text="Not in download state anymore! Keep this message to resume the seed if seed enabled!", show_alert=True)
+
+    if not hasattr(download_info, "listener"):
+        query.answer(
+            text="Not in download state anymore! Keep this message to resume the seed if seed enabled!",
+            show_alert=True,
+        )
         return
-    if user_id != listener.message.from_user.id:
+
+    if user_id != download_info.listener.message.from_user.id:
         query.answer(text="This task is not for you!", show_alert=True)
     elif data[1] == "pin":
         query.answer(text=data[3], show_alert=True)
     elif data[1] == "done":
         query.answer()
-        id_ = data[3]
-        if len(id_) > 20:
-            client = dl.client()
-            tor_info = client.torrents_info(torrent_hash=id_)[0]
-            path = tor_info.content_path.rsplit('/', 1)[0]
-            res = client.torrents_files(torrent_hash=id_)
-            for f in res:
-                if f.priority == 0:
-                    f_paths = [f"{path}/{f.name}", f"{path}/{f.name}.!qB"]
-                    for f_path in f_paths:
-                       if ospath.exists(f_path):
-                           try:
-                               remove(f_path)
-                           except:
-                               pass
-            client.torrents_resume(torrent_hashes=id_)
-        else:
-            res = aria2.client.get_files(id_)
-            for f in res:
-                if f['selected'] == 'false' and ospath.exists(f['path']):
-                    try:
-                        remove(f['path'])
-                    except:
-                        pass
-            try:
-                aria2.client.unpause(id_)
-            except Exception as e:
-                LOGGER.error(f"{e} Error in resume, this mostly happens after abuse aria2. Try to use select cmd again!")
-        sendStatusMessage(listener.message, listener.bot)
+
+        try:
+            if download_info.is_qbit:
+                aria2 = Aria2Client()
+                aria2.client.torrents_resume(download_info.hash)
+            else:
+                aria2.client.unpause(download_info.gid)
+        except aria2p.Aria2WebException as e:
+            logger.error(f"Error in resume: {e}")
+            query.answer(
+                text="Error in resume! This mostly happens after abusing aria2. Try to use select cmd again!",
+                show_alert=True,
+            )
+            return
+
+        sendStatusMessage(download_info.listener.message, download_info.listener.bot)
         query.message.delete()
 
+def main() -> None:
+    select_handler = CommandHandler(BotCommands.BtSelectCommand, select_download, filters=CustomFilters.authorized_chat)
+    bts_handler = CallbackQueryHandler(get_confirm_selection, pattern="btsel")
 
-select_handler = CommandHandler(BotCommands.BtSelectCommand, select,
-                                filters=(CustomFilters.authorized_chat | CustomFilters.authorized_user))
-bts_handler = CallbackQueryHandler(get_confirm, pattern="btsel")
-dispatcher.add_handler(select_handler)
-dispatcher.add_handler(bts_handler)
+    dispatcher.add_handler(select_handler)
+    dispatcher.add_handler(bts_handler)
+
+    try:
+        dispatcher.start_polling()
+        logger.info("Bot started successfully!")
+    except TelegramError as e:
+        logger.error(f"Failed to start bot: {e}")
+
+if __name__ == "__main__":
+    main()
