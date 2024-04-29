2902459dec999e1c099ee8675b94d3c763c4894b
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:45:17 +0600

    improve

--- bot/helper/ext_utils/exceptions.py
+++ bot/helper/ext_utils/exceptions.py
@@ -3,16 +3,9 @@ class DownloadLinkError(Exception):
     pass
 
 class DirectDownloadLinkNotFoundError(DownloadLinkError):
-    """Raised when a method for extracting a direct download link from an HTTP link cannot be found.
-
-    This exception is used to indicate that there was an issue with extracting a direct download link
-    from an HTTP link, typically because the method for extracting the link was not found.
-    """
+    """Raised when a method for extracting a direct download link from an HTTP link cannot be found."""
     pass
 
-class UnsupportedArchiveFormatError(DownloadLinkError):
-    """Raised when the archive format is not supported.
-
-    This exception is used to indicate that the archive format is not supported by the application.
-    """
+class UnsupportedArchiveFormatError(Exception):
+    """Raised when the archive format is not supported by the application."""
     pass
