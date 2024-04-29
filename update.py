b08fbff263b537959ab16c8065aa06a7d85059a1
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:45:24 +0600

    improve

--- update.py
+++ update.py
@@ -13,9 +13,9 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-def load_env_variables(env_file: str) -> None:
+def load_env_variables() -> None:
     """Load environment variables from a .env file."""
-    load_dotenv(env_file, override=True)
+    load_dotenv('config.env', override=True)
 
 def initialize_mongodb_connection(database_url: str) -> MongoClient:
     """Initialize a MongoDB connection."""
@@ -44,47 +44,45 @@ def upgrade_packages() -> None:
     """Upgrade Python packages."""
     packages = [dist.project_name for dist in pkg_resources.working_set]
     try:
-        os.system("pip install " + " ".join(pkg for pkg in packages))
+        subprocess.check_call(["pip", "install"] + packages)
     except subprocess.CalledProcessError as e:
         logging.error(f"Failed to upgrade packages: {e}")
 
 def update_codebase(upstream_url: str, upstream_branch: str) -> None:
     """Update the codebase from the upstream repository."""
     try:
-        subprocess.run(
+        subprocess.check_call(
             [
                 "git", "remote", "add", "origin", upstream_url,
                 "&&", "git", "fetch", "origin", "-q",
                 "&&", "git", "reset", "--hard", f"origin/{upstream_branch}", "-q"
             ],
             shell=True,
-            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
         )
     except subprocess.CalledProcessError as e:
         logging.error(f"Failed to update codebase: {e}")
 
 if __name__ == "__main__":
-    load_env_variables('config.env')
+    load_env_variables()
 
-    MISSING_ENV_VAR = '_____REMOVE_THIS_LINE_____'
-    if MISSING_ENV_VAR in os.environ:
+    if 'MISSING_ENV_VAR' in os.environ:
         logging.error('The README.md file should be read! Exiting now!')
         exit()
 
-    BOT_TOKEN = os.getenv('BOT_TOKEN')
-    if not BOT_TOKEN:
+    if not os.getenv('BOT_TOKEN'):
         logging.error("BOT_TOKEN variable is missing! Exiting now")
         exit(1)
 
-    bot_id = BOT_TOKEN.split(':', 1)[0]
+    bot_id = os.getenv('BOT_TOKEN').split(':', 1)[0]
     DATABASE_URL = os.getenv('DATABASE_URL')
 
     if DATABASE_URL:
         client = initialize_mongodb_connection(DATABASE_URL)
         update_environment_variables(client, bot_id)
 
-    UPGRADE_PACKAGES = os.getenv('UPGRADE_PACKAGES', 'False').lower() == 'true'
-    if UPGRADE_PACKAGES:
+    if os.getenv('UPGRADE_PACKAGES', 'False').lower() == 'true':
         upgrade_packages()
 
     UPSTREAM_REPO = os.getenv('UPSTREAM_REPO')
