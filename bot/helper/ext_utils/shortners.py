0df34e35bfc72379671be5ca60722e1d03a74527
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:35:59 +0600

    comment

--- bot/helper/ext_utils/shortners.py
+++ bot/helper/ext_utils/shortners.py
@@ -8,23 +8,35 @@ from urllib3 import disable_warnings
 
 from bot import LOGGER, shorteners_list
 
-
 def short_url(longurl, attempt=0):
-    if not shorteners_list:
+    """
+    Shortens the given long URL using various URL shortening services.
+    If all services fail, the original URL is returned.
+
+    :param longurl: The URL to be shortened
+    :param attempt: The number of attempts made to shorten the URL
+    :return: The shortened URL or the original URL if all services fail
+    """
+    if not shorteners_list:  # If there are no shortening services configured
         return longurl
-    if attempt >= 4:
+
+    if attempt >= 4:  # If the maximum number of attempts is reached
         return longurl
+
     i = 0 if len(shorteners_list) == 1 else randrange(len(shorteners_list))
-    _shorten_dict = shorteners_list[i]
+    _shorten_dict = shorteners_list[i]  # Selects a random shortening service
     _shortener = _shorten_dict['domain']
-    _shortener_api =  _shorten_dict['api_key']
-    cget = create_scraper().request
-    disable_warnings()
+    _shortener_api = _shorten_dict['api_key']
+
+    cget = create_scraper().request  # Creates a request function using cloudscraper
+    disable_warnings()  # Disables warnings
+
     try:
         if "shorte.st" in _shortener:
             headers = {'public-api-token': _shortener_api}
             data = {'urlToShorten': quote(longurl)}
             return cget('PUT', 'https://api.shorte.st/v1/data/url', headers=headers, data=data).json()['shortenedUrl']
+
         elif "linkvertise" in _shortener:
             url = quote(b64encode(longurl.encode("utf-8")))
             linkvertise = [
@@ -33,13 +45,17 @@ def short_url(longurl, attempt=0):
                 f"https://direct-link.net/{_shortener_api}/{random() * 1000}/dynamic?r={url}",
                 f"https://file-link.net/{_shortener_api}/{random() * 1000}/dynamic?r={url}"]
             return choice(linkvertise)
+
         elif "bitly.com" in _shortener:
             headers = {"Authorization": f"Bearer {_shortener_api}"}
             return cget('POST', "https://api-ssl.bit.ly/v4/shorten", json={"long_url": longurl}, headers=headers).json()["link"]
+
         elif "ouo.io" in _shortener:
-            return cget('GET', f'http://ouo.io/api/{_shortener_api}?s={longurl}', verify=False).text
+            return cget('GET', f'http://ouo.io/api?api={_shortener_api}?s={longurl}', verify=False).text
+
         elif "cutt.ly" in _shortener:
             return cget('GET', f'http://cutt.ly/api/api.php?key={_shortener_api}&short={longurl}').json()['url']['shortLink']
+
         else:
             res = cget('GET', f'https://{_shortener}/api?api={_shortener_api}&url={quote(longurl)}').json()
             shorted = res['shortenedUrl']
@@ -51,8 +67,9 @@ def short_url(longurl, attempt=0):
             if not shorted:
                 shorted = longurl
             return shorted
+
     except Exception as e:
-        LOGGER.error(e)
+        LOGGER.error(e)  # Logs the error
         sleep(1)
-        attempt +=1
+        attempt += 1
         return short_url(longurl, attempt)
