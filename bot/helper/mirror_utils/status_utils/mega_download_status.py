26317af9df58c1531015f3721d1ea3a717639174
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:45:02 +0600

    improve

--- bot/helper/mirror_utils/status_utils/mega_download_status.py
+++ bot/helper/mirror_utils/status_utils/mega_download_status.py
@@ -3,19 +3,30 @@ from dataclasses import dataclass
 from typing import NamedTuple
 from pathlib import Path
 from datetime import timedelta
-
 import dateutil.parser
 
 # Custom classes
-from bot.helper.ext_utils.bot_utils import EngineStatus, MirrorStatus, get_readable_file_size, get_readable_time
+# from bot.helper.ext_utils.bot_utils import EngineStatus, MirrorStatus, get_readable_file_size, get_readable_time
 
-
-class UploadDetails(NamedTuple):
+class CustomNamedTuple(NamedTuple):
+    """
+    A custom version of NamedTuple that allows for class methods and properties.
+    """
     user: str
     url: str
 
+    @property
+    def readable_size(self):
+        """
+        Returns the size of the file in a human-readable format.
+        """
+        return get_readable_file_size(self.size)
+
 
 class MegaDownloadStatus:
+    """
+    A class representing the status of a Mega download.
+    """
     def __init__(self, name: str, size: int, gid: str, obj, message, upload_details):
         """
         Initialize the MegaDownloadStatus object.
@@ -34,3 +45,26 @@ class MegaDownloadStatus:
         self.message = message
         self.upload_details = upload_details
 
+    @property
+    def readable_size(self):
+        """
+        Returns the size of the file in a human-readable format.
+        """
+        return get_readable_file_size(self.size)
+
+    @property
+    def readable_time(self):
+        """
+        Returns the time taken for the download in a human-readable format.
+        """
+        return get_readable_time(self.obj.time)
+
+    def __str__(self):
+        """
+        Returns a string representation of the MegaDownloadStatus object.
+        """
+        return (f"File Name: {self.name}\n"
+                f"File Size: {self.readable_size}\n"
+                f"GID: {self.gid}\n"
+                f"Time Taken: {self.readable_time}\n"
+                f"Upload Details: {self.upload_details}\n")
