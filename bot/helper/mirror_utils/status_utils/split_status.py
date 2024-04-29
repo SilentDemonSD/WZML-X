01f17e3b9bb33dd34c38f87e21e840dc465fbdd8
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:31 +0600

    improve

--- bot/helper/mirror_utils/status_utils/split_status.py
+++ bot/helper/mirror_utils/status_utils/split_status.py
@@ -2,9 +2,26 @@
 from bot import LOGGER
 from bot.helper.ext_utils.bot_utils import EngineStatus, get_readable_file_size, MirrorStatus
 
-
 class SplitStatus:
-    def __init__(self, name, size, gid, listener):
+    """
+    Class representing the status of a file split operation.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        size: int,
+        gid: int,
+        listener,
+    ):
+        """
+        Initialize a new SplitStatus object.
+
+        :param name: The name of the file being split.
+        :param size: The size of the file in bytes.
+        :param gid: The group ID associated with the file.
+        :param listener: The listener object associated with the file.
+        """
         self.__name = name
         self.__gid = gid
         self.__size = size
@@ -12,34 +29,83 @@ class SplitStatus:
         self.upload_details = listener.upload_details
         self.message = listener.message
 
-    def gid(self):
+    @property
+    def gid(self) -> int:
+        """
+        Get the group ID associated with the file.
+
+        :return: The group ID.
+        """
         return self.__gid
 
-    def progress(self):
+    def progress(self) -> str:
+        """
+        Get the progress of the file split operation.
+
+        :return: The progress as a string.
+        """
         return '0'
 
-    def speed(self):
+    def speed(self) -> str:
+        """
+        Get the speed of the file split operation.
+
+        :return: The speed as a string.
+        """
         return '0'
 
-    def name(self):
+    def name(self) -> str:
+        """
+        Get the name of the file being split.
+
+        :return: The name of the file.
+        """
         return self.__name
 
-    def size(self):
+    def size(self) -> str:
+        """
+        Get the size of the file being split.
+
+        :return: The size of the file as a human-readable string.
+        """
         return get_readable_file_size(self.__size)
 
-    def eta(self):
+    def eta(self) -> str:
+        """
+        Get the estimated time of arrival of the file split operation.
+
+        :return: The ETA as a string.
+        """
         return '0s'
 
-    def status(self):
+    def status(self) -> MirrorStatus:
+        """
+        Get the status of the file split operation.
+
+        :return: The status as a MirrorStatus object.
+        """
         return MirrorStatus.STATUS_SPLITTING
 
-    def processed_bytes(self):
+    def processed_bytes(self) -> int:
+        """
+        Get the number of bytes processed by the file split operation.
+
+        :return: The number of processed bytes.
+        """
         return 0
 
-    def download(self):
+    def download(self) -> 'SplitStatus':
+        """
+        Get the SplitStatus object for the file download operation.
+
+        :return: The SplitStatus object.
+        """
         return self
 
     async def cancel_download(self):
+        """
+        Cancel the file split operation.
+        """
         LOGGER.info(f'Cancelling Split: {self.__name}')
         if self.__listener.suproc is not None:
             self.__listener.suproc.kill()
@@ -47,6 +113,24 @@ class SplitStatus:
             self.__listener.suproc = 'cancelled'
         await self.__listener.onUploadError('splitting stopped by user!')
 
+    def eng(self) -> EngineStatus:
+        """
+        Get the engine status for the file split operation.
 
-    def eng(self):
+        :return: The engine status as an EngineStatus object.
+        """
         return EngineStatus().STATUS_SPLIT_MERGE
+
+    def __str__(self):
+        """
+        Get a human-readable representation of the SplitStatus object.
+
+        :return: A string representation of the object.
+        """
+        return (
+            f'SplitStatus('
+            f'name={self.__name}, '
+            f'size={self.__size}, '
+            f'gid={self.__gid}, '
+            f'listener={self.__listener})'
+        )
