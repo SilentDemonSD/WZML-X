dce3183e6f3774f4d340ead5a9e47b2eef5c6ef0
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:36:33 +0600

    improve

--- bot/helper/mirror_utils/upload_utils/ddlEngine.py
+++ bot/helper/mirror_utils/upload_utils/ddlEngine.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import asyncio
 import pathlib
 import traceback
@@ -8,24 +7,22 @@ import aiofiles.os as aiopath
 import time
 import aiohttp
 from tenacity import retry, wait_exponential, stop_after_attempt, retry_if_exception_type
-from bot.helper.mirror_utils.upload_utils.ddlserver.gofile import Gofile
-from bot.helper.mirror_utils.upload_utils.ddlserver.streamtape import Streamtape
-from bot.helper.ext_utils.fs_utils import get_mime_type
+from typing import Dict, Any, Union, Optional
 
 class ProgressFileReader(io.BufferedReader):
-    def __init__(self, filename, read_callback=None):
+    def __init__(self, filename: str, read_callback: Optional[Callable[[int], None]] = None):
         super().__init__(open(filename, "rb"))
         self.__read_callback = read_callback
         self.length = pathlib.Path(filename).stat().st_size
 
-    def read(self, size=None):
+    def read(self, size: Optional[int] = None) -> Union[bytes, MemoryView]:
         size = size or (self.length - self.tell())
         if self.__read_callback:
             self.__read_callback(self.tell())
         return super().read(size)
 
 class DDLUploader:
-    def __init__(self, listener=None, name=None, path=None):
+    def __init__(self, listener: Any, name: str, path: str):
         self.name = name
         self.__processed_bytes = 0
         self.last_uploaded = 0
@@ -41,18 +38,22 @@ class DDLUploader:
         self.__asyncSession = None
         self.__user_id = self.__listener.message.from_user.id
 
+    def __del__(self):
+        if self.__asyncSession:
+            self.__asyncSession.close()
+
     async def __user_settings(self):
         user_dict = user_data.get(self.__user_id, {})
         self.__ddl_servers = user_dict.get('ddl_servers', {})
 
-    def __progress_callback(self, current):
+    def __progress_callback(self, current: int):
         chunk_size = current - self.last_uploaded
         self.last_uploaded = current
         self.__processed_bytes += chunk_size
 
     @retry(wait=wait_exponential(multiplier=2, min=4, max=8), stop=stop_after_attempt(3),
         retry=retry_if_exception_type(Exception))
-    async def upload_aiohttp(self, url, file_path, req_file, data):
+    async def upload_aiohttp(self, url: str, file_path: str, req_file: str, data: Dict[str, Any]) -> Optional[Dict[str, Any]]:
         with ProgressFileReader(filename=file_path, read_callback=self.__progress_callback) as file:
             data[req_file] = file
             async with aiohttp.ClientSession() as self.__asyncSession:
@@ -65,7 +66,7 @@ class DDLUploader:
                         except json.JSONDecodeError:
                             return None
 
-    async def __upload_to_ddl(self, file_path):
+    async def __upload_to_ddl(self, file_path: str) -> Optional[Dict[str, Any]]:
         all_links = {}
         for serv, (enabled, api_key) in self.__ddl_servers.items():
             if enabled:
@@ -73,30 +74,47 @@ class DDLUploader:
                 self.total_folders = 0
                 if serv == 'gofile':
                     self.__engine = 'GoFile API'
-                    nlink = await Gofile(self, api_key).upload(file_path)
+                    if await aiopath.isfile(file_path):
+                        mime_type = get_mime_type(file_path)
+                    else:
+                        mime_type = 'Folder'
+                    try:
+                        nlink = await Gofile(self, api_key).upload(file_path)
+                    except Exception as e:
+                        print(e)
+                        continue
                     all_links['GoFile'] = nlink
                 if serv == 'streamtape':
                     self.__engine = 'StreamTape API'
                     try:
+                        if not await aiopath.isfile(file_path):
+                            raise Exception("StreamTape only supports file uploads")
+                        mime_type = get_mime_type(file_path)
+                    except Exception as e:
+                        print(e)
+                        continue
+                    try:
                         login, key = api_key.split(':')
                     except ValueError:
                         raise Exception("StreamTape Login & Key not Found, Kindly Recheck !")
-                    nlink = await Streamtape(self, login, key).upload(file_path)
+                    try:
+                        nlink = await Streamtape(self, login, key).upload(file_path)
+                    except Exception as e:
+                        print(e)
+                        continue
                     all_links['StreamTape'] = nlink
                 self.__processed_bytes = 0
+                if all_links:
+                    break
         if not all_links:
             raise Exception("No DDL Enabled to Upload.")
         return all_links
 
-    async def upload(self, file_name, size):
+    async def upload(self, file_name: str, size: int):
         item_path = f"{self.__path}/{file_name}"
         print(f"Uploading: {item_path} via DDL")
         await self.__user_settings()
         try:
-            if await aiopath.isfile(item_path):
-                mime_type = get_mime_type(item_path)
-            else:
-                mime_type = 'Folder'
             link = await self.__upload_to_ddl(item_path)
             if link is None:
                 raise Exception('Upload has been manually cancelled!')
@@ -114,21 +132,21 @@ class DDLUploader:
         finally:
             if self.is_cancelled or self.__is_errored:
                 return
-            await self.__listener.onUploadComplete(link, size, self.total_files, self.total_folders, mime_type, file_name)
+            await self.__listener.onUploadComplete(link, size, self.total_files, self.total_folders, 'application/octet-stream', file_name)
 
     @property
-    def speed(self):
+    def speed(self) -> float:
         try:
             return self.__processed_bytes / int(time.time() - self.__start_time)
         except ZeroDivisionError:
             return 0
 
     @property
-    def processed_bytes(self):
+    def processed_bytes(self) -> int:
         return self.__processed_bytes
 
     @property
-    def engine(self):
+    def engine(self) -> str:
         return self.__engine
 
     async def cancel_download(self):
