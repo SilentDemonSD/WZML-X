6bc310636546f8faa8e65018d043c300ef05c7ff
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:51:46 +0600

    comment

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -23,18 +23,26 @@ from psutil import (
 )
 
 # Initialize the bot
-bot = pyrogram.Client("bot")
+bot = pyrogram.Client("bot")  # Creating a Pyrogram client instance with the name 'bot'
 
 # Define bot commands
-START_COMMAND = BotCommand("start", "Start the bot")
-HELP_COMMAND = BotCommand("help", "Show this help message")
+START_COMMAND = BotCommand("start", "Start the bot")  # A command object for the /start command
+HELP_COMMAND = BotCommand("help", "Show this help message")  # A command object for the /help command
 
 @bot.on_message(pyrogram.Filters.command(START_COMMAND))
 async def start_command(client, message):
+    """
+    This is an asynchronous function that is called when the /start command is issued.
+    It responds with a simple message indicating that the bot has started.
+    """
     await message.reply("Bot has started!")
 
 @bot.on_message(pyrogram.Filters.command(HELP_COMMAND))
 async def help_command(client, message):
+    """
+    This is an asynchronous function that is called when the /help command is issued.
+    It responds with a list of available commands and their descriptions.
+    """
     await message.reply("Available commands: \n/start - Start the bot \n/help - Show this help message")
 
 # Rest of the code remains the same
