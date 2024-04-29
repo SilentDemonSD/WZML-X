741a7d759a53d8e1a260189c7324e96b1853d0f6
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:35:52 +0600

    comment

--- bot/helper/telegram_helper/__init__.py
+++ bot/helper/telegram_helper/__init__.py
@@ -1,1 +1,7 @@
-
+# This function calculates the area of a rectangle
+def rectangle_area(length, width):
+    # The area of a rectangle is calculated by multiplying its length and width
+    area = length * width
+    
+    # The area is then returned as the output of the function
+    return area
