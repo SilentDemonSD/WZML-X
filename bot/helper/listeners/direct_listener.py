42dd068156d43d4b1ee3b1cd3acf5945544f3fb9
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:55 +0600

    improve

--- bot/helper/listeners/direct_listener.py
+++ bot/helper/listeners/direct_listener.py
@@ -1,5 +1,33 @@
-from bot.helper.ext_utils.bot_utils import async_to_sync, sync_to_async
 import aiohttp
 import asyncio
-from bot import LOGGER, aria2
+from functools import wraps
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+async def aiohttp_get(*args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(*args, **kwargs) as response:
+            return await response.text()
+
+def aio_to_sync(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return asyncio.run(func(*args, **kwargs))
+    return wrapper
+
+@aio_to_sync
+async def download_file(url, output_file):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            content_length = int(response.headers['Content-Length'])
+            with open(output_file, 'wb') as f:
+                while True:
+                    chunk = await response.content.read(4096)
+                    if not chunk:
+                        break
+                    f.write(chunk)
+                    percentage = (f.tell() / content_length) * 100
+                    logger.info(f"Downloaded {percentage:.2f}%")
 
