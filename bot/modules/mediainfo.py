ea7b23fbff15a4a532fcbd71af15ec6f62de90cd
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:26 +0600

    improve

--- bot/modules/mediainfo.py
+++ bot/modules/mediainfo.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-import aiohttp
-from re import search as re_search
-from shlex import split as ssplit
-from aiofiles import open as aiopen
-from aiofiles.os import remove as aioremove, path as aiopath, mkdir
-from os import path as ospath, getcwd
+import asyncio
+import os
+import re
+import shlex
+from pathlib import Path
 
-from pyrogram.handlers import MessageHandler 
+import aiohttp
+import aiofiles
+from pyrogram.handlers import MessageHandler
 from pyrogram.filters import command
+from pyrogram.errors import UserIsBlocked, MessageNotModified, ChatWriteForbidden
 
 from bot import LOGGER, bot, config_dict
 from bot.helper.telegram_helper.filters import CustomFilters
@@ -16,81 +18,28 @@ from bot.helper.telegram_helper.message_utils import editMessage, sendMessage
 from bot.helper.ext_utils.bot_utils import cmd_exec
 from bot.helper.ext_utils.telegraph_helper import telegraph
 
+MEDIAINFO_PATH = "Mediainfo/"
 
-async def gen_mediainfo(message, link=None, media=None, mmsg=None):
-    temp_send = await sendMessage(message, '<i>Generating MediaInfo...</i>')
+async def download_file(session, url, file_path):
+    async with session.get(url, headers={"user-agent": "Mozilla/5.0"}) as response:
+        if response.status != 200:
+            raise Exception(f"Failed to download file: {response.status}")
+        async with aiofiles.open(file_path, "wb") as f:
+            while True:
+                chunk = await response.content.read(10000000)
+                if not chunk:
+                    break
+                await f.write(chunk)
+
+async def generate_mediainfo(message, link=None, media=None, mmsg=None):
+    temp_send = await sendMessage(message, "Generating MediaInfo...")
     try:
-        path = "Mediainfo/"
-        if not await aiopath.isdir(path):
-            await mkdir(path)
         if link:
-            filename = re_search(".+/(.+)", link).group(1)
-            des_path = ospath.join(path, filename)
-            headers = {"user-agent":"Mozilla/5.0 (Linux; Android 12; 2201116PI) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"}
+            file_name = re.search(".+/(.+)", link).group(1)
+            file_path = Path(MEDIAINFO_PATH) / file_name
             async with aiohttp.ClientSession() as session:
-                async with session.get(link, headers=headers) as response:
-                    async with aiopen(des_path, "wb") as f:
-                        async for chunk in response.content.iter_chunked(10000000):
-                            await f.write(chunk)
-                            break
+                await download_file(session, link, file_path)
         elif media:
-            des_path = ospath.join(path, media.file_name)
+            file_path = Path(MEDIAINFO_PATH) / media.file_name
             if media.file_size <= 50000000:
-                await mmsg.download(ospath.join(getcwd(), des_path))
-            else:
-                async for chunk in bot.stream_media(media, limit=5):
-                    async with aiopen(des_path, "ab") as f:
-                        await f.write(chunk)
-        stdout, _, _ = await cmd_exec(ssplit(f'mediainfo "{des_path}"'))
-        tc = f"<h4>📌 {ospath.basename(des_path)}</h4><br><br>"
-        if len(stdout) != 0:
-            tc += parseinfo(stdout)
-    except Exception as e:
-        LOGGER.error(e)
-        await editMessage(temp_send, f"MediaInfo Stopped due to {str(e)}")
-    finally:
-        await aioremove(des_path)
-    link_id = (await telegraph.create_page(title='MediaInfo X', content=tc))["path"]
-    await temp_send.edit(f"<b>MediaInfo:</b>\n\n➲ <b>Link :</b> https://graph.org/{link_id}", disable_web_page_preview=False)
-
-
-section_dict = {'General': '🗒', 'Video': '🎞', 'Audio': '🔊', 'Text': '🔠', 'Menu': '🗃'}
-def parseinfo(out):
-    tc = ''
-    trigger = False
-    for line in out.split('\n'):
-        for section, emoji in section_dict.items():
-            if line.startswith(section):
-                trigger = True
-                if not line.startswith('General'):
-                    tc += '</pre><br>'
-                tc += f"<h4>{emoji} {line.replace('Text', 'Subtitle')}</h4>"
-                break
-        if trigger:
-            tc += '<br><pre>'
-            trigger = False
-        else:
-            tc += line + '\n'
-    tc += '</pre><br>'
-    return tc
-
-
-async def mediainfo(_, message):
-    rply = message.reply_to_message
-    help_msg = "<b>By replying to media:</b>"
-    help_msg += f"\n<code>/{BotCommands.MediaInfoCommand[0]} or /{BotCommands.MediaInfoCommand[1]}" + " {media}" + "</code>"
-    help_msg += "\n\n<b>By reply/sending download link:</b>"
-    help_msg += f"\n<code>/{BotCommands.MediaInfoCommand[0]} or /{BotCommands.MediaInfoCommand[1]}" + " {link}" + "</code>"
-    if len(message.command) > 1 or rply and rply.text:
-        link = rply.text if rply else message.command[1]
-        return await gen_mediainfo(message, link)
-    elif rply:
-        file = next((i for i in [rply.document, rply.video, rply.audio, rply.voice,
-                         rply.animation, rply.video_note] if i is not None), None)
-        if not file:
-            return await sendMessage(message, help_msg)
-        return await gen_mediainfo(message, None, file, rply)
-    else:
-        return await sendMessage(message, help_msg)
-
-bot.add_handler(MessageHandler(mediainfo, filters=command(BotCommands.MediaInfoCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
+              
