f51e8d4ae07f89503c21e28b131dd0b54607c5d7
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:35 +0600

    improve

--- update.py
+++ update.py
@@ -1,80 +1,99 @@
-from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
-from os import path as ospath, environ, remove
-from subprocess import run as srun
-from requests import get as rget
+import os
+import pathlib
+import subprocess
+from typing import Any, Optional
+
+import pymongo
+from pydantic import BaseModel
+from requests import get
+
 from dotenv import load_dotenv, dotenv_values
-from pymongo import MongoClient
 
-if ospath.exists('log.txt'):
-    with open('log.txt', 'r+') as f:
-        f.truncate(0)
+# Load environment variables from .env file
+load_dotenv()
 
-if ospath.exists('rlog.txt'): #RClone Logs
-    remove('rlog.txt')
+# Logging configuration
+log_fmt = "[%(asctime)s] [%(levelname)s] - %(message)s"
+date_fmt = "%d-%b-%y %I:%M:%S %p"
+handlers = [logging.FileHandler("log.txt"), logging.StreamHandler()]
+basicConfig(format=log_fmt, datefmt=date_fmt, handlers=handlers, level=logging.INFO)
 
-basicConfig(format="[%(asctime)s] [%(levelname)s] - %(message)s",
-            datefmt="%d-%b-%y %I:%M:%S %p",
-            handlers=[FileHandler('log.txt'), StreamHandler()],
-            level=INFO)
+class Config(BaseModel):
+    UPSTREAM_REPO: Optional[str] = None
+    UPSTREAM_BRANCH: Optional[str] = "master"
 
-load_dotenv('config.env', override=True)
+class Database(BaseModel):
+    DATABASE_URL: Optional[str] = None
 
-try:
-    if bool(environ.get('_____REMOVE_THIS_LINE_____')):
-        log_error('The README.md file there to be read! Exiting now!')
-        exit()
-except:
-    pass
+def get_env_var(var_name: str, default: Any = None) -> Any:
+    """Get the value of an environment variable, or return a default value if it's not set."""
+    return os.getenv(var_name, default)
 
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
-if len(BOT_TOKEN) == 0:
-    log_error("BOT_TOKEN variable is missing! Exiting now")
-    exit(1)
+def is_true(val: str) -> bool:
+    """Check if a string is truthy."""
+    return val.lower() in ["true", "yes", "t"]
 
-bot_id = BOT_TOKEN.split(':', 1)[0]
+def update_repository() -> None:
+    """Update the repository with the latest commits from the upstream repository."""
+    upstream_repo = get_env_var("UPSTREAM_REPO")
+    upstream_branch = get_env_var("UPSTREAM_BRANCH")
 
-DATABASE_URL = environ.get('DATABASE_URL', '')
-if len(DATABASE_URL) == 0:
-    DATABASE_URL = None
+    if not upstream_repo:
+        log_error("UPSTREAM_REPO variable is missing! Exiting now")
+        exit(1)
 
-if DATABASE_URL is not None:
-    conn = MongoClient(DATABASE_URL)
-    db = conn.wzmlx
-    old_config = db.settings.deployConfig.find_one({'_id': bot_id})
-    config_dict = db.settings.config.find_one({'_id': bot_id})
-    if old_config is not None:
-        del old_config['_id']
-    if (old_config is not None and old_config == dict(dotenv_values('config.env')) or old_config is None) \
-            and config_dict is not None:
-        environ['UPSTREAM_REPO'] = config_dict['UPSTREAM_REPO']
-        environ['UPSTREAM_BRANCH'] = config_dict['UPSTREAM_BRANCH']
-    conn.close()
+    if not upstream_branch:
+        upstream_branch = "master"
 
-UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
-if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = None
+    if os.path.exists(".git"):
+        subprocess.run(["rm", "-rf", ".git"], check=True)
 
-UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
-if len(UPSTREAM_BRANCH) == 0:
-    UPSTREAM_BRANCH = 'master'
+    subprocess.run(
+        [
+            "git init -q && git config --global user.email doc.adhikari@gmail.com && git config --global user.name weebzone && git add . && git commit -sm update -q && git remote add origin {} && git fetch origin -q && git reset --hard origin/{} -q".format(
+                upstream_repo, upstream_branch
+            )
+        ],
+        shell=True,
+        check=True,
+    )
 
-if UPSTREAM_REPO is not None:
-    if ospath.exists('.git'):
-        srun(["rm", "-rf", ".git"])
+    repo = upstream_repo.split("/")
+    upstream_repo = "https://github.com/{}".format("/".join(repo[-2:]))
 
-    update = srun([f"git init -q \
-                     && git config --global user.email doc.adhikari@gmail.com \
-                     && git config --global user.name weebzone \
-                     && git add . \
-                     && git commit -sm update -q \
-                     && git remote add origin {UPSTREAM_REPO} \
-                     && git fetch origin -q \
-                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+    log_info("Successfully updated with latest commits !!")
+    log_error(f"UPSTREAM_REPO: {upstream_repo} | UPSTREAM_BRANCH: {upstream_branch}")
 
-    repo = UPSTREAM_REPO.split('/')
-    UPSTREAM_REPO = f"https://github.com/{repo[-2]}/{repo[-1]}"
-    if update.returncode == 0:
-        log_info('Successfully updated with latest commits !!')
-    else:
-        log_error('Something went Wrong !!')
-    log_error(f'UPSTREAM_REPO: {UPSTREAM_REPO} | UPSTREAM_BRANCH: {UPSTREAM_BRANCH}')
+if __name__ == "__main__":
+    bot_token = get_env_var("BOT_TOKEN")
+    if not bot_token:
+        log_error("BOT_TOKEN variable is missing! Exiting now")
+        exit(1)
+
+    bot_id = bot_token.split(":")[0]
+
+    database_url = get_env_var("DATABASE_URL")
+    if not database_url:
+        database_url = None
+
+    if database_url is not None:
+        conn = pymongo.MongoClient(database_url)
+        db = conn.wzmlx
+        old_config = db.settings.deployConfig.find_one({"_id": bot_id})
+        config_dict = db.settings.config.find_one({"_id": bot_id})
+
+        if old_config is not None:
+            del old_config["_id"]
+
+        if (
+            old_config is not None
+            and old_config == dotenv_values("config.env")
+            or old_config is None
+        ) and config_dict is not None:
+            os.environ["UPSTREAM_REPO"] = config_dict["UPSTREAM_REPO"]
+            os.environ["UPSTREAM_BRANCH"] = config_dict["UPSTREAM_BRANCH"]
+
+        conn.close()
+
+    config = Config(UPSTREAM_REPO=os.getenv("UPSTREAM_REPO"), UPSTREAM_BRANCH=os.getenv("UPSTREAM_BRANCH"))
+    update_repository()
