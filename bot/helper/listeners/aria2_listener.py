0afad31dd8598356a36ad937d6060b78e6487f33
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:51:39 +0600

    comment

--- bot/helper/listeners/aria2_listener.py
+++ bot/helper/listeners/aria2_listener.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
-import asyncio
-import logging
-import time
-from typing import Any, AsyncIterator, Callable, Optional
+import asyncio  # Importing the asyncio library to use asynchronous programming features
+import logging  # Importing the logging library for logging purposes
+import time  # Importing the time library to measure elapsed time
+from typing import Any, AsyncIterator, Callable, Optional  # Importing types for type hinting
 
-import aioaria2
-import aiopath
-import humanize
-import walrus
-from bot import aria2, download_dict_lock, download_dict, LOGGER, config_dict
-from bot.helper.ext_utils.task_manager import limit_checker, create_task
-from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-from bot.helper.mirror_utils.status_utils.aria2_status import Aria2Status
-from bot.helper.ext_utils.fs_utils import get_base_name, clean_unwanted
-from bot.helper.ext_utils.bot_utils import getDownloadByGid
-from bot.helper.telegram_helper.message_utils import sendMessage, deleteMessage, update_all_messages
-from bot.helper.themes import BotTheme
+# Importing custom modules and libraries
+import aioaria2  # A library for interacting with aria2 RPC interface
+import aiopath  # A library for handling file and directory paths asynchronously
+import humanize  # A library for making byte sizes more readable
+import walrus  # A library for working with key-value stores
+from bot import aria2, download_dict_lock, download_dict, LOGGER, config_dict  # Importing objects from the bot module
+from bot.helper.ext_utils.task_manager import limit_checker, create_task  # Importing functions from the task_manager module
+from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper  # Importing the GoogleDriveHelper class
+from bot.helper.mirror_utils.status_utils.aria2_status import Aria2Status  # Importing the Aria2Status class
+from bot.helper.ext_utils.fs_utils import get_base_name, clean_unwanted  # Importing functions from the fs_utils module
+from bot.helper.ext_utils.bot_utils import getDownloadByGid  # Importing the getDownloadByGid function
+from bot.helper.telegram_helper.message_utils import sendMessage, deleteMessage, update_all_messages  # Importing functions from the message_utils module
+from bot.helper.themes import BotTheme  # Importing the BotTheme class
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)  # Creating a logger instance with the current module's name
 
 
 async def on_download_started(api: aioaria2.Aria2, gid: str) -> None:
-    ...
-
+    ...  # Placeholder for the on_download_started function definition
