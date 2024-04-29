ff294d04500609164e08cabf2a66eefeff7a86ef
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:45 +0600

    improve

--- generate_drive_token.py
+++ generate_drive_token.py
@@ -3,47 +3,52 @@ import os
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-def load_and_refresh_credentials():
-    """Loads and refreshes the Google Drive credentials if necessary.
+def load_credentials():
+    """Loads the Google Drive credentials from the file.
 
     Returns:
-        google.oauth2.credentials.Credentials: The Google Drive credentials.
+        google.oauth2.credentials.Credentials: The Google Drive credentials or None if not found.
     """
     credentials = None
-    __G_DRIVE_TOKEN_FILE = "token.pickle"
+    token_file = "token.pickle"
 
-    if os.path.exists(__G_DRIVE_TOKEN_FILE):
-        try:
-            with open(__G_DRIVE_TOKEN_FILE, 'rb') as f:
-                credentials = pickle.load(f)
+    if os.path.exists(token_file):
+        with open(token_file, 'rb') as f:
+            credentials = pickle.load(f)
 
-                if (
-                    credentials is not None
-                    and not credentials.valid
-                    and credentials.expired
-                    and credentials.refresh_token
-                ):
-                    credentials.refresh(Request())
-        except Exception as e:
-            print(f"Error loading credentials: {e}")
+    return credentials
 
-    if credentials is None:
-        __OAUTH_SCOPE = ["https://www.googleapis.com/auth/drive"]
+def refresh_and_save_credentials():
+    """Refreshes the Google Drive credentials if necessary and saves them to the file.
+    """
+    credentials = None
+    oauth_scope = ["https://www.googleapis.com/auth/drive"]
+
+    if load_credentials() is None:
         flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', __OAUTH_SCOPE)
+            'credentials.json', oauth_scope)
         try:
             credentials = flow.run_local_server(port=0, open_browser=False)
         except Exception as e:
             print(f"Error running local server: {e}")
 
-    # Save the credentials for the next run
-    try:
-        with open(__G_DRIVE_TOKEN_FILE, 'wb') as token:
-            pickle.dump(credentials, token)
-    except Exception as e:
-        print(f"Error saving credentials: {e}")
+    if (
+        credentials is not None
+        and not credentials.valid
+        and credentials.expired
+        and credentials.refresh_token
+    ):
+        try:
+            credentials.refresh(Request())
+        except Exception as e:
+            print(f"Error refreshing credentials: {e}")
 
-    return credentials
+    if credentials is not None:
+        try:
+            with open("token.pickle", 'wb') as token:
+                pickle.dump(credentials, token)
+        except Exception as e:
+            print(f"Error saving credentials: {e}")
 
 if __name__ == "__main__":
-    load_and_refresh_credentials()
+    refresh_and_save_credentials()
