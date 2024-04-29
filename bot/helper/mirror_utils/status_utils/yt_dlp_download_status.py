d63e3a56aa00c07b0352102a148ffa3231342134
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:54 +0600

    improve

--- bot/helper/mirror_utils/status_utils/yt_dlp_download_status.py
+++ bot/helper/mirror_utils/status_utils/yt_dlp_download_status.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-from bot.helper.ext_utils.bot_utils import EngineStatus, MirrorStatus, get_readable_file_size, get_readable_time, async_to_sync
+from bot.helper.ext_utils.bot_utils import EngineStatus, MirrorStatus
 from bot.helper.ext_utils.fs_utils import get_path_size
+from bot.helper.ext_utils.time_utils import get_readable_time
+from typing import Union
 
 class YtDlpDownloadStatus:
     def __init__(self, obj, listener, gid):
@@ -10,57 +12,3 @@ class YtDlpDownloadStatus:
         self.gid = gid
         self.message = listener.message
 
-    def get_gid(self):
-        return self.gid
-
-    def processed_bytes(self):
-        return get_readable_file_size(self.processed_raw())
-
-    def processed_raw(self):
-        if self.obj.downloaded_bytes != 0:
-            return self.obj.downloaded_bytes
-        else:
-            return async_to_sync(get_path_size, self.listener.dir)
-
-    def size(self):
-        return get_readable_file_size(self.obj.size)
-
-    def status(self):
-        return MirrorStatus.STATUS_DOWNLOADING
-
-    def name(self):
-        return self.obj.name
-
-    def progress(self):
-        return f'{round(self.obj.progress, 2)}%'
-
-    def speed(self):
-        return f'{get_readable_file_size(self.obj.download_speed)}/s'
-
-    def eta(self):
-        if self.obj.eta != '-':
-            return get_readable_time(self.obj.eta)
-        try:
-            seconds = (self.obj.size - self.processed_raw()) / self.obj.download_speed
-            return get_readable_time(seconds)
-        except:
-            return '-'
-
-    def download(self):
-        return self.obj
-
-    def engine(self):
-        return EngineStatus().STATUS_YT
-
-    def __str__(self):
-        return (f"YtDlpDownloadStatus:\n"
-                f"gid: {self.gid}\n"
-                f"processed_bytes: {self.processed_bytes}\n"
-                f"size: {self.size}\n"
-                f"status: {self.status}\n"
-                f"name: {self.name}\n"
-                f"progress: {self.progress}\n"
-                f"speed: {self.speed}\n"
-                f"eta: {self.eta}\n"
-                f"download: {self.download}\n"
-                f"engine: {self.engine}\n")
