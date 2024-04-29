bc9b9bf0b693bf4416419f468c0852de305a1812
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:51:44 +0600

    comment

--- bot/modules/save_msg.py
+++ bot/modules/save_msg.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import asyncio
-from typing import Dict, Any, Optional, TypeVar, Union
-from pyrogram.types import InlineKeyboardMarkup, CallbackQuery
-from pyrogram.handlers import CallbackQueryHandler
-from pyrogram.filters import regex
+from typing import Dict, Any, Optional, TypeVar, Union  # Importing various types from the typing module
 
-import bot
-from bot import bot, bot_name, user_data
+from pyrogram.types import InlineKeyboardMarkup, CallbackQuery  # Importing InlineKeyboardMarkup and CallbackQuery from the pyrogram.types module
+from pyrogram.handlers import CallbackQueryHandler  # Importing CallbackQueryHandler from the pyrogram.handlers module
+from pyrogram.filters import regex  # Importing regex from the pyrogram.filters module
+
+import bot  # Importing the bot module
+from bot import bot, bot_name, user_data  # Importing bot, bot_name, and user_data from the bot module
 
