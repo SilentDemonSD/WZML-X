e3fda2d3f8c7e378a1c09fd2d22e989a2c8ba63e
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:36:28 +0600

    improve

--- bot/modules/gd_list.py
+++ bot/modules/gd_list.py
@@ -1,79 +1,85 @@
 #!/usr/bin/env python3
+from typing import List, Tuple, Union
+
+import asyncio
 from random import choice
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 from pyrogram.filters import command, regex
+from pyrogram.types import Message, CallbackQuery
 
 from bot import LOGGER, bot, config_dict
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-from bot.helper.telegram_helper.message_utils import sendMessage, editMessage, delete_links
+from bot.helper.telegram_helper.message_utils import sendMessage, editMessage, delete_messages
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.button_build import ButtonMaker
 from bot.helper.ext_utils.bot_utils import sync_to_async, new_task, get_telegraph_list, checking_access
 from bot.helper.themes import BotTheme
 
-
-async def list_buttons(user_id, isRecursive=True):
+async def list_buttons(user_id: int, is_recursive: bool = True) -> List[List[str]]:
     buttons = ButtonMaker()
-    buttons.ibutton("Only Folders", f"list_types {user_id} folders {isRecursive}")
-    buttons.ibutton("Only Files", f"list_types {user_id} files {isRecursive}")
-    buttons.ibutton("Both", f"list_types {user_id} both {isRecursive}")
-    buttons.ibutton(f"{'✅️' if isRecursive else ''} Recursive", f"list_types {user_id} rec {isRecursive}")
+    buttons.ibutton("Only Folders", f"list_types {user_id} folders {is_recursive}")
+    buttons.ibutton("Only Files", f"list_types {user_id} files {is_recursive}")
+    buttons.ibutton("Both", f"list_types {user_id} both {is_recursive}")
+    buttons.ibutton(f"{'✅️' if is_recursive else ''} Recursive", f"list_types {user_id} rec {is_recursive}")
     buttons.ibutton("Cancel", f"list_types {user_id} cancel")
     return buttons.build_menu(2)
 
-
-async def _list_drive(key, message, user_id, item_type, isRecursive):
+async def _list_drive(key: str, message: Message, user_id: int, item_type: str, is_recursive: bool):
     LOGGER.info(f"GDrive List: {key}")
     gdrive = GoogleDriveHelper()
-    telegraph_content, contents_no = await sync_to_async(gdrive.drive_list, key, isRecursive=isRecursive, itemType=item_type, userId=user_id)
+    try:
+        telegraph_content, contents_no = await sync_to_async(gdrive.drive_list, key, is_recursive=is_recursive, itemType=item_type, userId=user_id)
+    except Exception as e:
+        await bot.edit_message_text(chat_id=message.chat.id, message_id=message.id, text=str(e))
+        return
+
     if telegraph_content:
         try:
             button = await get_telegraph_list(telegraph_content)
         except Exception as e:
-            await editMessage(message, e)
+            await bot.edit_message_text(chat_id=message.chat.id, message_id=message.id, text=str(e))
             return
-        msg = BotTheme('LIST_FOUND', NO=contents_no, NAME=key)
-        await editMessage(message, msg, button)
+        msg = BotTheme.get_string('LIST_FOUND', NO=contents_no, NAME=key)
+        await bot.edit_message_text(chat_id=message.chat.id, message_id=message.id, text=msg, reply_markup=button)
     else:
-        await editMessage(message, BotTheme('LIST_NOT_FOUND', NAME=key))
+        await bot.edit_message_text(chat_id=message.chat.id, message_id=message.id, text=BotTheme.get_string('LIST_NOT_FOUND', NAME=key))
 
-
-@new_task
-async def select_type(_, query):
+async def select_type(_: Message, query: CallbackQuery):
     user_id = query.from_user.id
     message = query.message
     key = message.reply_to_message.text.split(maxsplit=1)[1].strip()
     data = query.data.split()
     if user_id != int(data[1]):
-        return await query.answer(text="Not Yours!", show_alert=True)
+        await bot.answer(query, text="Not Yours!", show_alert=True)
+        return
     elif data[2] == 'rec':
-        await query.answer()
-        isRecursive = not bool(eval(data[3]))
-        buttons = await list_buttons(user_id, isRecursive)
-        return await editMessage(message, '<b>Choose drive list options:</b>', buttons)
+        is_recursive = not bool(eval(data[3]))
+        buttons = await list_buttons(user_id, is_recursive)
+        await bot.edit_message_text(chat_id=message.chat.id, message_id=message.id, text='<b>Choose drive list options:</b>', reply_markup=buttons)
+        await bot.answer(query)
+        return
     elif data[2] == 'cancel':
-        await query.answer()
-        return await editMessage(message, "<b>List has been canceled!</b>")
-    await query.answer()
+        await bot.answer(query)
+        await bot.edit_message_text(chat_id=message.chat.id, message_id=message.id, text="<b>List has been canceled!</b>")
+        return
+    await bot.answer(query)
     item_type = data[2]
-    isRecursive = eval(data[3])
-    await editMessage(message, BotTheme('LIST_SEARCHING', NAME=key))
-    await _list_drive(key, message, user_id, item_type, isRecursive)
+    is_recursive = eval(data[3])
+    await bot.edit_message_text(chat_id=message.chat.id, message_id=message.id, text=BotTheme.get_string('LIST_SEARCHING', NAME=key))
+    await _list_drive(key, message, user_id, item_type, is_recursive)
 
-
-async def drive_list(_, message):
+async def drive_list(_: Message, message: Message):
     args = message.text.split() if message.text else ['/cmd']
     if len(args) == 1:
-        return await sendMessage(message, '<i>Send a search key along with command</i>')
+        return await sendMessage(message, BotTheme.get_string('SEND_KEY'))
     user_id = message.from_user.id
     msg, btn = await checking_access(user_id)
     if msg is not None:
         await sendMessage(message, msg, btn.build_menu(1))
         return
     buttons = await list_buttons(user_id)
-    await sendMessage(message, '<b>Choose drive list options:</b>', buttons, 'IMAGES')
+    await sendMessage(message, BotTheme.get_string('CHOOSE_OPTIONS'), buttons, 'IMAGES')
 
-bot.add_handler(MessageHandler(drive_list, filters=command(
-    BotCommands.ListCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
-bot.add_handler(CallbackQueryHandler(select_type, filters=regex("^list_types")))
+bot.add_handler(MessageHandler(drive_list, filters=command(BotCommands.ListCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted), group=2)
+bot.add_handler(CallbackQueryHandler(select_type, filters=regex("^list_types")), group=2)
