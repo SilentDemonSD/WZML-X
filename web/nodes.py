11cfa5f55ed42342ae8ab8944661184bb6ce3449
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:50 +0600

    improve

--- web/nodes.py
+++ web/nodes.py
@@ -1,16 +1,14 @@
-from anytree import NodeMixin
-from re import findall as re_findall
-from os import environ
+import os
+import re
+from anytree import NodeMixin, RenderTree
+from typing import List, Tuple, Dict, Any
 
-DOWNLOAD_DIR = environ.get('DOWNLOAD_DIR', '')
-if len(DOWNLOAD_DIR) == 0:
-    DOWNLOAD_DIR = '/usr/src/app/downloads/'
-elif not DOWNLOAD_DIR.endswith("/"):
+DOWNLOAD_DIR = os.environ.get('DOWNLOAD_DIR', '/usr/src/app/downloads/')
+if not DOWNLOAD_DIR.endswith('/'):
     DOWNLOAD_DIR += '/'
 
-
 class TorNode(NodeMixin):
-    def __init__(self, name, is_folder=False, is_file=False, parent=None, size=None, priority=None, file_id=None, progress=None):
+    def __init__(self, name: str, is_folder: bool = False, is_file: bool = False, parent: 'TorNode' = None, size: int = None, priority: int = None, file_id: str = None, progress: float = None):
         super().__init__()
         self.name = name
         self.is_folder = is_folder
@@ -27,80 +25,54 @@ class TorNode(NodeMixin):
         if progress is not None:
             self.progress = progress
 
+def qb_get_folders(path: str) -> List[str]:
+    return path.split('/')[1:] if path else []
 
-def qb_get_folders(path):
-    return path.split("/")
-
-def get_folders(path):
-    fs = re_findall(f'{DOWNLOAD_DIR}[0-9]+/(.+)', path)[0]
+def get_folders(path: str) -> List[str]:
+    fs = re.findall(f'{DOWNLOAD_DIR}[0-9]+/(.+)', path)[0]
     return fs.split('/')
 
-def make_tree(res, aria2=False):
+def make_tree(res: List[Any], aria2: bool = False) -> TorNode:
     parent = TorNode("Torrent")
-    if not aria2:
-        for i in res:
-            folders = qb_get_folders(i.name)
-            if len(folders) > 1:
-                previous_node = parent
-                for j in range(len(folders)-1):
-                    current_node = next((k for k in previous_node.children if k.name == folders[j]), None)
-                    if current_node is None:
-                        previous_node = TorNode(folders[j], parent=previous_node, is_folder=True)
-                    else:
-                        previous_node = current_node
-                TorNode(folders[-1], is_file=True, parent=previous_node, size=i.size, priority=i.priority, \
-                        file_id=i.id, progress=round(i.progress*100, 5))
-            else:
-                TorNode(folders[-1], is_file=True, parent=parent, size=i.size, priority=i.priority, \
-                        file_id=i.id, progress=round(i.progress*100, 5))
-    else:
-        for i in res:
-            folders = get_folders(i['path'])
-            priority = 1
-            if i['selected'] == 'false':
-                priority = 0
-            if len(folders) > 1:
-                previous_node = parent
-                for j in range(len(folders)-1):
-                    current_node = next((k for k in previous_node.children if k.name == folders[j]), None)
-                    if current_node is None:
-                        previous_node = TorNode(folders[j], parent=previous_node, is_folder=True)
-                    else:
-                        previous_node = current_node
-                TorNode(folders[-1], is_file=True, parent=previous_node, size=i['length'], priority=priority, \
-                        file_id=i['index'], progress=round((int(i['completedLength'])/int(i['length']))*100, 5))
-            else:
-                TorNode(folders[-1], is_file=True, parent=parent, size=i['length'], priority=priority, \
-                        file_id=i['index'], progress=round((int(i['completedLength'])/int(i['length']))*100, 5))
-    return create_list(parent, ["", 0])
+    for i in res:
+        folders = qb_get_folders(i.name) if not aria2 else get_folders(i['path'])
+        current_node = parent
+        for folder in folders:
+            new_node = next((k for k in current_node.children if k.name == folder), None)
+            if new_node is None:
+                new_node = TorNode(folder, parent=current_node, is_folder=True)
+            current_node = new_node
 
-"""
-def print_tree(parent):
-    for pre, _, node in RenderTree(parent):
-        treestr = u"%s%s" % (pre, node.name)
-        print(treestr.ljust(8), node.is_folder, node.is_file)
-"""
+        if not aria2:
+            TorNode(folders[-1], is_file=True, parent=current_node, size=i.size, priority=i.priority, file_id=i.id, progress=round(i.progress*100, 5))
+        else:
+            priority = 1 if i['selected'] == 'true' else 0
+            TorNode(folders[-1], is_file=True, parent=current_node, size=i['length'], priority=priority, file_id=i['index'], progress=round((int(i['completedLength'])/int(i['length']))*100, 5))
+    return parent
 
-def create_list(par, msg):
+def create_list(par: TorNode, msg: Tuple[str, int]) -> str:
+    html = msg[0]
+    index = msg[1]
+
     if par.name != ".unwanted":
-        msg[0] += '<ul>'
+        html += '<ul>'
     for i in par.children:
         if i.is_folder:
-            msg[0] += "<li>"
+            html += "<li>"
             if i.name != ".unwanted":
-                msg[0] += f'<input type="checkbox" name="foldernode_{msg[1]}"> <label for="{i.name}">{i.name}</label>'
-            create_list(i, msg)
-            msg[0] += "</li>"
-            msg[1] += 1
+                html += f'<input type="checkbox" name="foldernode_{index}"> <label for="{i.name}">{i.name}</label>'
+            html = create_list(i, (html, index + 1))
+            html += "</li>"
         else:
-            msg[0] += '<li>'
+            html += '<li>'
             if i.priority == 0:
-                msg[0] += f'<input type="checkbox" name="filenode_{i.file_id}" data-size="{i.size}"> <label data-size="{i.size}" for="filenode_{i.file_id}">{i.name}</label> / {i.progress}%'
+                html += f'<input type="checkbox" name="filenode_{i.file_id}" data-size="{i.size}"> <label data-size="{i.size}" for="filenode_{i.file_id}">{i.name}</label> / {i.progress}%'
             else:
-                msg[0] += f'<input type="checkbox" checked name="filenode_{i.file_id}" data-size="{i.size}"> <label data-size="{i.size}" for="filenode_{i.file_id}">{i.name}</label> / {i.progress}%'
-            msg[0] += f'<input type="hidden" value="off" name="filenode_{i.file_id}">'
-            msg[0] += "</li>"
+                html += f'<input type="checkbox" checked name="filenode_{i.file_id}" data-size="{i.size}"> <label data-size="{i.size}" for="filenode_{i.file_id}">{i.name}</label> / {i.progress}%'
+            html += f'<input type="hidden" value="off" name="filenode_{i.file_id}">'
+            html += "</li>"
 
     if par.name != ".unwanted":
-        msg[0] += "</ul>"
-    return msg
+        html += "</ul>"
+    return html
+
