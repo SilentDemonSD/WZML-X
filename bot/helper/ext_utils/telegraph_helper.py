f2f86ec799905fe182cba22b4ef2354437ebff2a
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:36 +0600

    improve

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -7,25 +7,58 @@ from telegraph.exceptions import RetryAfterError
 
 from bot import LOGGER, bot_loop, config_dict
 
-
 class TelegraphHelper:
-    def __init__(self, author_name=None, author_url=None):
-        self.telegraph = Telegraph(domain='graph.org')
+    """
+    A helper class for working with the Telegraph API.
+    """
+
+    def __init__(self, telegraph: Telegraph, author_name: str = None, author_url: str = None):
+        """
+        Initialize a new `TelegraphHelper` instance.
+
+        :param telegraph: A `Telegraph` object for interacting with the Telegraph API.
+        :param author_name: The name of the account author.
+        :param author_url: The URL of the account author.
+        """
+        self.telegraph = telegraph
         self.short_name = ''.join(SystemRandom().choices(ascii_letters, k=8))
         self.access_token = None
         self.author_name = author_name
         self.author_url = author_url
 
-    async def create_account(self):
-        await self.telegraph.create_account(
-            short_name=self.short_name,
-            author_name=self.author_name,
-            author_url=self.author_url
-        )
-        self.access_token = self.telegraph.get_access_token()
-        LOGGER.info("Creating Telegraph Account")
+    def __str__(self):
+        """
+        Return a human-readable representation of the `TelegraphHelper` object.
 
-    async def create_page(self, title, content):
+        :return: A string representation of the object.
+        """
+        return f"TelegraphHelper(access_token='{self.access_token}', author_name='{self.author_name}', author_url='{self.author_url}', short_name='{self.short_name}')"
+
+    async def create_account(self):
+        """
+        Create a new Telegraph account.
+
+        :return: None
+        """
+        try:
+            await self.telegraph.create_account(
+                short_name=self.short_name,
+                author_name=self.author_name,
+                author_url=self.author_url
+            )
+            self.access_token = self.telegraph.get_access_token()
+            LOGGER.info("Creating Telegraph Account")
+        except Exception as e:
+            LOGGER.error(f"Error creating Telegraph account: {e}")
+
+    async def create_page(self, title: str, content: str):
+        """
+        Create a new Telegraph page.
+
+        :param title: The title of the page.
+        :param content: The content of the page.
+        :return: The created page object.
+        """
         try:
             return await self.telegraph.create_page(
                 title=title,
@@ -39,7 +72,15 @@ class TelegraphHelper:
             await sleep(st.retry_after)
             return await self.create_page(title, content)
 
-    async def edit_page(self, path, title, content):
+    async def edit_page(self, path: str, title: str, content: str):
+        """
+        Edit an existing Telegraph page.
+
+        :param path: The path of the page.
+        :param title: The title of the page.
+        :param content: The content of the page.
+        :return: The edited page object.
+        """
         try:
             return await self.telegraph.edit_page(
                 path=path,
@@ -54,7 +95,14 @@ class TelegraphHelper:
             await sleep(st.retry_after)
             return await self.edit_page(path, title, content)
 
-    async def edit_telegraph(self, path, telegraph_content):
+    async def edit_telegraph(self, path: list, telegraph_content: list):
+        """
+        Edit multiple Telegraph pages.
+
+        :param path: A list of paths for the pages to be edited.
+        :param telegraph_content: A list of content strings for the pages.
+        :return: None
+        """
         nxt_page = 1
         prev_page = 0
         num_of_path = len(path)
@@ -77,7 +125,11 @@ class TelegraphHelper:
         return
 
 
-telegraph = TelegraphHelper(config_dict['AUTHOR_NAME'],
-                            config_dict['AUTHOR_URL'])
+if __name__ == "__main__":
+    if config_dict is not None:
+        telegraph = TelegraphHelper(Telegraph(domain='graph.org'), config_dict['AUTHOR_NAME'],
+                                    config_dict['AUTHOR_URL'])
 
-bot_loop.run_until_complete(telegraph.create_account())
+        bot_loop.run_until_complete(telegraph.create_account())
+    else:
+        LOGGER.error("config_dict is not defined")
