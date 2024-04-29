21c957c3fe81e35ce07af5bba68dfdcfb4f1850e
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:52:22 +0600

    improve

--- bot/helper/ext_utils/exceptions.py
+++ bot/helper/ext_utils/exceptions.py
@@ -1,20 +1,3 @@
 class DownloadLinkError(Exception):
     """
-    This is the base class for exceptions related to download link errors.
-    When a more specific download link error occurs, this class should be used as the base class.
-    """
-    pass
 
-class DirectDownloadLinkNotFoundError(DownloadLinkError):
-    """
-    This class is raised when a method for extracting a direct download link from an HTTP link cannot be found.
-    It inherits from DownloadLinkError, indicating that it is a specific type of download link error.
-    """
-    pass
-
-class UnsupportedArchiveFormatError(Exception):
-    """
-    This class is raised when the archive format is not supported by the application.
-    It does not inherit from DownloadLinkError, as it is not a type of download link error.
-    """
-    pass
