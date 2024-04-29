56dd2efd55a3f2c60d6dbc2e79d315bfcf506b36
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:45:15 +0600

    improve

--- bot/modules/shell.py
+++ bot/modules/shell.py
@@ -7,17 +7,4 @@ import sys
 import pyrogram
 from pyrogram.errors import RpcError
 from pyrogram.handlers import MessageHandler, EditedMessageHandler
-from pyrogram.filters import command
-
-log = logging.getLogger(__name__)
-
-class ShellBot(pyrogram.Client):
-    def __init__(self):
-        super().__init__(
-            name="ShellBot",
-            api_id=<your_api_id>,
-            api_hash=<your_api_hash>,
-            bot_token=<your_bot_token>,
-            plugins=dict(root="bot.plugins"),
-        )
 
