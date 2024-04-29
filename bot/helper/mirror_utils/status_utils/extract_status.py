ea5e19bfc6cbfb9b293ef2481ebeb3bcc5d39b79
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:52:25 +0600

    improve

--- bot/helper/mirror_utils/status_utils/extract_status.py
+++ bot/helper/mirror_utils/status_utils/extract_status.py
@@ -3,4 +3,11 @@ from pathlib import Path
 from time import time, sleep
 from typing import Optional
 
+def current_time() -> float:
+    """Returns the current time in seconds since the epoch."""
+    return time()
 
+if __name__ == "__main__":
+    while True:
+        print(current_time())
+        sleep(1)
