5e2b081bac6ed4f41ba2432aa057c3dbbf7d8c35
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:49:36 +0600

    improve

--- bot/helper/mirror_utils/download_utils/qbit_download.py
+++ bot/helper/mirror_utils/download_utils/qbit_download.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
 import asyncio
 import aiohttp
 import aiofiles
 import httpx
-from typing import TypeVar, Optional, Tuple, Dict, Any, List, Union
+from typing import TypeVar, Optional, Tuple, Dict, Any, List, Union, AsyncContextManager
 from contextlib import asynccontextmanager
 from bot import download_dict, download_dict_lock, get_client, LOGGER, config_dict, non_queued_dl, queue_dict_lock
 from bot.helper.mirror_utils.status_utils.qbit_status import QbittorrentStatus
@@ -14,7 +13,14 @@ from bot.helper.ext_utils.task_manager import is_queued
 T = TypeVar('T')
 
 @asynccontextmanager
-async def async_open(file: Union[str, aiofiles.File], mode: str = 'r') -> aiofiles.File:
+async def async_open(file: Union[str, aiofiles.File], mode: str = 'r') -> AsyncContextManager[aiofiles.File]:
+    """Open a file asynchronously with context manager."""
     async with aiofiles.open(file, mode) as f:
         yield f
+
+
+@sync_to_async
+def is_queued_sync(task_name: str) -> bool:
+    """Check if a task is queued or not."""
+    return is_queued(task_name)
 
