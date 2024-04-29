7fe2a9344037bae6d10133b4c9c864b5fa7ebce3
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:45:07 +0600

    improve

--- bot/helper/ext_utils/task_manager.py
+++ bot/helper/ext_utils/task_manager.py
@@ -20,31 +20,77 @@ from telegram.ext import CommandHandler, Filters, CallbackContext, run_async
 from telegram.utils.helpers import escape_markdown
 from typing import List, Dict, Union, Tuple, Optional, AsyncContextManager, Callable, Coroutine, Awaitable
 
-# Add necessary imports here
+async def stop_duplicate_check(name: str, listener: 'Any') -> Tuple[Optional[str], Optional[List[InlineKeyboardButton]]]:
+    """
+    Stop duplicate check for a given name and listener.
 
-...
-
-async def stop_duplicate_check(name: str, listener: Any) -> Tuple[Optional[str], Optional[List[InlineKeyboardButton]]]:
+    :param name: Name to stop duplicate check for.
+    :param listener: Listener to stop duplicate check for.
+    :return: Tuple of message string and optional inline keyboard buttons.
+    """
     ...
 
 async def timeval_check(user_id: int) -> Optional[int]:
+    """
+    Check if user has reached the time limit.
+
+    :param user_id: User ID to check time limit for.
+    :return: Optional integer representing the time left or None if the user has not reached the time limit.
+    """
     ...
 
-async def is_queued(uid: int) -> Tuple[bool, Optional[Event]]:
+async def is_queued(uid: int) -> Tuple[bool, Optional[str]]:
+    """
+    Check if a user is queued for download.
+
+    :param uid: User ID to check queue for.
+    :return: Tuple of boolean indicating if the user is queued and optional message string.
+    """
     ...
 
 def start_dl_from_queued(uid: int):
+    """
+    Start download from queued users.
+
+    :param uid: User ID to start download for.
+    """
     ...
 
 def start_up_from_queued(uid: int):
+    """
+    Start upload from queued users.
+
+    :param uid: User ID to start upload for.
+    """
     ...
 
 @run_async
 async def start_from_queued():
+    """
+    Start processing queued users.
+    """
     ...
 
-async def limit_checker(size: int, listener: Any, isTorrent: bool = False, isMega: bool = False, isDriveLink: bool = False, isYtdlp: bool = False, isPlayList: Optional[int] = None) -> Optional[str]:
+async def limit_checker(size: int, listener: 'Any', isTorrent: bool = False, isMega: bool = False, isDriveLink: bool = False, isYtdlp: bool = False, isPlayList: Optional[int] = None) -> Optional[str]:
+    """
+    Check if the user has reached the download limit.
+
+    :param size: Size of the download in bytes.
+    :param listener: Listener to check limit for.
+    :param isTorrent: Boolean indicating if the download is a torrent.
+    :param isMega: Boolean indicating if the download is from mega.
+    :param isDriveLink: Boolean indicating if the download is from Google Drive.
+    :param isYtdlp: Boolean indicating if the download is from youtube-dl.
+    :param isPlayList: Optional integer indicating if the download is a playlist.
+    :return: Optional string with the error message or None if the user has not reached the limit.
+    """
     ...
 
 async def task_utils(message: Message) -> Tuple[List[str], Optional[List[InlineKeyboardButton]]]:
+    """
+    Process the user's message and return the list of tasks and optional inline keyboard buttons.
+
+    :param message: User's message.
+    :return: Tuple of list of tasks and optional inline keyboard buttons.
+    """
     ...
