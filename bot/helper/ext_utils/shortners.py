40a04293d1995c994f1f01c7ffba37907f14df60
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:39 +0600

    improve

--- bot/helper/ext_utils/shortners.py
+++ bot/helper/ext_utils/shortners.py
@@ -1,58 +1,157 @@
+import logging
+import time
 from base64 import b64encode
-from random import choice, random, randrange
-from time import sleep
-from urllib.parse import quote
+from typing import Any, Dict, List, Optional, Union
 
+import requests
 from cloudscraper import create_scraper
-from urllib3 import disable_warnings
+from urllib3 import Timeout
 
 from bot import LOGGER, shorteners_list
 
+logger = logging.getLogger(__name__)
 
-def short_url(longurl, attempt=0):
+def short_url(long_url: str, attempt: int = 0) -> Optional[str]:
+    """Shorten a long URL using various URL shortening services.
+
+    Args:
+        long_url (str): The long URL to shorten.
+        attempt (int, optional): The number of attempts to shorten the URL. Defaults to 0.
+
+    Returns:
+        Optional[str]: The shortened URL, or None if the maximum number of attempts has been reached.
+    """
     if not shorteners_list:
-        return longurl
+        return long_url
+
     if attempt >= 4:
-        return longurl
-    i = 0 if len(shorteners_list) == 1 else randrange(len(shorteners_list))
-    _shorten_dict = shorteners_list[i]
-    _shortener = _shorten_dict['domain']
-    _shortener_api =  _shorten_dict['api_key']
-    cget = create_scraper().request
-    disable_warnings()
+        return long_url
+
+    shortener = choice(shorteners_list)
+    shortener_api = shortener["api_key"]
+
+    scraper = create_scraper()
+    scraper.request(
+        "GET",
+        f"https://{shortener['domain']}/api?api={shortener_api}&url={long_url}",
+        timeout=Timeout(10.0),
+    )
+
     try:
-        if "shorte.st" in _shortener:
-            headers = {'public-api-token': _shortener_api}
-            data = {'urlToShorten': quote(longurl)}
-            return cget('PUT', 'https://api.shorte.st/v1/data/url', headers=headers, data=data).json()['shortenedUrl']
-        elif "linkvertise" in _shortener:
-            url = quote(b64encode(longurl.encode("utf-8")))
-            linkvertise = [
-                f"https://link-to.net/{_shortener_api}/{random() * 1000}/dynamic?r={url}",
-                f"https://up-to-down.net/{_shortener_api}/{random() * 1000}/dynamic?r={url}",
-                f"https://direct-link.net/{_shortener_api}/{random() * 1000}/dynamic?r={url}",
-                f"https://file-link.net/{_shortener_api}/{random() * 1000}/dynamic?r={url}"]
-            return choice(linkvertise)
-        elif "bitly.com" in _shortener:
-            headers = {"Authorization": f"Bearer {_shortener_api}"}
-            return cget('POST', "https://api-ssl.bit.ly/v4/shorten", json={"long_url": longurl}, headers=headers).json()["link"]
-        elif "ouo.io" in _shortener:
-            return cget('GET', f'http://ouo.io/api/{_shortener_api}?s={longurl}', verify=False).text
-        elif "cutt.ly" in _shortener:
-            return cget('GET', f'http://cutt.ly/api/api.php?key={_shortener_api}&short={longurl}').json()['url']['shortLink']
-        else:
-            res = cget('GET', f'https://{_shortener}/api?api={_shortener_api}&url={quote(longurl)}').json()
-            shorted = res['shortenedUrl']
-            if not shorted:
-                shrtco_res = cget('GET', f'https://api.shrtco.de/v2/shorten?url={quote(longurl)}').json()
-                shrtco_link = shrtco_res['result']['full_short_link']
-                res = cget('GET', f'https://{_shortener}/api?api={_shortener_api}&url={shrtco_link}').json()
-                shorted = res['shortenedUrl']
-            if not shorted:
-                shorted = longurl
-            return shorted
-    except Exception as e:
-        LOGGER.error(e)
-        sleep(1)
-        attempt +=1
-        return short_url(longurl, attempt)
+        shortened_url = shortener_response(scraper, long_url, shortener, shortener_api)
+        if shortened_url:
+            return shortened_url
+
+        attempt += 1
+        return short_url(long_url, attempt)
+
+    except (requests.exceptions.RequestException, KeyError, TypeError) as e:
+        logger.error(e)
+        time.sleep(1)
+        attempt += 1
+        return short_url(long_url, attempt)
+
+def shortener_response(
+    scraper: create_scraper,
+    long_url: str,
+    shortener: Dict[str, Union[str, List[str]]],
+    shortener_api: str,
+) -> Optional[str]:
+    """Shorten a long URL using a specific URL shortening service.
+
+    Args:
+        scraper (create_scraper): The cloudscraper instance to use for the request.
+        long_url (str): The long URL to shorten.
+        shortener (Dict[str, Union[str, List[str]]]): The shortener configuration dictionary.
+        shortener_api (str): The shortener API key.
+
+    Returns:
+        Optional[str]: The shortened URL, or None if the shortener service failed to respond.
+    """
+    if "shorte.st" in shortener["domain"]:
+        headers = {"public-api-token": shortener_api}
+        data = {"urlToShorten": b64encode(long_url.encode("utf-8")).decode("utf-8")}
+        response = scraper.request(
+            "PUT",
+            "https://api.shorte.st/v1/data/url",
+            headers=headers,
+            data=data,
+        )
+        if response.status_code == 200:
+            return response.json()["shortenedUrl"]
+
+    elif "linkvertise" in shortener["domain"]:
+        url = b64encode(long_url.encode("utf-8")).decode("utf-8")
+        linkvertise = [
+            f"https://link-to.net/{shortener_api}/{random() * 1000}/dynamic?r={url}",
+            f"https://up-to-down.net/{shortener_api}/{random() * 1000}/dynamic?r={url}",
+            f"https://direct-link.net/{shortener_api}/{random() * 1000}/dynamic?r={url}",
+            f"https://file-link.net/{shortener_api}/{random() * 1000}/dynamic?r={url}"]
+        response = scraper.request(
+            "GET",
+            choice(linkvertise),
+            timeout=Timeout(10.0),
+        )
+        if response.status_code == 200:
+            return response.text
+
+    elif "bitly.com" in shortener["domain"]:
+        headers = {"Authorization": f"Bearer {shortener_api}"}
+        data = {"long_url": long_url}
+        response = scraper.request(
+            "POST",
+            "https://api-ssl.bit.ly/v4/shorten",
+            json=data,
+            headers=headers,
+            timeout=Timeout(10.0),
+        )
+        if response.status_code == 200:
+            return response.json()["link"]
+
+    elif "ouo.io" in shortener["domain"]:
+        response = scraper.request(
+            "GET",
+            f'http://ouo.io/api/{shortener_api}?s={long_url}',
+            timeout=Timeout(10.0),
+        )
+        if response.status_code == 200:
+            return response.text
+
+    elif "cutt.ly" in shortener["domain"]:
+        response = scraper.request(
+            "GET",
+            f'http://cutt.ly/api/api.php?key={shortener_api}&short={long_url}',
+            timeout=Timeout(10.0),
+        )
+        if response.status_code == 200:
+            return response.json()["url"]["shortLink"]
+
+    else:
+        response = scraper.request(
+            "GET",
+            f'https://{shortener["domain"]}/api?api={shortener_api}&url={long_url}',
+            timeout=Timeout(10.0),
+        )
+        if response.status_code == 200:
+            shortened_url = response.json().get("shortenedUrl")
+            if shortened_url:
+                return shortened_url
+
+            shrtco_response = scraper.request(
+                "GET",
+                f'https://api.shrtco.de/v2/shorten?url={long_url}',
+                timeout=Timeout(10.0),
+            )
+            if shrtco_response.status_code == 200:
+                shrtco_link = shrtco_response.json()["result"]["full_short_link"]
+                response = scraper.request(
+                    "GET",
+                    f'https://{shortener["domain"]}/api?api={shortener_api}&url={shrtco_link}',
+                    timeout=Timeout(10.0),
+                )
+                if response.status_code == 200:
+                    shortened_url = response.json().get("shortenedUrl")
+                    if shortened_url:
+                        return shortened_url
+
+    return None
