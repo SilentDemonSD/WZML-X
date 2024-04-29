fb583d1ac80c3c07807397aded0967767f963161
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:44:56 +0600

    improve

--- bot/helper/telegram_helper/button_build.py
+++ bot/helper/telegram_helper/button_build.py
@@ -2,10 +2,3 @@ from typing import List, Union, Dict
 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-class ButtonPosition:
-    HEADER = 'header'
-    F_BODY = 'f_body'
-    L_BODY = 'l_body'
-    FOOTER = 'footer'
-    BUTTONS = 'buttons'
-
