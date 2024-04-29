7ddbb330d76c3a18ba53977cf5028148b0cd1b57
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:51:44 +0600

    comment

--- bot/helper/mirror_utils/status_utils/split_status.py
+++ bot/helper/mirror_utils/status_utils/split_status.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
 
+# Import the bot_utils module from the bot.helper.ext_utils.bot_utils package.
+# This module contains various utility functions used by the bot.
 import bot.helper.ext_utils.bot_utils as bot_utils 
+
