ff65b4912f62a3e7fb990818efd149c8ad684d32
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:37:49 +0600

    improve

--- bot/helper/telegram_helper/filters.py
+++ bot/helper/telegram_helper/filters.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-from pyrogram.filters import create
+from typing import Union, AsyncContextManager, Callable, Coroutine, Any
+
+import pyrogram.filters
 from pyrogram.enums import ChatType
+from pyrogram.errors import ChatAdminRequired
 
 from bot import user_data, OWNER_ID
 from bot.helper.telegram_helper.message_utils import chat_info
@@ -8,62 +11,84 @@ from bot.helper.telegram_helper.message_utils import chat_info
 
 class CustomFilters:
 
-    async def owner_filter(self, _, message):
+    async def owner_filter(self, _: Any, message: Union[pyrogram.types.Message, pyrogram.types.CallbackQuery]) -> bool:
+        """Check if the user is the owner of the bot."""
         user = message.from_user or message.sender_chat
-        uid = user.id
-        return uid == OWNER_ID
+        return user.id == OWNER_ID
 
-    owner = create(owner_filter)
+    owner = pyrogram.filters.create(owner_filter)
 
-    async def authorized_user(self, _, message):
+    async def authorized_user(self, _: Any, message: Union[pyrogram.types.Message, pyrogram.types.CallbackQuery]) -> bool:
+        """Check if the user is authorized to use the bot."""
         user = message.from_user or message.sender_chat
-        uid = user.id
-        if bool(uid == OWNER_ID or (uid in user_data and (user_data[uid].get('is_auth', False) or user_data[uid].get('is_sudo', False)))):
+        user_id = user.id
+
+        if user_id == OWNER_ID or (
+            user_id in user_data
+            and (user_data[user_id].get('is_auth', False) or user_data[user_id].get('is_sudo', False))
+        ):
             return True
-        
-        auth_chat = False
+
         chat_id = message.chat.id
         if chat_id in user_data and user_data[chat_id].get('is_auth', False):
-            if len(topic_ids := user_data[chat_id].get('topic_ids', [])) == 0:
-                auth_chat = True
-            elif (is_forum := message.reply_to_message) and ((is_forum.text is None and is_forum.caption is None and is_forum.id in topic_ids)
-                or ((is_forum.text or is_forum.caption) and ((not is_forum.reply_to_top_message_id and is_forum.reply_to_message_id in topic_ids)
-                or (is_forum.reply_to_top_message_id in topic_ids)))):
-                auth_chat = True
-        return auth_chat
-    
-    authorized = create(authorized_user)
-    
-    async def authorized_usetting(self, _, message):
-        uid = (message.from_user or message.sender_chat).id
+            topic_ids = user_data[chat_id].get('topic_ids', [])
+            if not topic_ids:
+                return True
+
+            is_forum = message.reply_to_message
+            if (
+                not is_forum.text and not is_forum.caption and is_forum.id in topic_ids
+                or (is_forum.text or is_forum.caption)
+                and (
+                    not is_forum.reply_to_top_message_id and is_forum.reply_to_message_id in topic_ids
+                    or is_forum.reply_to_top_message_id in topic_ids
+                )
+            ):
+                return True
+
+        return False
+
+    authorized = pyrogram.filters.create(authorized_user)
+
+    async def authorized_user_setting(self, _: Any, message: Union[pyrogram.types.Message, pyrogram.types.CallbackQuery]) -> bool:
+        """Check if the user is authorized to change settings."""
+        user_id = (message.from_user or message.sender_chat).id
         chat_id = message.chat.id
-        isExists = False
-        if uid == OWNER_ID or (uid in user_data and (user_data[uid].get('is_auth', False) or user_data[uid].get('is_sudo', False))) or (chat_id in user_data and user_data[chat_id].get('is_auth', False)):
-            isExists = True
-        elif message.chat.type == ChatType.PRIVATE:
+
+        if (
+            user_id == OWNER_ID
+            or (user_id in user_data and (user_data[user_id].get('is_auth', False) or user_data[user_id].get('is_sudo', False)))
+            or (chat_id in user_data and user_data[chat_id].get('is_auth', False))
+        ):
+            return True
+
+        if message.chat.type == ChatType.PRIVATE:
             for channel_id in user_data:
                 if not (user_data[channel_id].get('is_auth') and str(channel_id).startswith('-100')):
                     continue
+
                 try:
-                    if await (await chat_info(str(channel_id))).get_member(uid):
-                        isExists = True
-                        break
-                except:
+                    if await chat_info(str(channel_id)).get_member(user_id):
+                        return True
+                except pyrogram.errors.ChatAdminRequired:
                     continue
-        return isExists
-        
-    authorized_uset = create(authorized_usetting)
 
-    async def sudo_user(self, _, message):
-        user = message.from_user or message.sender_chat
-        uid = user.id
-        return bool(uid == OWNER_ID or uid in user_data and user_data[uid].get('is_sudo'))
+        return False
 
-    sudo = create(sudo_user)
-    
-    async def blacklist_user(self, _, message):
+    authorized_user_setting = pyrogram.filters.create(authorized_user_setting)
+
+    async def sudo_user(self, _: Any, message: Union[pyrogram.types.Message, pyrogram.types.CallbackQuery]) -> bool:
+        """Check if the user is a sudo user."""
         user = message.from_user or message.sender_chat
-        uid = user.id
-        return bool(uid != OWNER_ID and uid in user_data and user_data[uid].get('is_blacklist'))
-        
-    blacklisted = create(blacklist_user)
+        user_id = user.id
+        return user_id == OWNER_ID or (user_id in user_data and user_data[user_id].get('is_sudo'))
+
+    sudo = pyrogram.filters.create(sudo_user)
+
+    async def blacklist_user(self, _: Any, message: Union[pyrogram.types.Message, pyrogram.types.CallbackQuery]) -> bool:
+        """Check if the user is blacklisted."""
+        user = message.from_user or message.sender_chat
+        user_id = user.id
+        return user_id != OWNER_ID and user_id in user_data and user_data[user_id].get('is_blacklist')
+
+    blacklisted = pyrogram.filters.create(blacklist_user)
