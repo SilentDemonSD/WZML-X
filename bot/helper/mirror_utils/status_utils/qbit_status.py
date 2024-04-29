7334f0a72378ef133d43d5b0e8e7bf1dde8339d8
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:37:16 +0600

    improve

--- bot/helper/mirror_utils/status_utils/qbit_status.py
+++ bot/helper/mirror_utils/status_utils/qbit_status.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
-from asyncio import sleep
+import asyncio
+import typing
 
-from bot import LOGGER, get_client, QbTorrents, qb_listener_lock
-from bot.helper.ext_utils.bot_utils import EngineStatus, MirrorStatus, get_readable_file_size, get_readable_time, sync_to_async
+from bot import LOGGER, get_client, QbTorrents
+from bot.helper.ext_utils.bot_utils import EngineStatus, MirrorStatus, get_readable_file_size, get_readable_time
 
-def get_download(client, tag):
+def get_download(client, tag: str) -> typing.Optional[typing.Dict]:
     """
     Fetches the download information for the given tag.
 
@@ -33,7 +33,7 @@ class QbittorrentStatus:
     __info (object): Information about the download.
     """
 
-    def __init__(self, listener, seeding=False, queued=False):
+    def __init__(self, listener, seeding: bool = False, queued: bool = False):
         """
         Initializes the QbittorrentStatus class with the given listener, seeding and queued flags.
 
@@ -61,7 +61,7 @@ class QbittorrentStatus:
     # Properties with getter methods
 
     @property
-    def progress(self):
+    def progress(self) -> str:
         """
         Returns:
         progress (str): Progress of the download as a percentage.
@@ -69,7 +69,7 @@ class QbittorrentStatus:
         return f'{round(self.__info.progress*100, 2)}%'
 
     @property
-    def processed_bytes(self):
+    def processed_bytes(self) -> str:
         """
         Returns:
         processed_bytes (str): Downloaded bytes in a human-readable format.
@@ -77,7 +77,7 @@ class QbittorrentStatus:
         return get_readable_file_size(self.__info.downloaded)
 
     @property
-    def speed(self):
+    def speed(self) -> str:
         """
         Returns:
         speed (str): Download speed in a human-readable format.
@@ -85,7 +85,7 @@ class QbittorrentStatus:
         return f"{get_readable_file_size(self.__info.dlspeed)}/s"
 
     @property
-    def name(self):
+    def name(self) -> str:
         """
         Returns:
         name (str): Name of the download.
@@ -96,7 +96,7 @@ class QbittorrentStatus:
             return self.__info.name
 
     @property
-    def size(self):
+    def size(self) -> str:
         """
         Returns:
         size (str): Total size of the download in a human-readable format.
@@ -104,7 +104,7 @@ class QbittorrentStatus:
         return get_readable_file_size(self.__info.size)
 
     @property
-    def eta(self):
+    def eta(self) -> str:
         """
         Returns:
         eta (str): Estimated time to completion in a human-readable format.
@@ -112,7 +112,7 @@ class QbittorrentStatus:
         return get_readable_time(self.__info.eta)
 
     @property
-    def status(self):
+    def status(self) -> str:
         """
         Returns:
         status (str): Status of the download.
@@ -133,7 +133,7 @@ class QbittorrentStatus:
             return MirrorStatus.STATUS_DOWNLOADING
 
     @property
-    def seeders_num(self):
+    def seeders_num(self) -> int:
         """
         Returns:
         seeders_num (int): Number of seeders for the download.
@@ -141,7 +141,7 @@ class QbittorrentStatus:
         return self.__info.num_seeds
 
     @property
-    def leechers_num(self):
+    def leechers_num(self) -> int:
         """
         Returns:
         leechers_num (int): Number of leechers for the download.
@@ -149,7 +149,7 @@ class QbittorrentStatus:
         return self.__info.num_leechs
 
     @property
-    def uploaded_bytes(self):
+    def uploaded_bytes(self) -> str:
         """
         Returns:
         uploaded_bytes (str): Uploaded bytes in a human-readable format.
@@ -157,7 +157,7 @@ class QbittorrentStatus:
         return get_readable_file_size(self.__info.uploaded)
 
     @property
-    def upload_speed(self):
+    def upload_speed(self) -> str:
         """
         Returns:
         upload_speed (str): Upload speed in a human-readable format.
@@ -165,7 +165,7 @@ class QbittorrentStatus:
         return f"{get_readable_file_size(self.__info.upspeed)}/s"
 
     @property
-    def ratio(self):
+    def ratio(self) -> str:
         """
         Returns:
         ratio (str): Ratio of upload to download.
@@ -173,7 +173,7 @@ class QbittorrentStatus:
         return f"{round(self.__info.ratio, 3)}"
 
     @property
-    def seeding_time(self):
+    def seeding_time(self) -> str:
         """
         Returns:
         seeding_time (str): Seeding time in a human-readable format.
@@ -182,14 +182,14 @@ class QbittorrentStatus:
 
     # Methods
 
-    def download(self):
+    def download(self) -> 'QbittorrentStatus':
         """
         Returns:
         self: QbittorrentStatus object itself for chaining method calls.
         """
         return self
 
-    def gid(self):
+    def gid(self) -> str:
         """
         Returns:
         gid (str): First 12 characters of the hash.
@@ -197,7 +197,7 @@ class QbittorrentStatus:
         self.__update()
         return self.__info.hash[:12]
 
-    def hash(self):
+    def hash(self) -> str:
         """
         Returns:
         hash (str): Hash of the download.
@@ -205,21 +205,21 @@ class QbittorrentStatus:
         self.__update()
         return self.__info.hash
 
-    def client(self):
+    def client(self) -> object:
         """
         Returns:
         client (object): Qbittorrent client object.
         """
         return self.__client
 
-    def listener(self):
+    def listener(self) -> object:
         """
         Returns:
         listener (object): Listener object associated with the download.
         """
         return self.__listener
 
-    async def cancel_download(self):
+    async def cancel_download(self) -> typing.Coroutine:
         """
         Cancels the download and deletes the associated files and tags.
         """
@@ -232,7 +232,7 @@ class QbittorrentStatus:
             else:
                 LOGGER.info(f"Cancelling Download: {self.__info.name}")
                 msg = 'Download stopped by user!'
-            await sleep(0.3)
+            await asyncio.sleep(0.3)
             await self.__listener.onDownloadError(msg)
             await sync_to_async(self.__client.torrents_delete, torrent_hashes=self.__info.hash, delete_files=True)
             await sync_to_async(self.__client.torrents_delete_tags, tags=self.__info.tags)
@@ -240,10 +240,3 @@ class QbittorrentStatus:
                 if self.__info.tags in QbTorrents:
                     del QbTorrents[self.__info.tags]
 
-    @property
-    def eng(self):
-        """
-        Returns:
-        eng (str): Status of the engine.
-        """
-        return EngineStatus().STATUS_QB
