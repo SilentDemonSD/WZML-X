5f6a29a0d11d54e913a8f7c0e7193195e77c2f2f
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:23 +0600

    improve

--- bot/version.py
+++ bot/version.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-def get_version() -> str:
+def get_version(MAJOR: str, MINOR: str, PATCH: str, STATE: str) -> str:
     '''
     Returns the version details. Do not Interfere with this !
 
+    :param MAJOR: The major version number
+    :param MINOR: The minor version number
+    :param PATCH: The patch version number
+    :param STATE: The state of the release
     :return: The version details in the format 'vMAJOR.MINOR.PATCH-STATE'
     :rtype: str
     '''
-    MAJOR = '1'
-    MINOR = '2'
-    PATCH = '0'
-    STATE = 'b'
     return f"v{MAJOR}.{MINOR}.{PATCH}-{STATE}"
 
 if __name__ == '__main__':
-    print(get_version())
+    print(get_version(MAJOR='1', MINOR='2', PATCH='0', STATE='b'))
