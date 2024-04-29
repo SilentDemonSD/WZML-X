8775788d70468a81e2f5ae36c1b79d0a12240dd3
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:51:43 +0600

    comment

--- update.py
+++ update.py
@@ -6,6 +6,7 @@ import requests
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
+# Configure logging
 logging.basicConfig(
     format="[%(asctime)s] [%(levelname)s] - %(message)s",
     datefmt="%d-%b-%y %I:%M:%S %p",
@@ -28,6 +29,7 @@ def update_environment_variables(client: MongoClient, bot_id: str) -> None:
     old_config = db.settings.deployConfig.find_one({'_id': bot_id})
     config_dict = db.settings.config.find_one({'_id': bot_id})
 
+    # Update the environment variables with the ones from the database
     if old_config and old_config != dict(os.environ):
         for k, v in old_config.items():
             if k in os.environ:
@@ -65,16 +67,20 @@ def update_codebase(upstream_url: str, upstream_branch: str) -> None:
         logging.error(f"Failed to update codebase: {e}")
 
 if __name__ == "__main__":
+    # Load environment variables from .env file
     load_env_variables()
 
+    # Check for missing environment variables
     if 'MISSING_ENV_VAR' in os.environ:
         logging.error('The README.md file should be read! Exiting now!')
         exit()
 
+    # Check for BOT_TOKEN
     if not os.getenv('BOT_TOKEN'):
         logging.error("BOT_TOKEN variable is missing! Exiting now")
         exit(1)
 
+    # Initialize MongoDB connection and update environment variables
     bot_id = os.getenv('BOT_TOKEN').split(':', 1)[0]
     DATABASE_URL = os.getenv('DATABASE_URL')
 
@@ -82,9 +88,11 @@ if __name__ == "__main__":
         client = initialize_mongodb_connection(DATABASE_URL)
         update_environment_variables(client, bot_id)
 
+    # Upgrade Python packages if specified
     if os.getenv('UPGRADE_PACKAGES', 'False').lower() == 'true':
         upgrade_packages()
 
+    # Update codebase from the upstream repository if specified
     UPSTREAM_REPO = os.getenv('UPSTREAM_REPO')
     UPSTREAM_BRANCH = os.getenv('UPSTREAM_BRANCH', 'master')
 
