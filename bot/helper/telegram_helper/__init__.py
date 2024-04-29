0b05940caceaa91feb56a34f418da9c011f08573
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:45:16 +0600

    improve

--- bot/helper/telegram_helper/__init__.py
+++ bot/helper/telegram_helper/__init__.py
@@ -3,8 +3,9 @@ def rectangle_area(length: float, width: float) -> float:
     # The area of a rectangle is calculated by multiplying its length and width
     area = length * width
     
-    # The area is then returned as the output of the function with a more informative message
-    return area with a message: "The area of the rectangle with length {:.2f} and width {:.2f} is {:.2f}.".format(length, width, area)
+    # The area is returned as the output of the function with a more informative message
+    print("The area of the rectangle with length {:.2f} and width {:.2f} is {:.2f}.".format(length, width, area))
+    return area
 
 # Example usage:
-print(rectangle_area(4, 5))
+rectangle_area(4, 5)
