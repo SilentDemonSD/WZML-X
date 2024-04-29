af9fbedf1d4e3c7a22e66ea7ea6030904119cfbf
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:39:48 +0600

    improve

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -4,5 +4,6 @@ import asyncio
 import time
 from traceback import format_exc
 from logging import getLogger, ERROR
-from aiofiles.os import remove as aioremove, path as aiopath, rename as aiorename, makedirs, rmdir, mkdir
+
+import aiofiles
 
