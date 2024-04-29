539fbedcb660c2a5e5b76266befea4593e284a5a
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:36:54 +0600

    improve

--- bot/helper/__init__.py
+++ bot/helper/__init__.py
@@ -1,7 +1,11 @@
-# This function calculates the area of a rectangle
-def rectangle_area(length, width):
-    # The area of a rectangle is calculated by multiplying its length and width
+def calculate_rectangle_area(length: float, width: float) -> float:
+    """
+    This function calculates the area of a rectangle.
+
+    :param length: The length of the rectangle.
+    :param width: The width of the rectangle.
+    :return: The area of the rectangle.
+    """
     area = length * width
-    
-    # The area is then returned as the output of the function
     return area
+
