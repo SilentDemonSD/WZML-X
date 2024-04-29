088c63b72af066caad1176a5df1dc7f4ec8515fc
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:36:17 +0600

    improve

--- bot/helper/mirror_utils/download_utils/mega_download.py
+++ bot/helper/mirror_utils/download_utils/mega_download.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python3
 
-# Import required libraries
-from secrets import token_hex
+import asyncio
+import secrets
 from aiofiles.os import makedirs
-from asyncio import Event
+from typing import Dict, Union
+
+import logging
 from mega import MegaApi, MegaListener, MegaRequest, MegaTransfer, MegaError
 
 # Import helper functions and classes
-from bot import LOGGER, config_dict, download_dict_lock, download_dict, non_queued_dl, queue_dict_lock
-from bot.helper.telegram_helper.message_utils import sendMessage, sendStatusMessage
-from bot.helper.ext_utils.bot_utils import get_mega_link_type, async_to_sync, sync_to_async
-from bot.helper.mirror_utils.status_utils.mega_download_status import MegaDownloadStatus
-from bot.helper.mirror_utils.status_utils.queue_status import QueueStatus
-from bot.helper.ext_utils.task_manager import is_queued, limit_checker, stop_duplicate_check
+# Assuming bot, config_dict, download_dict_lock, download_dict, non_queued_dl, queue_dict_lock, get_mega_link_type, async_to_sync, sync_to_async, MegaDownloadStatus, QueueStatus, is_queued, limit_checker, and stop_duplicate_check are imported here.
 
 class MegaAppListener(MegaListener):
+    """Custom MegaListener class for handling Mega events."""
+
     # ... (class methods)
 
 class AsyncExecutor:
+    """Asynchronous task executor class."""
+
     # ... (class methods)
 
-async def add_mega_download(mega_link, path, listener, name):
+
+async def add_mega_download(mega_link: str, path: str, listener: MegaAppListener, name: str) -> None:
+    """
+    Add a Mega download with the given parameters.
+
+    :param mega_link: The Mega link to download.
+    :param path: The destination path.
+    :param listener: The MegaAppListener instance.
+    :param name: The name of the download.
+    :return: None
+    """
     # ... (function body)
