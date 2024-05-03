4bbba5bc47c8e4ec27768b270a27046adaa48bf8
Silent Demon SD ( MysterySD ) <105407900+SilentDemonSD@users.noreply.github.com>
2024-05-03 20:10:26 +0530

    Rebooting Repo..!!
    Signed-off-by: Silent Demon SD ( MysterySD ) <105407900+SilentDemonSD@users.noreply.github.com>

--- bot/version.py
+++ bot/version.py
@@ -6,10 +6,10 @@ def get_version() -> str:
     :return: The version details in the format 'vMAJOR.MINOR.PATCH-STATE'
     :rtype: str
     '''
-    MAJOR = '1'
-    MINOR = '3'
-    PATCH = '3'
-    STATE = 'x'
+    MAJOR = '2'
+    MINOR = '0'
+    PATCH = '0'
+    STATE = 'dev'
     return f"v{MAJOR}.{MINOR}.{PATCH}-{STATE}"
 
 if __name__ == '__main__':
