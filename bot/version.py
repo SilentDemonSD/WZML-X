021d79cfb352c5a2e6b670a63eae92fd6fe35b7f
Silent Demon SD ( MysterySD ) <105407900+SilentDemonSD@users.noreply.github.com>
2024-05-10 15:36:18 +0000

    update: formatize & upgrade

--- bot/version.py
+++ bot/version.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 def get_version() -> str:
-    '''
+    """
     Returns the version details. Do not Interfere with this !
 
     :return: The version details in the format 'vMAJOR.MINOR.PATCH-STATE'
     :rtype: str
-    '''
-    MAJOR = '2'
-    MINOR = '0'
-    PATCH = '0'
-    STATE = 'dev'
+    """
+    MAJOR = "2"
+    MINOR = "0"
+    PATCH = "0"
+    STATE = "dev"
     return f"v{MAJOR}.{MINOR}.{PATCH}-{STATE}"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(get_version())
