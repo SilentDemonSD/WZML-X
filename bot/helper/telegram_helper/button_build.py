f08b89bbb34a0e206187f84af4e1ee43bfeb891a
SilentDemonSD <drxxstrange@gmail.com>
2024-05-18 09:49:13 +0530

    update: refactor & 1 line fevers

--- bot/helper/telegram_helper/button_build.py
+++ bot/helper/telegram_helper/button_build.py
@@ -1,4 +1,3 @@
-
 class ButtonMaker:
     def __init__(self, isSwitch=False):
         if isSwitch:
@@ -15,81 +14,37 @@ class ButtonMaker:
         self.__first_body_button = []
         self.__last_body_button = []
         self.__footer_button = []
+        self.button_lists = {
+            None: self.__button,
+            "header": self.__header_button,
+            "f_body": self.__first_body_button,
+            "l_body": self.__last_body_button,
+            "footer": self.__footer_button,
+        }
 
     def ubutton(self, key, link, position=None):
-        if not position:
-            self.__button.append(self.InlineKeyboardButton(text=key, url=link))
-        elif position == "header":
-            self.__header_button.append(self.InlineKeyboardButton(text=key, url=link))
-        elif position == "f_body":
-            self.__first_body_button.append(self.InlineKeyboardButton(text=key, url=link))
-        elif position == "l_body":
-            self.__last_body_button.append(self.InlineKeyboardButton(text=key, url=link))
-        elif position == "footer":
-            self.__footer_button.append(self.InlineKeyboardButton(text=key, url=link))
+        self.button_lists.get(position, self.__button).append(self.InlineKeyboardButton(text=key, url=link))
 
     def ibutton(self, key, data, position=None):
-        if not position:
-            self.__button.append(self.InlineKeyboardButton(text=key, callback_data=data))
-        elif position == "header":
-            self.__header_button.append(
-                self.InlineKeyboardButton(text=key, callback_data=data)
-            )
-        elif position == "f_body":
-            self.__first_body_button.append(
-                self.InlineKeyboardButton(text=key, callback_data=data)
-            )
-        elif position == "l_body":
-            self.__last_body_button.append(
-                self.InlineKeyboardButton(text=key, callback_data=data)
-            )
-        elif position == "footer":
-            self.__footer_button.append(
-                self.InlineKeyboardButton(text=key, callback_data=data)
-            )
+        self.button_lists.get(position, self.__button).append(self.InlineKeyboardButton(text=key, callback_data=data))
+    
+    def add_buttons(self, button_list, cols, menu, insert_at_start=False):
+        if button_list:
+            chunks = [button_list[i: i + cols] for i in range(0, len(button_list), cols)]
+            if insert_at_start:
+                menu[:0] = chunks
+            else:
+                menu.extend(chunks)
 
     def build_menu(self, b_cols=1, h_cols=8, fb_cols=2, lb_cols=2, f_cols=8):
-        menu = [
-            self.__button[i : i + b_cols] for i in range(0, len(self.__button), b_cols)
-        ]
-        if self.__header_button:
-            if len(self.__header_button) > h_cols:
-                header_buttons = [
-                    self.__header_button[i : i + h_cols]
-                    for i in range(0, len(self.__header_button), h_cols)
-                ]
-                menu = header_buttons + menu
-            else:
-                menu.insert(0, self.__header_button)
-        if self.__first_body_button:
-            if len(self.__first_body_button) > fb_cols:
-                [
-                    menu.append(self.__first_body_button[i : i + fb_cols])
-                    for i in range(0, len(self.__first_body_button), fb_cols)
-                ]
-            else:
-                menu.append(self.__first_body_button)
-        if self.__last_body_button:
-            if len(self.__last_body_button) > lb_cols:
-                [
-                    menu.append(self.__last_body_button[i : i + lb_cols])
-                    for i in range(0, len(self.__last_body_button), lb_cols)
-                ]
-            else:
-                menu.append(self.__last_body_button)
-        if self.__footer_button:
-            if len(self.__footer_button) > f_cols:
-                [
-                    menu.append(self.__footer_button[i : i + f_cols])
-                    for i in range(0, len(self.__footer_button), f_cols)
-                ]
-            else:
-                menu.append(self.__footer_button)
+        menu = [self.__button[i: i + b_cols] for i in range(0, len(self.__button), b_cols)]
+
+        self.add_buttons(self.__header_button, h_cols, menu, True)
+        self.add_buttons(self.__first_body_button, fb_cols, menu)
+        self.add_buttons(self.__last_body_button, lb_cols, menu)
+        self.add_buttons(self.__footer_button, f_cols, menu)
+
         return self.InlineMarkup(menu)
     
     def reset(self):
-        self.__button = []
-        self.__header_button = []
-        self.__first_body_button = []
-        self.__last_body_button = []
-        self.__footer_button = []
+        self.__button, self.__header_button, self.__first_body_button, self.__last_body_button, self.__footer_button = [], [], [], [], []
